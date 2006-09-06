48f9de49c269c073d5b34a9841cf8d96bbb5fbcf
Christopher Murtagh <chris@blamestella.com>
2006-09-06 16:57:02 +0000

    adding query error reporting should the create fail
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@38 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB/User.pm
+++ LedgerSMB/User.pm
@@ -330,7 +330,7 @@ sub dbcreate {
   &dbconnect_vars($form, $form->{dbdefault});
   my $dbh = DBI->connect($form->{dbconnect}, $form->{dbuser}, $form->{dbpasswd}) or $form->dberror;
   my $query = qq|$dbcreate{$form->{dbdriver}}|;
-  $dbh->do($query);
+  $dbh->do($query) || $form->dberror($query);
 
   if ($form->{dbdriver} eq 'Oracle') {
     $query = qq|GRANT CONNECT,RESOURCE TO "$form->{db}"|;
