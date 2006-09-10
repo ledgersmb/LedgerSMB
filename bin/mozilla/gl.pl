cdabf150ab4d23eed1e2b6af05767298d00085b9
Chris Travers <chris.travers@gmail.com>
2006-09-10 20:04:26 +0000

    Updated copyright notices on remaining bin/mozilla files. Centralized Lynx support to merely include most of the files in the bin/mozilla directory
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@59 4979c152-3d1c-0410-bac9-87ea11338e46

--- bin/mozilla/gl.pl
+++ bin/mozilla/gl.pl
@@ -1,12 +1,42 @@
 #=====================================================================
-# LedgerSMB 
-# Small Medium Business Accounting software
-# 
-# See COPYRIGHT file for copyright information
-#======================================================================
+# LedgerSMB Small Medium Business Accounting
+
+# Copyright (C) 2006
+# This work contains copyrighted information from a number of sources all used
+# with permission.
 #
-# This file has NOT undergone whitespace cleanup.
+# This file contains source code included with or based on SQL-Ledger which
+# is Copyright Dieter Simader and DWS Systems Inc. 2000-2005 and licensed 
+# under the GNU General Public License version 2 or, at your option, any later 
+# version.  For a full list including contact information of contributors, 
+# maintainers, and copyright holders, see the CONTRIBUTORS file.
 #
+# Original Copyright Notice from SQL-Ledger 2.6.17 (before the fork):
+# Copyright (c) 2001
+#
+#  Author: DWS Systems Inc.
+#     Web: http://www.sql-ledger.org
+#
+# Contributors:
+#
+#
+#  Author: DWS Systems Inc.
+#     Web: http://sourceforge.net/projects/ledger-smb/
+#
+# Contributors:
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #======================================================================
 #
 # Genereal Ledger
