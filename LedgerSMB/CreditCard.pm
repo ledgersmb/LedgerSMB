4e43cdcdd0e6f83dcefc80ba7ca1dbb00fac883e
Chris Travers <chris.travers@gmail.com>
2006-10-04 02:10:51 +0000

    Added copyright notices to credit card modules.
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@188 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB/CreditCard.pm
+++ LedgerSMB/CreditCard.pm
@@ -1,3 +1,18 @@
+#=====================================================================
+# LedgerSMB 
+# Small Medium Business Accounting software
+# http://www.ledgersmb.org/
+# Copyright (C) 2006
+# This work contains copyrighted information from a number of sources all used
+# with permission.
+#
+# This file contains source code included with or based on SQL-Ledger which
+# is Copyright Dieter Simader and DWS Systems Inc. 2000-2005 and licensed
+# under the GNU General Public License version 2 or, at your option, any later
+# version.  For a full list including contact information of contributors,
+# maintainers, and copyright holders, see the CONTRIBUTORS file.
+
+# POS Credit card processing-- could be extended for ecommerce
 
 package CreditCard;
 use LedgerSMB::CreditCard::Config;
