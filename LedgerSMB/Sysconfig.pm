4fabc96fd23f17db87cd70e5d86c93cedad9662e
Chris Travers <chris.travers@gmail.com>
2006-10-17 23:47:58 +0000

    Added session abstraction. Added sysconfig.pm which allows for LedgerSMB configuration parameters to be stored in a separate namespace.
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@222 4979c152-3d1c-0410-bac9-87ea11338e46

---
+++ LedgerSMB/Sysconfig.pm
@@ -0,0 +1,7 @@
+#  This is the new configuration file for LedgerSMB.  Eventually all system 
+# configuration directives will go here,  This will probably not fully replace
+# the ledger-smb.conf until 1.3, however.
+
+package LSMBConfig;
+
+$session='DB';
