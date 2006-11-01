ba3c6b561888ffac04ab835caf49c4cd9fe1d30a
Chris Travers <chris.travers@gmail.com>
2006-11-01 05:05:19 +0000

    More on the cli script wrapper
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@446 4979c152-3d1c-0410-bac9-87ea11338e46

--- utils/cli/ledgersmb_cli.pl
+++ utils/cli/ledgersmb_cli.pl
@@ -10,12 +10,24 @@ use LedgerSMB::Sysconfig;
 $form = new Form;
 
 while ($line = <>){
+	$line =~ s/#.*//; # strip out comments
 	if ($line =~ /^\s*CALL (.+) INTO (.+)/){
-		$form->{$2} = %${$1};
+		$form->{$2} = &{$1}(\%$form);
 	} elsif ($line =~ /^\s*MODULE (.+)/){
+		$module = $1;
+		$module =~ s/::/\//;
+		eval { require $module; };
 	} elsif ($line =~ /^\s*ENV:(.+)\s*=\s*(.*)/){
+		$ENV{$1} = $2;
 	} elsif ($line =~ /^\s*(.+)\s*=\s*(.+)/){
-	} else {
+		$form->{$1} = $2;
+	} elsif ($line =~ /^\s*CALL (.+)/){
+		&{$1}(\%$form);
+	} elsif ($line !~ /^\s*$/) {
 		die "Parse error in script file: $line";
 	}
+}
+
+for (keys %$form){
+	print "$_ = $form->{$_}\n";
 }
