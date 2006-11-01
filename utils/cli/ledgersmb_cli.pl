100aa2bac455c29c9e30c39f8de88aa638c1caa7
Chris Travers <chris.travers@gmail.com>
2006-11-01 05:11:07 +0000

    Adding copyright notice to cli script wrapper
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@448 4979c152-3d1c-0410-bac9-87ea11338e46

--- utils/cli/ledgersmb_cli.pl
+++ utils/cli/ledgersmb_cli.pl
@@ -1,5 +1,20 @@
 #!/usr/bin/perl
-
+#===========================================================================
+#
+# LedgerSMB Command-line script host
+#
+#
+# LedgerSMB 
+# Small Medium Business Accounting software
+# http://www.ledgersmb.org/
+# 
+#
+# Copyright (C) 2006
+# This work contains copyrighted information from a number of sources all used
+# with permission.  It is released under the GNU General Public License 
+# Version 2 or, at your option, any later version.  See COPYRIGHT file for 
+# details.
+#
 # This is a simple wrapper that allows you to write simple scripts with LSMB
 # See sample for the file format.
 
