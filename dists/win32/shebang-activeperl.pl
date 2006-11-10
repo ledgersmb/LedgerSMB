3376095204744579f8bd916a26451b3d5419a0ec
Chris Travers <chris.travers@gmail.com>
2006-11-10 17:46:46 +0000

    Added shebang scripts for Windows
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@503 4979c152-3d1c-0410-bac9-87ea11338e46

---
+++ dists/win32/shebang-activeperl.pl
@@ -0,0 +1,24 @@
+#!c:\perl\bin\perl
+# Use this script to convert the beginnings of files to the path to ActivePerl 
+# if you are installing with ActivePerl.
+
+  opendir DIR, ".";
+  @perlfiles = grep /\.pl/, readdir DIR;
+  closedir DIR;
+
+  foreach $file (@perlfiles) {
+    open FH, "+<$file";
+    
+    @file = <FH>;
+
+    seek(FH, 0, 0);
+    truncate(FH, 0);
+
+    $line = shift @file;
+
+    print FH "#!c:\\perl\\bin\\perl\n";
+    print FH @file;
+
+    close(FH);
+    
+  }
