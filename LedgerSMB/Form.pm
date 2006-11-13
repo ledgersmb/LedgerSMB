b62cbb3478ef0d563bb4b27bfe66aebd9c053373
Chris Travers <chris.travers@gmail.com>
2006-11-13 15:56:03 +0000

    Changing version numbers for Beta
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@566 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB/Form.pm
+++ LedgerSMB/Form.pm
@@ -70,7 +70,7 @@ sub new {
 	#menubar will be deprecated, replaced with below
 	$self->{lynx} = 1 if $self->{path} =~ /lynx/i;
 
-	$self->{version} = "1.1.1";
+	$self->{version} = "1.2.0 Beta 1";
 	$self->{dbversion} = "1.2.0";
 
 	bless $self, $type;
