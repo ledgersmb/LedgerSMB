8ed1d1d9f3c648b928bbae6573e696915ae6aaf1
tetragon <tetragon>
2006-12-07 23:25:10 +0000

    Fix date undefs for HR.pm
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.2@746 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB/HR.pm
+++ LedgerSMB/HR.pm
@@ -154,7 +154,7 @@ sub save_employee {
 		       managerid = ?
 		 WHERE id = ?|;
 	$sth = $dbh->prepare($query);
-	$form->db_prepare_vars('startdate', 'enddate');
+	$form->db_prepare_vars('dob', 'startdate', 'enddate');
 	$sth->execute(
 					$form->{employeenumber}, $form->{name}, $form->{address1},
 					$form->{address2}, $form->{city}, $form->{state},
