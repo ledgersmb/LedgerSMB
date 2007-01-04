212a79042805419c4c16f2c5b1712059299244b4
jasonjayr <jasonjayr>
2007-01-04 02:57:24 +0000

    Better message with instructions on how to enable the rest api.
    correted URLs
    
    
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@751 4979c152-3d1c-0410-bac9-87ea11338e46

--- rest.pl
+++ rest.pl
@@ -2,10 +2,11 @@
 
 use LedgerSMB::RESTXML::Handler;
 
-# To Enable the REST API,  Delete these 3 lines.
-
+# To Enable the REST API,  Delete these 5 lines.
 print "Content-type: text/plain\n\n";
-print "REST API disabled by default until authentication is working correctly";
+print "REST API disabled by default until authentication is working correctly\n";
+print "If you understand the security implications of this, and wish to enable the REST api\n";
+print "Then open rest.pl, and remove these 5 lines";
 exit;
 
 LedgerSMB::RESTXML::Handler->cgi_handle();
@@ -16,16 +17,16 @@ rest.pl - RESTful interface to LedgerSMB
 
 =head1 SUMMARY
 	
-	status
-	[OK] GET rest.pl/Customer/12345
-	[  ] GET rest.pl/Customer/CUSTOMERNUMBER
-	[OK] GET rest.pl/Customer_Search?_keyword=FOO
+	URLS that are working:
+	[OK] GET rest.pl/Login/Customer/12345
+	[  ] GET rest.pl/Login/Customer/CUSTOMERNUMBER
+	[OK] GET rest.pl/Login/Customer_Search?_keyword=FOO
 
-	[OK] GET rest.pl/Part/12345
-	[  ] GET rest.pl/Part/PARTNUMBER
-	[  ] GET rest.pl/Part_Search?_keyword=red
+	[OK] GET rest.pl/Login/Part/12345
+	[  ] GET rest.pl/Login/Part/PARTNUMBER
+	[  ] GET rest.pl/Login/Part_Search?_keyword=red
 
-	[  ] GET rest.pl/SalesOrder/12345
+	[  ] GET rest.pl/Login/SalesOrder/12345
 
 
 =cut
