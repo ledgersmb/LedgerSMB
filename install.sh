bb8d87fadbc584e51c8b072d88c4ab0ed4dc81b0
Chris Travers <chris.travers@gmail.com>
2007-02-06 07:32:45 +0000

    Changing to install script rather than default location.
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@799 4979c152-3d1c-0410-bac9-87ea11338e46

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+CWD=`pwd`
+
+sed -i "s|WORKING_DIR|$CWD|"
+
+echo "Which user does your web server run as?"
+read username
+
+chmod $username spool templates css
+
+echo "Where do we copy the ledger-smb-httpd.conf file to?"
+read location
+cp ledger-smb-httpd.conf $location
+
+echo "Please restart your web server for the changes to take effect."
