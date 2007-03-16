dad3930c0ff0ea5b6e6bf954e2d7ac97b4526ace
Chris Travers <chris.travers@gmail.com>
2007-03-16 02:05:28 +0000

    Adding Report.pm stub for stored procedure use via custom reports.
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@912 4979c152-3d1c-0410-bac9-87ea11338e46

---
+++ LedgerSMB/Report.pm
@@ -0,0 +1,8 @@
+=head1:  LedgerSMB::Report:  Stub function for custom reports.
+=head1:  Copyright (c) 2007.  LedgerSMB Core Team 
+
+=cut
+package LedgerSMB::Report;
+use base qw(LedgerSMB::DBObject);
+use strict;
+our $VERSION = '1.0.0';
