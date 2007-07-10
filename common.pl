2dc6f9765e6b34e1ee0b6f7ad34d8d6efb79a44b
Chris Travers <chris.travers@gmail.com>
2007-07-10 21:39:05 +0000

    Correcting error of losing ->{vc} on redirect
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.2@1375 4979c152-3d1c-0410-bac9-87ea11338e46

--- common.pl
+++ common.pl
@@ -26,7 +26,7 @@ sub redirect {
     my ( $script, $argv ) = split( /\?/, $form->{callback} );
 
     my @common_attrs = qw(
-      dbh login favicon stylesheet titlebar password custom_db_fields
+      dbh login favicon stylesheet titlebar password custom_db_fields vc
     );
 
     if ( !$script ) {    # http redirect to login.pl if called w/no args
