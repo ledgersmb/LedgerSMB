c174ff8969a25263d9fb0ea59f92ae1b3eccbfca
Chris Travers <chris.travers@gmail.com>
2007-10-07 06:07:42 +0000

    Adding login.js
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@1713 4979c152-3d1c-0410-bac9-87ea11338e46

---
+++ UI/login.js
@@ -0,0 +1,26 @@
+// Note: we do not heed to try other interfaces since we don't support IE 6 or
+// lower.  If we need to support other interfaces later, we can add them.
+// --CT
+function get_http_request_object(){
+	if (typeof XMLHttpRequest == undefined){
+		return false;
+	} else {
+		return new XMLHttpRequest();
+	}
+}
+
+function submit_form() {
+	var http = get_http_request_object();
+        var username = document.login.login.value;
+	var password = document.login.password.value;
+	http.open("get", 'login.pl?action=authenticate', false, 
+		username, password);
+	http.send("");
+	alert(http.status);
+        if (http.status != 200){
+		alert("Access Denied:  Bad username/Password");
+		return false;
+	}
+	document.location = document.login.action + "?action=login&company="+
+		documnet.login.company.value;
+}
