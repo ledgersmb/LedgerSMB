28d0619095735eedb730f1dea5aef97f1a21b5c4
tetragon <tetragon>
2007-11-05 19:14:39 +0000

    Add missing $parts_id to query in add_items_required
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.2@1853 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB/OE.pm
+++ LedgerSMB/OE.pm
@@ -2207,9 +2207,9 @@ sub add_items_required {
 			JOIN parts p ON (p.id = a.parts_id)
 			WHERE a.id = ?|;
         $sth = $dbh->prepare($query);
-        $sth->execute || $form->dberror($query);
+        $sth->execute($parts_id) || $form->dberror($query);
 
-        while ( $ref = $sth->fetchrow_hashref(NAME_lc) ) {
+        while ( $ref = $sth->fetchrow_hashref('NAME_lc') ) {
             $form->db_parse_numeric(sth=> $sth, hashref=> $ref);
             &add_items_required( "", $dbh, $form, $ref->{id},
                 $required * $ref->{qty},
