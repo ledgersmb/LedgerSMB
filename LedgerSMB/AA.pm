deb5ac5aa88e28fb9880880d5e6a4dfdf6d1fef9
Chris Travers <chris.travers@gmail.com>
2007-12-06 22:18:01 +0000

    Correcting errors in Outstanding reports.
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@1956 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB/AA.pm
+++ LedgerSMB/AA.pm
@@ -792,7 +792,7 @@ sub transactions {
 		     FROM $table a
 		     JOIN entity_credit_account vc USING (entity_id)
 		LEFT JOIN employee e ON (a.person_id = e.entity_id)
-		LEFT JOIN employee m ON (e.managerid = m.entity_id)
+		LEFT JOIN employee m ON (e.manager_id = m.entity_id)
 		     JOIN entity ee ON (e.entity_id = ee.id)
                 LEFT JOIN entity me ON (m.entity_id = me.id)
 		     JOIN entity vce ON (vc.entity_id = vce.id)
