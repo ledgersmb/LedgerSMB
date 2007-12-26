d6775b9138ec4c0ed9213abc7892471f9b46cc80
Chris Travers <chris.travers@gmail.com>
2007-12-26 16:55:01 +0000

    Applying David Mora's Patch to Location.sql
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@2002 4979c152-3d1c-0410-bac9-87ea11338e46

--- sql/modules/Location.sql
+++ sql/modules/Location.sql
@@ -61,7 +61,7 @@ $$ LANGUAGE PLPGSQL;
 
 COMMENT ON function location_save
 (in_companyname text, in_address1 text, in_address2 text, 
-	in_city text, in_state text, in_zipcode text, in_country text) IS
+	in_city text, in_state text, in_zipcode text, in_country int) IS
 $$ Note that this does NOT override the data in the database.
 Instead we search for locations matching the desired specifications and if none 
 are found, we insert one.  Either way, the return value of the location can be
