574fe910713949a2196e536585ba8dc5dfa3cb09
Chris Travers <chris.travers@gmail.com>
2008-05-12 18:43:21 +0000

    Adding fix for amount blank on AR/AP transaction report
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.2@2134 4979c152-3d1c-0410-bac9-87ea11338e46

---
+++ sql/fixes/amount_blank_ap_transactions.sql
@@ -0,0 +1,29 @@
+BEGIN;
+
+UPDATE ap 
+SET netamount = 
+	(select sum(amount) from acc_trans 
+	where trans_id = ap.id 
+		AND ((chart_id IN (select id from chart where link = 'AP')
+			     AND amount > 0)
+			OR (chart_id IN 
+				(select id from chart where link like '%tax%')
+			)
+		)
+	)
+WHERE netamount IS NULL;
+
+UPDATE ar
+SET netamount = -1 *
+	(select sum(amount) from acc_trans 
+	where trans_id = ap.id 
+		AND ((chart_id IN (select id from chart where link = 'AP')
+			     AND amount < 0)
+			OR (chart_id IN 
+				(select id from chart where link like '%tax%')
+			)
+		)
+	)
+WHERE netamount IS NULL;
+
+commit;
