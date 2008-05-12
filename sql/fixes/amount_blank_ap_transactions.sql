c5609765bbc26f6757f3085a26d769f7ef6d05c0
Chris Travers <chris.travers@gmail.com>
2008-05-12 18:57:17 +0000

    Final version of amount_blank_ap_transactions.sql fix
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.2@2135 4979c152-3d1c-0410-bac9-87ea11338e46

--- sql/fixes/amount_blank_ap_transactions.sql
+++ sql/fixes/amount_blank_ap_transactions.sql
@@ -16,8 +16,8 @@ WHERE netamount IS NULL;
 UPDATE ar
 SET netamount = -1 *
 	(select sum(amount) from acc_trans 
-	where trans_id = ap.id 
-		AND ((chart_id IN (select id from chart where link = 'AP')
+	where trans_id = ar.id 
+		AND ((chart_id IN (select id from chart where link = 'AR')
 			     AND amount < 0)
 			OR (chart_id IN 
 				(select id from chart where link like '%tax%')
