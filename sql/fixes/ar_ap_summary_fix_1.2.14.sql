a31f9331152fc1a1908a06a3cecc4cf3ad45e213
Chris Travers <chris.travers@gmail.com>
2008-05-14 21:40:07 +0000

    better approach to handling negative amounts on the fix.
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.2@2145 4979c152-3d1c-0410-bac9-87ea11338e46

--- sql/fixes/ar_ap_summary_fix_1.2.14.sql
+++ sql/fixes/ar_ap_summary_fix_1.2.14.sql
@@ -13,12 +13,11 @@ SET netamount =
 			)
 		)
 	ELSE
-		-1 *
 		(select sum(amount) from acc_trans 
 		where trans_id = ap.id 
 			AND ((chart_id IN 
 				(select id from chart where link = 'AP')
-				     AND amount > 0)
+				     AND amount < 0)
 				OR (chart_id IN 
 					(select id from chart 
 					where link like '%tax%')
@@ -44,12 +43,12 @@ SET netamount =
 				)
 			)
 		)
-	ELSE 
+	ELSE  -1 *
 		(select sum(amount) from acc_trans 
 		where trans_id = ar.id 
 			AND ((chart_id IN (select id from chart 
 				where link = 'AR')
-				     AND amount < 0)
+				     AND amount > 0)
 			OR (chart_id IN 
 				(select id from chart where link like '%tax%')
 				)
