d225824c275813916c6d6d3d8937b802a84bfa21
linuxpoet <jd@commandprompt.com>
2008-06-06 23:40:20 +0000

    add apt-get list for ubuntu
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@2159 4979c152-3d1c-0410-bac9-87ea11338e46

---
+++ dists/deb/ubuntu/ubuntu.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is not complete but it is a start
+#
+apt-get install libmime-perl
+apt-get install texlive
+apt-get install libtest-exception-perl
+apt-get install libmime-lite-perl
+apt-get install libconfig-std-perl 
+apt-get install libtemplate-perl
+apt-get install liberror-perl
+apt-get install libcgi-simple-perl
+apt-get install libtest-exception-pe
+apt-get install liblocale-maketext-lexicon-perl
