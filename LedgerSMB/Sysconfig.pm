b383b7aa6997bd19d7f3a92dc26638f8980f2bdc
Chris Travers <chris.travers@gmail.com>
2008-07-10 19:44:01 +0000

    commenting on defaults, resetting max_post_size default to one megabyte
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@2209 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB/Sysconfig.pm
+++ LedgerSMB/Sysconfig.pm
@@ -34,8 +34,10 @@ $force_username_case = undef; # don't force case
 # if you have latex installed set to 1
 $latex = 1;
 
-$max_post_size = 1000000;
+# Defaults to 1 megabyte
+$max_post_size = 1024 * 1024;
 
+# defaults to 2
 $decimal_places = 2;
 # spool directory for batch printing
 $spool = "spool";
