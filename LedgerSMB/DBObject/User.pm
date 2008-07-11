01f3399a9ec0d5510f64c950ad6ba8c7722e9acd
aurynn_cmd <ashaw@commandprompt.com>
2008-07-11 20:20:58 +0000

    Re-adding my admin-specific stuff.
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@2213 4979c152-3d1c-0410-bac9-87ea11338e46

---
+++ LedgerSMB/DBObject/User.pm
@@ -0,0 +1,80 @@
+package LedgerSMB::DBObject::User;
+
+use base qw/LedgerSMB::DBObject/;
+
+sub save {
+    
+    my $self = shift @_;
+    
+    my $user = $self->get();
+    
+    if ( $user->{id} && $self->{is_a_user} ) {
+    
+        # doesn't check for the password - that's done in the sproc.
+        $self->{id} = shift @{ $self->exec_method(procname=>'admin__save_user', 
+            args=>[$user->{id}, $self->{username}, $self->{password}] ) }; 
+        if (!$self->{id}) {
+            
+            return 0;
+        }
+    }
+    elsif ($user && !$self->{is_a_user}) {
+        
+        # there WAS a user, and we've decided we want that user to go away now.
+        
+        $self->{id} = $user->{id};
+        return $self->remove();
+        
+    }
+    elsif ($self->{is_a_user}) {
+        
+        # No user ID, meaning, creating a new one.        
+        $self->{id} = shift @{ $self->exec_method(procname=>'admin__save_user', 
+            args=>[undef, $self->{username}, $self->{password}] ) };
+    }
+    return 1;
+}
+
+sub get {
+    
+    my $self = shift @_;
+    
+    my ($user_id, $username) = @{ $self->exec_method(procname=>'admin__get_user',
+        args=>[$self->{id}])};
+        
+    return {id=>$user_id, username=>$username};
+}
+
+sub remove {
+    
+    my $self = shift;
+    
+    my $code = $self->exec_method(procname=>"admin__delete_user", args=>[$self->{id}, $self->{username}]);
+    $self->{id} = undef; # never existed..
+    
+    return $code->[0];
+}
+
+sub save_prefs {
+    
+    my $self = shift @_; 
+    
+    my $pref_id = $self->exec_method(procname=>"admin__save_preferences", 
+        args=>[
+            'language',
+            'stylesheet',
+            'printer',
+            'dateformat',
+            'numberformat'
+        ]
+    );
+}
+
+sub get_all_users {
+    
+    my $self = shift @_;
+    
+    $self->{users} = $self->exec_method( procname=>"user__get_all_users" );
+}
+
+1;
---
+++ LedgerSMB/Employee.pm
@@ -0,0 +1,91 @@
+
+=head1 NAME
+
+LedgerSMB::Employee - LedgerSMB class for managing Employees 
+
+=head1 SYOPSIS
+
+This module creates object instances based on LedgerSMB's in-database ORM.  
+
+=head1 METHODS
+
+The following method is static:
+
+=over
+
+=item new ($LedgerSMB object);
+
+=back
+
+The following methods are passed through to stored procedures via Autoload.
+
+=over
+
+=item save
+
+=item get
+
+=item search
+
+=item list_managers
+
+The above list may grow over time, and may depend on other installed modules.
+
+=back
+
+=head1 Copyright (C) 2007, The LedgerSMB core team.
+
+This file is licensed under the Gnu General Public License version 2, or at your
+option any later version.  A copy of the license should have been included with
+your software.
+
+=cut
+
+package LedgerSMB::Employee;
+use base qw(LedgerSMB::DBObject);
+use strict;
+our $VERSION = '1.0.0';
+
+sub save {
+    my $self = shift;
+        
+    my $hashref = shift @{ $self->exec_method( procname => "employee_save" ) };
+    $self->merge( $hashref, 'id' );
+}
+
+sub get {
+    my $self = shift;
+    my $hashref = shift @{ $self->exec_method( procname => "employee_get" ) };
+    $self->merge( $hashref, keys %{$hashref} );
+}
+
+sub list_managers {
+    my $self = shift;
+    $self->{manager_list} =
+      $self->exec_method( procname => "employee_list_managers" );
+}
+
+sub search {
+    my $self = shift;
+    $self->{search_results} =
+      $self->exec_method( procname => "employee_search" );
+}
+
+sub set_location {
+    
+    my $self = shift @_;
+    my $location = shift @_;
+    
+    my $code = $self->exec_method ( procname => 'employee_set_location', 
+        args=>[ $self->{id}, $location->{id} ] );
+    
+    if ($code) {
+        
+        # good, it worked.
+        
+        return 1;        
+    }
+    return 0;
+}
+
+1;
