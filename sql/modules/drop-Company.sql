3cf3c009554081d46ca550d3d7b5d1199233aecc
Chris Travers <chris.travers@gmail.com>
2008-08-06 23:08:18 +0000

    Adding script to drop company data types.
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@2236 4979c152-3d1c-0410-bac9-87ea11338e46

---
+++ sql/modules/drop-Company.sql
@@ -0,0 +1,15 @@
+-- Only dropping types for now.  May want to add functions later.
+
+DROP TYPE company_search_result CASCADE;
+
+DROP TYPE entity_credit_search_return CASCADE;
+
+DROP TYPE entity_credit_retrieve CASCADE;
+
+DROP TYPE company_billing_info CASCADE;
+
+DROP TYPE contact_list CASCADE;
+
+DROP TYPE  entity_note_list CASCADE;
+
+
