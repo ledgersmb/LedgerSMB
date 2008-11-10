0747e7730b749936e2657e2a9699ca78f4de2f57
Chris Travers <chris.travers@gmail.com>
2008-11-10 22:33:57 +0000

    Test commit, given suspicion of missing test cases.
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@2386 4979c152-3d1c-0410-bac9-87ea11338e46

--- sql/modules/test/Company.sql
+++ sql/modules/test/Company.sql
@@ -43,6 +43,7 @@ WHERE contact = 'test_c';
 
 SELECT * FROM test_result;
 
+
 SELECT (select count(*) from test_result where success is true) 
 || ' tests passed and ' 
 || (select count(*) from test_result where success is not true) 
