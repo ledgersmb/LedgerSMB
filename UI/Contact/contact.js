5d633d9e5d0348b8e50f4f20afa7bfaaeb4c5bc7
Chris Travers <chris.travers@gmail.com>
2009-03-15 00:34:39 +0000

    Move to Javascript-based form hiding/showing of portions of the contact management bit
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@2501 4979c152-3d1c-0410-bac9-87ea11338e46

---
+++ UI/Contact/contact.js
@@ -0,0 +1,45 @@
+
+function init() {
+	var lis = document.getElementsByTagName('li');
+	for (var e in lis){
+		if (e != e * 1){
+			continue;
+                }
+		e = lis.item(e);
+		if (e.getAttribute('class') == "nav"){
+			e.addEventListener('click', function (e) {
+				for (var a in this.getElementsByTagName('a')){
+					if (a != a * 1){
+						continue;
+					}
+					a = this.getElementsByTagName('a').item(a);
+					var dest = a.getAttribute('href');
+					dest = dest.replace('#', '');
+					select_div(dest);
+					break;
+				}
+				return false;
+			},false);
+		}
+	}
+}
+
+function select_div(div_id){
+	var divs = document.getElementsByTagName('div');
+	var i = 0;
+	for (i=0;i<=divs.length;i++){
+		var e = divs.item(i);
+		if (!e || !e.getAttribute || !e.getAttribute('class')){
+			continue;
+		}
+		if (e.getAttribute('class').match(/^container/)){
+			if (e.getAttribute('id') == div_id){
+				e.className = 'container';
+			}
+			else {
+				e.className = 'container_hidden';
+			}
+		}
+	}
+}
+
