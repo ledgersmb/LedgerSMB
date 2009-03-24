25a91a721b99ae62021fbd573015a4c85f45c98a
Chris Travers <chris.travers@gmail.com>
2009-03-24 20:52:20 +0000

    Payment javascript fixes
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@2528 4979c152-3d1c-0410-bac9-87ea11338e46

--- UI/payments/payments_detail.js
+++ UI/payments/payments_detail.js
@@ -1,67 +1,52 @@
 
 function init() {
-	var spans = document.getElementsByTagName('span');
-	var tables = document.getElementsByTagName('table');
-	for (var i=0, il=tables.length; i<il; i++){
-		var table = tables.item(i);
-		if (table.getAttribute('class') == 'detail_table_visible'){
-			var tid = table.getAttribute('id');
-			var cid = tid.slice(19, tid.length);
-			var rg = document.getElementsByName('paid_' + cid);
-			for (var i=0, il=rg.length; i<il; i++){
-				var button = rg.item(i);
-				if ((button.value == 'all') && 
-						(button.checked == true)){
-					table.className = 'detail_table_hidden';
-				}
-			}
+	var cc = document.getElementById('contact-count').value;
+	for (var i=1; i <= cc; i++){
+		var paid = '';
+		var cid = document.getElementById('contact-' + i).value;
+		var radio = document.getElementById('paid-all-' + cid);
+		if (radio.checked == true){
+			paid = 'all';
+		} else {
+			paid = 'some';
 		}
-	}
-	for (var i=0, il=spans.length; i<il; i++){
-		var span = spans.item(i);
-		if (span.getAttribute('class') != 'details_select'){
-			continue;
-		}
-		var inputs = span.getElementsByTagName('input');
-		for (var i=0, il=inputs.length; i<il; i++){
-			var input = inputs.item(i);
-			if (input.getAttribute('class') == 'paid_some'){
-				input.addEventListener('click', 
-					function(e){
-						var my_id = 
-							this.getAttribute('id');
-						var contact_id = 
-							my_id.slice(10, 
-								my_id.length);
-						show_details(contact_id);
-						return true;
-					}, false);
-			}
-			else if (input.getAttribute('class') == 'paid_all'){
-				input.addEventListener('click', 
-					function(e){
-						var my_id = 
-							this.getAttribute('id');
-						var contact_id = 
-							my_id.slice(9, 
-								my_id.length);
-						hide_details(contact_id);
-						return true;
-					}, false);
-			}
+		radio.addEventListener('click', 
+			function(e){
+				var my_id = 
+					this.getAttribute('id');
+					var contact_id = 
+						my_id.slice(9, 
+							my_id.length);
+					hide_details(contact_id);
+					return true;
+				}, false);
+		radio = document.getElementById('paid-some-' + cid);
+		radio.addEventListener('click', 
+			function(e){
+				var my_id = 
+					this.getAttribute('id');
+					var contact_id = 
+						my_id.slice(10, 
+							my_id.length);
+					show_details(contact_id);
+					return true;
+				}, false);
+		var table = document.getElementById('invoice-data-table-' + cid);
+		if (paid == 'all'){
+			table.className = 'detail_table_hidden';
 		}
 	}
 }
 
 function show_details(contact_id){
-	var e_id = "invoice_data_table_" + contact_id;
+	var e_id = "invoice-data-table-" + contact_id;
 	var e = document.getElementById(e_id);
 	e.className = "detail_table_visible";
 	return true;
 }
 
 function hide_details(contact_id){
-	var e_id = "invoice_data_table_" + contact_id;
+	var e_id = "invoice-data-table-" + contact_id;
 	var e = document.getElementById(e_id);
 	e.className = "detail_table_hidden";
 	return true;
