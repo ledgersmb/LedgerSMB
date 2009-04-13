63a85207f1d921cdd756b89ab533acb414aff700
Chris Travers <chris.travers@gmail.com>
2009-04-13 05:03:26 +0000

    Merging in closedto/cogs handling bugs
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@2550 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB/IR.pm
+++ LedgerSMB/IR.pm
@@ -425,10 +425,10 @@ sub post_invoice {
 				            invoice_id,
 				            transdate) 
 				     VALUES (?, ?, ?, ?,
-				            ?, (SELECT CASE WHEN ? > value::date
-				                            THEN ?
-				                            ELSE value::date +
+				            ?, (SELECT CASE WHEN ? <= value::date
+				                            THEN value::date +
 				                               '1 day'::interval
+				                            ELSE ?
 				                        END AS value 
 				                  FROM defaults
 				                  WHERE setting_key = 'closedto'
@@ -449,10 +449,10 @@ sub post_invoice {
 				            project_id, invoice_id,
 				            transdate) 
 				     VALUES (?, ?, ?, ?,
-				            ?, (SELECT CASE WHEN ? > value::date
-				                            THEN ?
-				                            ELSE value::date +
+				            ?, (SELECT CASE WHEN ? <= value::date
+				                            THEN value::date +
 				                               '1 day'::interval
+				                            ELSE ?
 				                        END AS value 
 				                  FROM defaults
 				                  WHERE setting_key = 'closedto'
