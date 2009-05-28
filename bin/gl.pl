7b23bd48b19facda4eaf8cd9c43e4bfb23534f7a
Chris Travers <chris.travers@gmail.com>
2009-05-28 16:18:40 +0000

    ajax gl report fixes
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@2614 4979c152-3d1c-0410-bac9-87ea11338e46

--- bin/gl.pl
+++ bin/gl.pl
@@ -288,7 +288,7 @@ sub search {
 
 sub generate_report {
     my $output_options = shift;
-
+    ($form->{accno}) = split /--/, $form->{account};
     $form->{sort} = "transdate" unless $form->{sort};
     $form->{amountfrom} = $form->parse_amount(\%myconfig, $form->{amountfrom});
     $form->{amountto} = $form->parse_amount(\%myconfig, $form->{amountto});
