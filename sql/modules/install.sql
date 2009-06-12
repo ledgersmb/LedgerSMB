e1cad148f0d773b6e2ff1e7c11ac74689f4b8923
aurynn_cmd <ashaw@commandprompt.com>
2009-06-12 21:00:20 +0000

    Adds a basic file load SQL, that should correctly load all modules in the required order.
    Does not attempt to load the Roles.sql file.
    
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@2669 4979c152-3d1c-0410-bac9-87ea11338e46

---
+++ sql/modules/install.sql
@@ -0,0 +1,23 @@
+BEGIN;
+\i Drafts.sql
+\i Chart.sql
+\i Account.sql
+\i Session.sql
+\i Business_type.sql
+\i Location.sql
+\i Company.sql
+\i Customer.sql 
+\i Date.sql
+\i Defaults.sql
+\i Settings.sql
+\i Employee.sql
+\i Entity.sql
+\i Payment.sql
+\i Person.sql
+\i Report.sql
+\i Voucher.sql
+\i Reconciliation.sql
+\i admin.sql -- Adds the new user functionality.
+
+
+COMMIT;
