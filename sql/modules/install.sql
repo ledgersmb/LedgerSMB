aeec7f3f548982a9744856a0ad3e8ad39b6fffdc
Jeff Kowalczyk <jtk@yahoo.com>
2009-06-12 21:48:35 +0000

    sql/modules/install.sql: make module paths relative to LedgerSMB root directory.
    For reuse by the test script install-mycompany and future installers, prefix the
    lines of sql/modules/install.sql with the path to those modules: sql/modules.
    
    The script can now be run without changing to the modules directory first. Of
    course, the same script can't be run in the modules directory now, either.
    
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@2673 4979c152-3d1c-0410-bac9-87ea11338e46

--- sql/modules/install.sql
+++ sql/modules/install.sql
@@ -1,23 +1,23 @@
 BEGIN;
-\i Drafts.sql
-\i Chart.sql
-\i Account.sql
-\i Session.sql
-\i Business_type.sql
-\i Location.sql
-\i Company.sql
-\i Customer.sql 
-\i Date.sql
-\i Defaults.sql
-\i Settings.sql
-\i Employee.sql
-\i Entity.sql
-\i Payment.sql
-\i Person.sql
-\i Report.sql
-\i Voucher.sql
-\i Reconciliation.sql
-\i admin.sql -- Adds the new user functionality.
+\i sql/modules/Drafts.sql
+\i sql/modules/Chart.sql
+\i sql/modules/Account.sql
+\i sql/modules/Session.sql
+\i sql/modules/Business_type.sql
+\i sql/modules/Location.sql
+\i sql/modules/Company.sql
+\i sql/modules/Customer.sql 
+\i sql/modules/Date.sql
+\i sql/modules/Defaults.sql
+\i sql/modules/Settings.sql
+\i sql/modules/Employee.sql
+\i sql/modules/Entity.sql
+\i sql/modules/Payment.sql
+\i sql/modules/Person.sql
+\i sql/modules/Report.sql
+\i sql/modules/Voucher.sql
+\i sql/modules/Reconciliation.sql
+\i sql/modules/admin.sql -- Adds the new user functionality.
 
 
 COMMIT;
