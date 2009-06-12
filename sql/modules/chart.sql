75e2a9c9e3624c222f0f82382ff2a90ac0a4d883
Chris Travers <chris.travers@gmail.com>
2009-06-12 18:48:59 +0000

    Correcting lost function in chart.sql
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@2662 4979c152-3d1c-0410-bac9-87ea11338e46

--- sql/modules/chart.sql
+++ sql/modules/chart.sql
@@ -139,5 +139,26 @@ BEGIN
 END;
 $$ language plpgsql;
 
+CREATE OR REPLACE FUNCTION chart_list_cash(in_account_class int)
+returns setof chart
+as $$
+ DECLARE resultrow record;
+         link_string text;
+ BEGIN
+         IF in_account_class = 1 THEN
+            link_string := '%AP_paid%';
+         ELSE 
+            link_string := '%AR_paid%';
+         END IF;
+ 
+         FOR resultrow IN
+           SELECT *  FROM chart
+           WHERE link LIKE link_string
+           ORDER BY accno
+           LOOP
+           return next resultrow;
+         END LOOP;
+ END;
+$$ language plpgsql;
 COMMENT ON FUNCTION chart_list_cash(in_account_class int) IS
 $$ This function returns the overpayment accounts acording with in_account_class which must be 1 or 2 $$;
