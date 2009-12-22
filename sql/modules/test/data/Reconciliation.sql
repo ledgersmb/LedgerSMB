83e3e14882d4d0355219382deac2e9c966e7f34a
aurynn_cmd <ashaw@commandprompt.com>
2009-12-22 01:19:55 +0000

    Updates to the test cases to add a currency value to the AR inserts.
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@2832 4979c152-3d1c-0410-bac9-87ea11338e46

--- sql/modules/test/data/Reconciliation.sql
+++ sql/modules/test/data/Reconciliation.sql
@@ -9,15 +9,15 @@ INSERT INTO entity_credit_account (entity_id, id, meta_number, entity_class, ar_
 INSERT INTO entity_credit_account (entity_id, id, meta_number, entity_class, ar_ap_account_id) values (-200, -201, 'T-11112', 1, -1000);
 
 
-INSERT INTO ar (id, invnumber, amount, netamount, paid, entity_credit_account, transdate) values (-200, '-2000', '10', '10', '0', -200, '1000-01-01');
-INSERT INTO ar (id, invnumber, amount, netamount, paid, entity_credit_account, transdate) values (-201, '-2001', '10', '10', '0', -200, '1000-01-03');
-INSERT INTO ar (id, invnumber, amount, netamount, paid, entity_credit_account, transdate) values (-204, '-2002', '10', '10', '0', -200, '1000-01-01');
-INSERT INTO ar (id, invnumber, amount, netamount, paid, entity_credit_account, transdate) values (-205, '-2003', '10', '10', '0', -200, '1000-01-03');
+INSERT INTO ar (id, invnumber, amount, netamount, paid, entity_credit_account, transdate, curr) values (-200, '-2000', '10', '10', '0', -200, '1000-01-01', 'USD');
+INSERT INTO ar (id, invnumber, amount, netamount, paid, entity_credit_account, transdate, curr) values (-201, '-2001', '10', '10', '0', -200, '1000-01-03', 'USD');
+INSERT INTO ar (id, invnumber, amount, netamount, paid, entity_credit_account, transdate, curr) values (-204, '-2002', '10', '10', '0', -200, '1000-01-01', 'USD');
+INSERT INTO ar (id, invnumber, amount, netamount, paid, entity_credit_account, transdate, curr) values (-205, '-2003', '10', '10', '0', -200, '1000-01-03', 'USD');
 
-INSERT INTO ar (id, invnumber, amount, netamount, paid, entity_credit_account, transdate) values (-206, '-2004', '10', '10', '0', -201, '1000-01-01');
-INSERT INTO ar (id, invnumber, amount, netamount, paid, entity_credit_account, transdate) values (-207, '-2005', '10', '10', '0', -201, '1000-01-03');
-INSERT INTO ar (id, invnumber, amount, netamount, paid, entity_credit_account, transdate) values (-208, '-2006', '10', '10', '0', -201, '1000-01-01');
-INSERT INTO ar (id, invnumber, amount, netamount, paid, entity_credit_account, transdate) values (-209, '-2007', '10', '10', '0', -201, '1000-01-03');
+INSERT INTO ar (id, invnumber, amount, netamount, paid, entity_credit_account, transdate, curr) values (-206, '-2004', '10', '10', '0', -201, '1000-01-01', 'USD');
+INSERT INTO ar (id, invnumber, amount, netamount, paid, entity_credit_account, transdate, curr) values (-207, '-2005', '10', '10', '0', -201, '1000-01-03', 'USD');
+INSERT INTO ar (id, invnumber, amount, netamount, paid, entity_credit_account, transdate, curr) values (-208, '-2006', '10', '10', '0', -201, '1000-01-01', 'USD');
+INSERT INTO ar (id, invnumber, amount, netamount, paid, entity_credit_account, transdate, curr) values (-209, '-2007', '10', '10', '0', -201, '1000-01-03', 'USD');
 
 INSERT INTO gl (id, reference, transdate) values (-202, 'Recon gl test 1', '1000-01-01');
 INSERT INTO gl (id, reference, transdate) values (-203, 'Recon gl test 2', '1000-01-01');
