283536da21a9396c57a30310717dc6889019758d
Chris Travers <chris.travers@gmail.com>
2010-03-10 05:33:00 +0000

    Fixing SIC update bug reported by Adam Thompson
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.2@2955 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB/AM.pm
+++ LedgerSMB/AM.pm
@@ -798,7 +798,7 @@ sub save_sic {
 			   SET code = ?,
 			       sictype = ?,
 			       description = ?
-			 WHERE code = ?)|;
+			 WHERE code = ?|;
         push @queryargs, $form->{id};
 
     }
