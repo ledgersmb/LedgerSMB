6c1b5e2f86b5dbb4558e16ef37cc37d587f0c671
Chris Travers <chris.travers@gmail.com>
2011-06-16 01:17:35 +0000

    Adding new role for perms for editing orders
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@3259 4979c152-3d1c-0410-bac9-87ea11338e46

--- sql/modules/Roles.sql
+++ sql/modules/Roles.sql
@@ -273,6 +273,7 @@ values (51, 'allow', 'lsmb_<?lsmb dbname ?>__sales_order_create');
 
 CREATE ROLE "lsmb_<?lsmb dbname ?>__sales_order_edit";
 GRANT DELETE ON orderitems TO "lsmb_<?lsmb dbname ?>__sales_order_edit";
+GRANT DELETE ON new_shipto TO "lsmb_<?lsmb dbname ?>__sales_order_edit";
 
 CREATE ROLE "lsmb_<?lsmb dbname ?>__sales_quotation_create"
 WITH INHERIT NOLOGIN
@@ -440,6 +441,7 @@ GRANT ALL on inventory TO "lsmb_<?lsmb dbname ?>__purchase_order_create";
 
 CREATE ROLE "lsmb_<?lsmb dbname ?>__purchase_order_edit";
 GRANT DELETE ON orderitems TO "lsmb_<?lsmb dbname ?>__purchase_order_edit";
+GRANT DELETE ON new_shipto TO "lsmb_<?lsmb dbname ?>__purchase_order_edit";
 
 INSERT INTO menu_acl (node_id, acl_type, role_name)
 values (50, 'allow', 'lsmb_<?lsmb dbname ?>__purchase_order_create');
