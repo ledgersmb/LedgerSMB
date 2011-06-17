bd5ab7d8b848b4bcff6015267052418e10d3b98c
Erik Huelsmann <ehuels@gmail.com>
2011-06-17 08:28:11 +0000

    * tools/detele-company-database.sh: Patch submitted by Luke, adding usage description.
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@3268 4979c152-3d1c-0410-bac9-87ea11338e46

--- tools/delete-company-database.sh
+++ tools/delete-company-database.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# Simple check to print usage, and prevent runaway deletion
+if test -z "$1"
+then
+  echo "Usage: $0 COMPANY [ USER ]"
+  echo "\"COMPANY\" is the name of the company database, such as the name given"
+  echo "to --company when using the prepare-company-database.sh script."
+  echo "\"USER\" is an optional PGSQL user (owner of the database) to remove."
+  exit 1
+fi
+
 cmd="select rolname FROM pg_roles WHERE rolname LIKE 'lsmb_${1}__%';"
 company_roles=`su -c "psql -U postgres -t -c \"$cmd\"" postgres`
 
