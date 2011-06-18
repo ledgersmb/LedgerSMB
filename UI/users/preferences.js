3383027464c1970da0e1d9272485112d7b586736
Chris Travers <chris.travers@gmail.com>
2011-06-18 00:40:24 +0000

    password changing doesn't reuire the http auth box to pop up if Javascript is enabled
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@3278 4979c152-3d1c-0410-bac9-87ea11338e46

--- UI/users/preferences.js
+++ UI/users/preferences.js
@@ -1,10 +1,10 @@
 
 function submit_form() {
 	var http = get_http_request_object();
-	var login = document.pref.username.value;
-	var old_password = document.pref.old_password.value;
-	var new_password = document.pref.new_password.value;
-	var confirm_pass = document.pref.confirm_password.value;
+	var login = document.prefs.username.value;
+	var old_password = document.prefs.old_password.value;
+	var new_password = document.prefs.new_password.value;
+	var confirm_pass = document.prefs.confirm_password.value;
 	http.open("get", 'user.pl?action=change_password' +
                           '&old_password='+old_password+
                           '&new_password='+new_password+
