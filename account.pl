57945a64828a7048d2b704abf4f926f359c8f7ec
Erik Huelsmann <ehuels@gmail.com>
2011-06-20 20:55:19 +0000

    Add support for suEXEC setups as reported by mst on #ledgersmb.
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@3304 4979c152-3d1c-0410-bac9-87ea11338e46

--- account.pl
+++ account.pl
@@ -1,5 +1,10 @@
 #!/usr/bin/perl
 
+use FindBin;
+BEGIN {
+  lib->import($FindBin::Bin) unless $ENV{mod_perl}
+}
+
 require "lsmb-request.pl";
 #!/usr/bin/perl
 
--- pe.pl
+++ pe.pl
@@ -1,2 +1,7 @@
 #!/usr/bin/perl
+
+use FindBin;
+BEGIN {
+  lib->import($FindBin::Bin) unless $ENV{mod_perl}
+}
 require "old-handler.pl";
