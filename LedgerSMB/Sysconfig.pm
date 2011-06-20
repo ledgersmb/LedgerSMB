f60434eabfdcbeb0f98e3c57990eb7198e1e1302
Chris Travers <chris.travers@gmail.com>
2011-06-20 14:19:19 +0000

    Removing unused scripts from Sysconfig whitelist
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@3303 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB/Sysconfig.pm
+++ LedgerSMB/Sysconfig.pm
@@ -29,8 +29,8 @@ our @io_lineitem_columns = qw(unit onhand sellprice discount linetotal);
 our @scripts = (
     'aa.pl', 'admin.pl', 'am.pl',      'ap.pl',
     'ar.pl', 'arap.pl',  'arapprn.pl', 'bp.pl',
-    'ca.pl', 'cp.pl',    'ct.pl',      'gl.pl',
-    'hr.pl', 'ic.pl',    'io.pl',      'ir.pl',
+    'ca.pl', 'gl.pl',
+    'ic.pl',    'io.pl',      'ir.pl',
     'is.pl', 'jc.pl',    'login.pl',   'menu.pl',
     'oe.pl', 'pe.pl',    'pos.pl',     'ps.pl',
     'pw.pl', 'rc.pl',    'rp.pl', 	'initiate.pl'
