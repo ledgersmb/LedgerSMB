d0b72ad8101990f538658879d9d2910fd2c03d65
Chris Travers <chris.travers@gmail.com>
2011-06-22 13:47:09 +0000

    Removing need for preprocess on role update file since this just updates menu acls.
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@3314 4979c152-3d1c-0410-bac9-87ea11338e46

--- sql/upgrade/3313-role-updates.sql
+++ sql/upgrade/3313-role-updates.sql
@@ -1,4 +1,4 @@
 
 INSERT INTO menu_acl (node_id, acl_type, role_name)
-SELECT id, 'allow', 'lsmb_<?lsmb dbname ?>__tax_form_save'
-  FROM menu_node WHERE parent_id = 217 and position in (2,3);
+SELECT id, 'allow', 'lsmb_' || current_database() ||'__tax_form_save'
+  FROM menu_node WHERE parent = 217 and position in (2,3);
