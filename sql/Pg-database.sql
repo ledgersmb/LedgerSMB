bb6037ee83fae108fcb5afea17b477f5d56efe2c
Chris Travers <chris.travers@gmail.com>
2011-06-24 13:56:54 +0000

    Correcting in-sproc sql injection issues in custom field management routines, not believed to be exploitable because these routines are only occasionally run by knowledgeable administrators from general-purpose database consoles and so cannot do anything the administrator could not already do.
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.2@3322 4979c152-3d1c-0410-bac9-87ea11338e46

--- sql/Pg-database.sql
+++ sql/Pg-database.sql
@@ -1096,23 +1096,26 @@ new_field_name ALIAS FOR $2;
 field_datatype ALIAS FOR $3;
 
 BEGIN
-	EXECUTE ''SELECT TABLE_ID FROM custom_table_catalog 
-		WHERE extends = '''''' || table_name || '''''' '';
+	perform TABLE_ID FROM custom_table_catalog 
+		WHERE extends = table_name;
 	IF NOT FOUND THEN
 		BEGIN
 			INSERT INTO custom_table_catalog (extends) 
 				VALUES (table_name);
-			EXECUTE ''CREATE TABLE custom_''||table_name || 
+			EXECUTE ''CREATE TABLE '' || 
+                               quote_ident(''custom_'' ||table_name) ||
 				'' (row_id INT PRIMARY KEY)'';
 		EXCEPTION WHEN duplicate_table THEN
 			-- do nothing
 		END;
 	END IF;
-	EXECUTE ''INSERT INTO custom_field_catalog (field_name, table_id)
-	VALUES ( '''''' || new_field_name ||'''''', (SELECT table_id FROM custom_table_catalog
-		WHERE extends = ''''''|| table_name || ''''''))'';
-	EXECUTE ''ALTER TABLE custom_''||table_name || '' ADD COLUMN '' 
-		|| new_field_name || '' '' || field_datatype;
+	INSERT INTO custom_field_catalog (field_name, table_id)
+	values (new_field_name, (SELECT table_id 
+                                        FROM custom_table_catalog
+		WHERE extends = table_name));
+	EXECUTE ''ALTER TABLE ''|| quote_ident(''custom_''||table_name) || 
+                '' ADD COLUMN '' || quote_ident(new_field_name) || '' '' || 
+                  quote_ident(field_datatype);
 	RETURN TRUE;
 END;
 ' LANGUAGE PLPGSQL;
@@ -1129,8 +1132,8 @@ BEGIN
 	WHERE field_name = custom_field_name AND 
 		table_id = (SELECT table_id FROM custom_table_catalog 
 			WHERE extends = table_name);
-	EXECUTE ''ALTER TABLE custom_'' || table_name || 
-		'' DROP COLUMN '' || custom_field_name;
+	EXECUTE ''ALTER TABLE '' || quote_ident(''custom_'' || table_name) || 
+		'' DROP COLUMN '' || quote_ident(custom_field_name);
 	RETURN TRUE;	
 END;
 ' LANGUAGE PLPGSQL;
