1e8c12b8bcdf55b956247e20f17d8d5a58fc27cc
Chris Travers <chris.travers@gmail.com>
2011-06-25 11:03:33 +0000

    Bank account saving no longer duplicates, upgrade script
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@3332 4979c152-3d1c-0410-bac9-87ea11338e46

---
+++ sql/upgrade/3331-drop-bank-acct-save.sql
@@ -0,0 +1,6 @@
+DROP FUNCTION eca__save_bank_account
+(in_entity_id int, in_credit_id int, in_bic text, in_iban text);
+
+DROP FUNCTION entity__save_bank_account 
+(in_entity_id int, in_bic text, in_iban text);
+
