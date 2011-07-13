a6dc9b7482caf05f1403f6482842805f832d76c7
Chris Travers <chris.travers@gmail.com>
2011-07-13 19:08:37 +0000

    Correcting schema update script
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@3500 4979c152-3d1c-0410-bac9-87ea11338e46

--- sql/upgrade/3497-schema-changes.sql
+++ sql/upgrade/3497-schema-changes.sql
@@ -1,6 +1,8 @@
-ALTER TABLE invoice ADD FOREIGN KEY trans_id REFERENCES transactions(id);
-ALTER TABLE invoice ADD FOREIGN KEY parts_id REFERENCES parts(id);
+ALTER TABLE invoice ADD FOREIGN KEY (trans_id) REFERENCES transactions(id);
+ALTER TABLE invoice ADD FOREIGN KEY (parts_id) REFERENCES parts(id);
 
-ALTER TABLE tax ADD FOREIGN KEY chart_id REFERENCES account(id);
-CREATE TRIGGER ap_audit_trail AFTER insert or update or delete ON ap;
+ALTER TABLE tax ADD FOREIGN KEY (chart_id) REFERENCES account(id);
+CREATE TRIGGER ap_audit_trail AFTER insert or update or delete ON ap
+FOR EACH ROW EXECUTE PROCEDURE gl_audit_trail_append();
+
 
