96e8e885c0e8a97378500b8b0461b9aeea355ba3
Chris Travers <chris.travers@gmail.com>
2011-07-14 21:04:23 +0000

    Doc strings for Customer.pm and Vendor.pm
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@3511 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB/DBObject/Customer.pm
+++ LedgerSMB/DBObject/Customer.pm
@@ -1,3 +1,28 @@
+
+=head1 NAME
+
+LedgerSMB::DBObject::Customer, LedgerSMB Class for Customers
+
+=head1 SYNOPSIS
+
+This library contains the base utility functions for creating, saving, and
+retrieving customers.
+
+=head1 INHERITS
+
+LedgerSMB::DBObject::Company
+
+=head1 METHODS
+
+=over
+
+=item set_entity_class
+
+Sets entity_class to 2.
+
+=cut
+
+
 package LedgerSMB::DBObject::Customer;
 
 use base qw(LedgerSMB::DBObject::Company);
@@ -12,4 +37,14 @@ sub set_entity_class {
 
 }
     
+=back
+
+=head1 COPYRIGHT
+
+Copyright (c) 2009, the LedgerSMB Core Team.  This is licensed under the GNU 
+General Public License, version 2, or at your option any later version.  Please 
+see the accompanying License.txt for more information.
+
+=cut
+
 1;
--- LedgerSMB/DBObject/Vendor.pm
+++ LedgerSMB/DBObject/Vendor.pm
@@ -1,3 +1,29 @@
+
+
+=head1 NAME
+
+LedgerSMB::DBObject::Vendor, LedgerSMB Class for Vendors
+
+=head1 SYNOPSIS
+
+This library contains the base utility functions for creating, saving, and
+retrieving vendors.
+
+=head1 INHERITS
+
+LedgerSMB::DBObject::Company
+
+=head1 METHODS
+
+=over
+
+=item set_entity_class
+
+Sets entity_class to 1.
+
+=cut
+
+
 package LedgerSMB::DBObject::Vendor;
 
 use base qw(LedgerSMB::DBObject::Company);
@@ -10,4 +36,14 @@ sub set_entity_class {
     $self->{entity_class} = $ENTITY_CLASS;
 }
     
+=back
+
+=head1 COPYRIGHT
+
+Copyright (c) 2009, the LedgerSMB Core Team.  This is licensed under the GNU 
+General Public License, version 2, or at your option any later version.  Please 
+see the accompanying License.txt for more information.
+
+=cut
+
 1;
