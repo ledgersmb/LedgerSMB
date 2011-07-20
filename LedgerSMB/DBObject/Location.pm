564ea16e44b828aa64ca82a2cb3bacbb833c1eef
Chris Travers <chris.travers@gmail.com>
2011-07-20 23:08:21 +0000

    Correcting errors saving user address.  Edit/delete still broken
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@3548 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB/DBObject/Location.pm
+++ LedgerSMB/DBObject/Location.pm
@@ -1,3 +1,16 @@
+
+# A few things that are suboptimmal about this file.
+# 1)  No use of discovery in stored proc arg lists means that API's are brittle
+# 2)  Dynamic generation of sproc names makes this extremely hard to use to 
+# find if a sproc is used or not.  This also (as per #1) forces sprocs to share
+# exactly identical argument lists which is a problem in terms of API 
+# flexibility.  I am inclined to mark this "depricated" despite the fact that 
+# this is new code.    Right now this is only used by user management routines
+# and it should stay that way.
+#
+# --CT
+#
+
 package LedgerSMB::DBObject::Location;
 
 use base LedgerSMB::DBObject;
@@ -13,18 +26,26 @@ sub save {
     my $type = shift @_;
     
     # assumes all the parameters are present...
+            'address1',
+            'address2',
+            'city',
+            'state',
+            'zipcode',
+            'country',
+            'companyname',            
     
     my ($ret) = $self->exec_method(funcname=>$type."__save_location", args=>[
         $self->{user_id}, # entity_id           
         $self->{location_id}, # location_id
-        $self->{location_class}, # location class, for _to_contact
-        $self->{line_one},
-        $self->{line_two},
+        $self->{location_class} || 1, # location class, for _to_contact
+        $self->{line_one} || $self->{'address1'},
+        $self->{line_two} || $self->{'address2'},
         $self->{line_three}, # address info
         $self->{city}, # city
         $self->{state}, # state/province
-        $self->{mail_code},
-        $self->{country} # obviously, country.
+        $self->{mail_code} || $self->{'zipcode'},
+        $self->{country}, # obviously, country.
+        $self->{old_location_class} || 1 # obviously, country.
     ]);
     $self->{id} = $ret->{$type."__save_location"};
     $self->{dbh}->commit();
