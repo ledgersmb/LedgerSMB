5c261a7b9cb7c19d724d677515f321b8efa56c63
Chris Travers <chris.travers@gmail.com>
2011-07-22 16:12:14 +0000

    Correction to permissions of chart view
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@3561 4979c152-3d1c-0410-bac9-87ea11338e46

--- sql/Pg-database.sql
+++ sql/Pg-database.sql
@@ -121,6 +121,8 @@ CREATE VIEW chart AS
 SELECT id, accno, description, 'H' as charttype, NULL as category, NULL as link, NULL as account_heading, null as gifi_accno, false as contra, false as tax from account_heading UNION
 select c.id, c.accno, c.description, 'A' as charttype, c.category, concat_colon(l.description) as link, heading, gifi_accno, contra, tax from account c left join account_link l ON (c.id = l.account_id) group by c.id, c.accno, c.description, c.category, c.heading, c.gifi_accno, c.contra, c.tax;
 
+GRANT SELECT ON chart TO public;
+
 COMMENT ON VIEW chart IS $$Compatibility chart for 1.2 and earlier.$$;
 -- pricegroup added here due to references
 CREATE TABLE pricegroup (
--- sql/upgrade/3290-tax-account.sql
+++ sql/upgrade/3290-tax-account.sql
@@ -91,6 +91,7 @@ CREATE VIEW chart AS
 SELECT id, accno, description, 'H' as charttype, NULL as category, NULL as link, NULL as account_heading, null as gifi_accno, false as contra, false as tax from account_heading UNION
 select c.id, c.accno, c.description, 'A' as charttype, c.category, concat_colon(l.description) as link, heading, gifi_accno, contra, tax from account c left join account_link l ON (c.id = l.account_id) group by c.id, c.accno, c.description, c.category, c.heading, c.gifi_accno, c.contra, c.tax;
 
+GRANT SELECT ON chart TO public;
 
 CREATE OR REPLACE RULE chart_i AS ON INSERT TO chart
 DO INSTEAD
