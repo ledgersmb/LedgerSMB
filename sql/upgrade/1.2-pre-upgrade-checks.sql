151c391addc6b4abc5361bbb5d1baaa3f96c2407
Herman Vierendeels <herman.vierendeels@gmail.com>
2011-11-13 18:33:31 +0000

    add more checks
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.3@3995 4979c152-3d1c-0410-bac9-87ea11338e46

--- sql/upgrade/1.2-pre-upgrade-checks.sql
+++ sql/upgrade/1.2-pre-upgrade-checks.sql
@@ -1,3 +1,6 @@
+--tshvr use lsmb12 if 1.2 db public schema has already been renamed to lsmb12
+SET search_path = lsmb12, public, pg_catalog;
+
 SELECT count(*), customernumber from customer
 GROUP BY customernumber
 HAVING count(*) > 1;
@@ -20,4 +23,9 @@ group by partnumber having count(*) > 1;
 
 SELECT invnumber, count(*) from ar
 group by invnumber having count(*) > 1;
+SELECT invnumber, count(*) from ap group by invnumber having count(*) > 1;
 
+SELECT trans_id,count(trans_id) FROM acc_trans where trans_id not in (select id from ap union select id from ar union select id from gl) group by trans_id;
+SELECT trans_id,count(trans_id) FROM invoice where trans_id not in (select id from ap union select id from ar union select id from gl) group by trans_id;
+
+select setting_key,value from defaults where setting_key='curr';
