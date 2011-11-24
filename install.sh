191b273457add7c481229f3fd3897f36bc750008
Herman Vierendeels <herman.vierendeels@gmail.com>
2011-11-24 08:46:32 +0000

    eventual  apache alias
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.3@4041 4979c152-3d1c-0410-bac9-87ea11338e46

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+#./install.sh myLsmb13_test
+
 CWD=`pwd`
+APACHE_ALIAS='ledgersmb'
+
+if  [ $# -eq 1 ]
+then
+ APACHE_ALIAS=$1
+ echo "setting apache alias to $APACHE_ALIAS"
+fi
+
 
 echo "Installing Perl Modules"
 
@@ -12,7 +22,8 @@ make
 
 echo "Configuring Apache"
 
-sed "s|WORKING_DIR|$CWD|" ledgersmb-httpd.conf.template > ledgersmb-httpd.conf
+#sed "s|WORKING_DIR|$CWD|" ledgersmb-httpd.conf.template > ledgersmb-httpd.conf
+sed  "s|/ledgersmb|/$APACHE_ALIAS|g;s|WORKING_DIR|$CWD|g" ledgersmb-httpd.conf.template > ledgersmb-httpd.conf
 
 echo "Which user does your web server run as?"
 read username
