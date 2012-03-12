27d5ed3b2217de167e296bd6026b3662dcf17175
Chris Travers <chris.travers@gmail.com>
2012-03-12 06:48:42 +0000

    Version number changes for 1.3.13
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.3@4447 4979c152-3d1c-0410-bac9-87ea11338e46

--- dists/source/build.sh
+++ dists/source/build.sh
@@ -2,7 +2,7 @@
 
 # Simple script to prepare for release
 
-version="1.3.12";
+version="1.3.13";
 build_d="../release";
 
 if test -d $build_d/ledgersmb; then
--- sql/Pg-database.sql
+++ sql/Pg-database.sql
@@ -1018,7 +1018,7 @@ sinumber|1
 sonumber|1
 yearend|1
 businessnumber|1
-version|1.3.12
+version|1.3.13
 closedto|\N
 revtrans|1
 ponumber|1
