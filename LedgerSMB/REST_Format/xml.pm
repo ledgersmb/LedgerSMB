d74687d05335bddc9ac2e548ff042fb5283d9a11
Chris Travers <chris.travers@gmail.com>
2012-05-22 06:58:29 +0000

    XML format handler for RESTful web services
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@4745 4979c152-3d1c-0410-bac9-87ea11338e46

---
+++ LedgerSMB/REST_Format/xml.pm
@@ -0,0 +1,36 @@
+=head1 NAME
+
+LedgerSMB::REST_Format::xml - XML file support for LedgerSMB RESTful web 
+services.
+
+=head1 SYNOPSYS
+
+
+my $hash = LedgerSMB::REST_Format::xml::from_input($request);
+my $xml = LedgerSMB::REST_Format::xml::to_output($request);
+
+=head1 COPYRIGHT 
+
+Copyright (C) 2012 The LedgerSMB Core Team.
+
+This file may be used under the terms of the GNU General Public License 
+version 2 or at your option any later version.  Please see the included
+LICENSE.TXT file.
+
+=cut
+
+use XML::Simple;
+use strict;
+use warnings;
+
+sub from_input{
+    my $request = shift @_;
+    return XMLin($request->{payload}, ForceArray=>1);
+}
+
+sub to_output{
+    my $request = shift @_; 
+    return XMLout($request->{payload}, RootName => $request->{class_name});
+}
+
+1;
