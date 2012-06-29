445d379f2dd91e6a883304957cdb6acd5046a186
Chris Travers <chris.travers@gmail.com>
2012-06-29 06:24:45 +0000

    correcting localization of date formats for taxform reports
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.3@4950 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB/User.pm
+++ LedgerSMB/User.pm
@@ -189,6 +189,15 @@ sub fetch_config {
     $sth = $dbh->prepare("SELECT value FROM defaults WHERE setting_key = 'templates'");
     $sth->execute();
     ($templates) = $sth->fetchrow_array() || 'demo';
+    my %date_query = (
+        'mm/dd/yy' => 'set DateStyle to \'SQL, US\'',
+        'mm-dd-yy' => 'set DateStyle to \'POSTGRES, US\'',
+        'dd/mm/yy' => 'set DateStyle to \'SQL, EUROPEAN\'',
+        'dd-mm-yy' => 'set DateStyle to \'POSTGRES, EUROPEAN\'',
+        'dd.mm.yy' => 'set DateStyle to \'GERMAN\''
+    );
+    $dbh->do( $date_query{ $myconfig{dateformat} } );
+
     $myconfig->{templates} = "$LedgerSMB::Sysconfig::templates/$templates";
     return $myconfig;
 }
