816ad6c864f6be0f54f1dcf359e1857c8829e8d1
Chris Travers <chris.travers@gmail.com>
2012-07-24 02:10:21 +0000

    Corrected customer info not populating orders list for shipping.  Fixed shipping redirects.
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.3@5028 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB/AA.pm
+++ LedgerSMB/AA.pm
@@ -121,7 +121,7 @@ sub post_transaction {
     my $tax         = 0;
     my $fxtax       = 0;
     my $amount;
-    my $diff = 0;
+    my $diff        = 0;
 
     my %tax = ();
     my $accno;
--- bin/oe.pl
+++ bin/oe.pl
@@ -1625,7 +1625,7 @@ sub transactions {
     for (
         "oldsort",     "direction", "path",      "type",
         "vc",          "login",     "sessionid", "transdatefrom",
-        "transdateto", "open",      "closed"
+        "transdateto", "open",      "closed", "oe_class_id"
       )
     {
         $callback .= qq|&$_=$form->{$_}|;
