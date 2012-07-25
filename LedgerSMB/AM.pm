a791f85329ab86200a9969c83b7638f2305272db
Chris Travers <chris.travers@gmail.com>
2012-07-25 10:53:37 +0000

    correcting directory transversal not allowed errors when using fs_cssdir
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.3@5042 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB/AM.pm
+++ LedgerSMB/AM.pm
@@ -1501,6 +1501,11 @@ sub check_template_name {
 
     my @allowedsuff = qw(css tex txt html xml);
     my $test = $form->{file};
+    $test =~ s|^$LedgerSMB::Sysconfig::fs_cssdir||;
+    if ($LedgerSMB::Sysconfig::fs_cssdir 
+           and $LedgerSMB::Sysconfig::fs_cssdir !~ m|/$|){
+         $test =~ s|^/||;
+    }
     if ($LedgerSMB::Sysconfig::templates =~ /^(.:)*?\//){
         $test =~ s#^$LedgerSMB::Sysconfig::templates/?\\?##;
     }
