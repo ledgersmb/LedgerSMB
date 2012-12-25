9a1e8029e6f54f563a79156dcddd1485d676bcc1
Chris Travers <chris.travers@gmail.com>
2012-12-25 06:20:44 +0000

    Fixed minor localization issue with aa.pl updated .pot added script to update .pot
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@5409 4979c152-3d1c-0410-bac9-87ea11338e46

---
+++ tools/rebuild_pot.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+# EXTRACTING DB STRINGS
+echo "select 'text(''' || label || ''')' FROM menu_node" | 
+ psql -U postgres > tools/dbstrings
+
+echo "select 'text(''' || class || ''')' FROM entity_class" | 
+ psql -U postgres >> tools/dbstrings
+
+echo "select 'text(''' || class || ''')' FROM batch_class" | 
+ psql -U postgres >> tools/dbstrings
+
+echo "select 'text(''' || class || ''')' FROM contact_class" | 
+ psql -U postgres >> tools/dbstrings
+
+echo "select 'text(''' || class || ''')' FROM location_class" | 
+ psql -U postgres >> tools/dbstrings
+
+
+# SETTING UP FILE LIST
+find . -name '*.pl' | grep -v blib > tools/files
+find . -name '*.pm' | grep -v blib | grep -v Num2text | 
+  grep -v LaTeX >> tools/files
+
+# EXTRACT STRINGS AND MERGE WITH .POT
+
+xgettext -f tools/files -ktext -o locale/LedgerSMB.pot 
+
+xgettext -ktext -j -o locale/LedgerSMB.pot -a tools/dbstrings --language=perl
