e0238d53d289af0924f51685c3355e41725fd2ab
Chris Travers <chris.travers@gmail.com>
2013-01-09 10:08:19 +0000

    Merging from trunk, fix regarding browser handling on logout
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.3@5525 4979c152-3d1c-0410-bac9-87ea11338e46

--- UI/logout/iexplore.js
+++
@@ -1,9 +0,0 @@
-try{
-var agt=navigator.userAgent.toLowerCase();
-if (agt.indexOf("msie") != -1) {
-// IE clear HTTP Authentication
-    document.execCommand("ClearAuthenticationCache");
-}
-}
-catch (e) {
-}
--- UI/logout/konqueror.js
+++
@@ -1,1 +0,0 @@
-// logout routines for Konqueror go here
