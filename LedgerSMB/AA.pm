5c676f5b7941750c5e8643c0a243e3d3714b59ca
Chris Travers <chris.travers@gmail.com>
2013-05-10 15:08:47 +0000

    Merging in from trunk address line for customer/vendor invoices and transactions
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.3@5757 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB/AA.pm
+++ LedgerSMB/AA.pm
@@ -1292,7 +1292,7 @@ sub get_name {
     
 
     # get customer/vendor
-    my $query = qq|
+    my $query = qq/
 		   SELECT entity.name AS $form->{vc}, c.discount, 
 		          c.creditlimit, 
 		          c.terms, c.taxincluded,
@@ -1301,11 +1301,18 @@ sub get_name {
 			  b.discount AS tradediscount, 
 		          b.description AS business, 
 			  entity.control_code as entity_control_code,
-			  c.meta_number
+			  c.meta_number, ecl.*
 		     FROM entity_credit_account c
 		     JOIN entity ON (entity.id = c.entity_id)
 		LEFT JOIN business b ON (b.id = c.business_id)
-		    WHERE c.id = ?|;
+                LEFT JOIN (SELECT coalesce(line_one, '')
+                               || ' ' || coalesce(line_two, '') as address,
+                               l.city, etl.credit_id
+                          FROM eca_to_location etl
+                          JOIN location l ON etl.location_id = l.id
+                          WHERE etl.location_class = 1) ecl
+                        ON (c.id = ecl.credit_id)
+		    WHERE c.id = ?/;
     # TODO:  Add location join
 
     @queryargs = ( $form->{"$form->{vc}_id"} );
