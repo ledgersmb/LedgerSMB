978fb71bca71f46613111b5588f440ee45e7d2e4
Chris Travers <chris.travers@gmail.com>
2013-05-12 09:17:14 +0000

    Merging gl void UI anomily fix from trunk
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.3@5777 4979c152-3d1c-0410-bac9-87ea11338e46

--- bin/gl.pl
+++ bin/gl.pl
@@ -1175,6 +1175,10 @@ sub post {
     }
 
     if ( GL->post_transaction( \%myconfig, \%$form, $locale) ) {
+        for (0 .. $form->{rowcount}){
+            delete $form->{"credit_$_"};
+            delete $form->{"debit_$_"};
+        }
         edit();
     }
     else {
