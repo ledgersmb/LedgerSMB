73104e3407c4b92a41071074343a688d90779343
Chris Travers <chris.travers@gmail.com>
2013-05-14 04:38:16 +0000

    forward porting test fix
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@5784 4979c152-3d1c-0410-bac9-87ea11338e46

--- tools/pre-release-tests.sh
+++ tools/pre-release-tests.sh
@@ -4,4 +4,6 @@ sh tools/delete-company-database.sh lsmb13installtest
 
 rm -f /tmp/ledgersmb/dblog*
 
+PGUSER=postgres PGPASSWORD=test dropdb lsmbinstall
+
 PGUSER=postgres PGPASSWORD=test LSMB_TEST_DB=1 LSMB_NEW_DB=lsmbinstall make test
