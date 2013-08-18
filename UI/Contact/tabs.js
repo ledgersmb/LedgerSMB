3e3f0bbb0b957ab891d061ac8eaf45862dafe9c3
John Locke <john@freelock.com>
2013-08-18 15:41:23 +0000

    Re-implement loading up contact screen with selected tab.
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@5948 4979c152-3d1c-0410-bac9-87ea11338e46

--- UI/Contact/tabs.js
+++ UI/Contact/tabs.js
@@ -1,8 +1,19 @@
-define(['dijit/layout/TabContainer', 'dijit/layout/ContentPane', 'dojo/parser'],
-    function(TabContainer, ContentPane, Parser) {
+define(['dijit/registry','dojo/ready'],
+    function(registry, ready) {
       return {
         init: function(){
-          Parser.parse();
+          // global should get set on the page, but is not available at init.
+          // Need to wait for widgets to initialize before setting active tab.
+          // dojo/ready queues this job to run after the parser is finished.
+          if (activeDiv) {
+            ready(function() {
+              var td = registry.byId('contact_tabs');
+              var ad = registry.byId(activeDiv);
+              console.log(td,ad,activeDiv);
+              td.selectChild(ad);
+
+            });
+          }
         }
       };
     }
