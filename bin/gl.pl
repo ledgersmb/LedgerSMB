790c2702ec0c69565bca9d2274a0c84c07247137
Chris Travers <chris.travers@gmail.com>
2013-09-19 11:36:36 +0000

    Syntax error fix from converting old code to dojo
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@6061 4979c152-3d1c-0410-bac9-87ea11338e46

--- bin/gl.pl
+++ bin/gl.pl
@@ -120,7 +120,7 @@ sub approve {
     if ($form->{callback}){
         print "Location: $form->{callback}\n";
         print "Status: 302 Found\n\n";
-        print "<html><body class="$form->{dojo_theme}">";
+        print qq|<html><body class="$form->{dojo_theme}">|;
         my $url = $form->{callback};
         print qq|If you are not redirected automatically, click <a href="$url">|
                 . qq|here</a>.</body></html>|;
