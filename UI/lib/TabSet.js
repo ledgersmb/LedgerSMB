4c4ae19f17abe103b5ade4da345964a0d719f603
Chris Travers <chris.travers@gmail.com>
2013-09-26 10:27:51 +0000

    Adding accidently omitted TabSet.js
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@6086 4979c152-3d1c-0410-bac9-87ea11338e46

---
+++ UI/lib/TabSet.js
@@ -0,0 +1,34 @@
+define([
+    'dijit/layout/TabContainer',
+    'dojo/_base/declare'
+    ],
+    function(TabContainer, declare) {
+      return declare('dijit/layout/TabContainer',
+        [TabContainer],
+        {
+          startup: function() {
+           var myself = this; // needed for AMD query usage below.
+           require (['dojo/query', 
+                     'dijit/layout/ContentPane', 
+                     'dijit/registry', 'dojo/domReady!'],
+           function(query, cp, registry) {
+               query('div.lsmbtab').forEach(function(cnode){
+               new cp (
+                   { "data-dojo-type": 'dijit.layout.ContentPane',
+                     "title": cnode.title},
+                   cnode 
+               );
+               var t = registry.byId(cnode.id);
+               if (t !== undefined){
+                   myself.addChild(t);
+               }
+               });
+            });
+            this.inherited(arguments);
+            
+          }
+          
+        });
+    }
+    );
+
