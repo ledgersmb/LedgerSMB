bbb0015e4c7395a9553bca5cc81d70bbe22c6d19
Chris Travers <chris.travers@gmail.com>
2013-09-28 03:32:02 +0000

    updated docs on new tab widget
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@6091 4979c152-3d1c-0410-bac9-87ea11338e46

--- UI/lib/TabSet.js
+++ UI/lib/TabSet.js
@@ -1,3 +1,37 @@
+/* lsmb/lib/TabSet
+ * A Dojo tabset widget for LedgerSMB
+ * based on dijit/layout/TabContainer
+ *
+ * This widget features autodetection of internal content panes but currently 
+ * does not support nesting unless content panes are remote.  That may change
+ * in future versions.  Additionally, this widget supports initial tab selection
+ * and size declarations via the startup() call.
+ *
+ * Overridden methods:
+ *
+ * startup(activeDiv, boxSize)
+ *
+ * activeDiv is the element id of the pane to be activated on startup, and
+ * boxSize is the size to make the tabset.
+ *
+ * Internal content panes are created from divs with a class of 'lsmb-tab' 
+ * allowing for dojo-agnostic auto-detection.
+ *
+ * Sample (instantiates a tab set, sets the active div to the activeDiv 
+ *         parameter in the global scope, and sets the size to the viewport):
+ * require([
+ *          'dojo/query',
+ *          'dojo/dom',
+ *          "lsmb/lib/TabSet",
+ *          'dojo/window',
+ *          'dojo/domReady!'],
+ * function(query, dom, tc, win){
+ *     var tabs = new tc({}, dom.byId('contact_tabs'));
+ *     tabs.startup(activeDiv, win.getBox());
+ * }
+ * ); 
+ *
+ */
 define([
     'dijit/layout/TabContainer',
     'dojo/_base/declare'
