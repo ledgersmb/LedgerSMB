6e130fa10d9155773e4e84defd18b99536c9bd0f
Chris Travers <chris.travers@gmail.com>
2013-09-28 03:27:52 +0000

    Removed unused dependencies after moving tabs to new widget
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@6090 4979c152-3d1c-0410-bac9-87ea11338e46

--- UI/Contact/contact.js
+++ UI/Contact/contact.js
@@ -2,12 +2,9 @@ require([
          'dojo/query', 
          'dojo/dom', 
          "lsmb/lib/TabSet",
-         "dijit/layout/ContentPane",
-         'lsmb/Contact/tabs',
-          'dijit/registry',
           'dojo/window',
-         'dojo/ready'],
-       function(query, dom, tc, cp, obj, registry, win){
+         'dojo/domReady!'],
+       function(query, dom, tc, win){
            var tabs = new tc({}, dom.byId('contact_tabs'));
            tabs.startup(activeDiv, win.getBox());
        } 
