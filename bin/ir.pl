edc1df0a264edb8a221c492849aaf9e05ac2dbcd
Chris Travers <chris.travers@gmail.com>
2013-10-23 12:11:54 +0000

    Dojo-related HTML issues resolved that were causing invoice dates not to be submitted
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@6163 4979c152-3d1c-0410-bac9-87ea11338e46

--- bin/ir.pl
+++ bin/ir.pl
@@ -553,11 +553,11 @@ function on_return_submit(event){
 	      </tr>
 	      <tr>
 		<th align=right nowrap>| . $locale->text('Invoice Date') . qq|</th>
-		<td><input class="date" name=transdate size=11 title="$myconfig{dateformat}" value=$form->{transdate}></td>
+		<td><input class="date" name=transdate size=11 title="$myconfig{dateformat}" value="$form->{transdate}" id="transdate"></td>
 	      </tr>
 	      <tr>
 		<th align=right nowrap>| . $locale->text('Due Date') . qq|</th>
-		<td><input class="date" name=duedate size=11 title="$myconfig{dateformat}" value=$form->{duedate}></td>
+		<td><input class="date" name=duedate size=11 title="$myconfig{dateformat}" value="$form->{duedate}" id="duedate"></td>
 	      </tr>
 	      <tr>
 		<th align=right nowrap>| . $locale->text('PO Number') . qq|</th>
