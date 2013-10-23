8fc1c86307f13e76a42dc2b968c566d14dd09599
Chris Travers <chris.travers@gmail.com>
2013-10-23 11:56:21 +0000

    dojo-related syntax errors fixed
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@6162 4979c152-3d1c-0410-bac9-87ea11338e46

--- bin/ir.pl
+++ bin/ir.pl
@@ -87,7 +87,7 @@ sub approve {
     if ($form->{callback}){
         print "Location: $form->{callback}\n";
         print "Status: 302 Found\n\n";
-        print "<html><body class="$form->{dojo_theme}">";
+        print qq|<html><body class="$form->{dojo_theme}">|;
         my $url = $form->{callback};
         print qq|If you are not redirected automatically, click <a href="$url">|
                 . qq|here</a>.</body></html>|;
@@ -1464,7 +1464,7 @@ sub save_info {
 	    if ($form->{callback}){
 		print "Location: $form->{callback}\n";
 		print "Status: 302 Found\n\n";
-		print "<html><body class="$form->{dojo_theme}">";
+		print qq|<html><body class="$form->{dojo_theme}">|;
 		my $url = $form->{callback};
 		print qq|If you are not redirected automatically, click <a href="$url">|
 			. qq|here</a>.</body></html>|;
