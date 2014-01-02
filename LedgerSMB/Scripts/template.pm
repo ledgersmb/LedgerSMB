3c832f382346078c74527c3ab0142268170e28ab
Chris Travers <chris.travers@gmail.com>
2014-01-02 11:46:07 +0000

    Adding omitted workflow script
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@6346 4979c152-3d1c-0410-bac9-87ea11338e46

---
+++ LedgerSMB/Scripts/template.pm
@@ -0,0 +1,94 @@
+=head1 NAME
+
+LedgerSMB::Scripts::template - Template editing workflows for LedgerSMB
+
+=cut
+
+package LedgerSMB::Scripts::template;
+use LedgerSMB::Template::DB;
+use LedgerSMB::Report::Listings::Templates;
+use LedgerSMB::Template;
+
+=head1 SYNPOSIS
+
+To display the edit screen
+
+   LedgerSMB::Scripts::template::display($request)
+
+To edit:
+
+   LedgerSMB::Scripts::template::edit($request)
+
+=head1 FUNCTIONS
+
+=head2 list($request)
+
+Lists the templates.
+
+=cut
+
+sub list {
+    my ($request) = @_;
+    LedgerSMB::Report::Listing::Templates->new(%$request)->render($request);
+}
+
+=head2 display($request)
+
+Displays a template for review
+
+=cut
+
+sub display {
+    my ($request) = @_;
+    my $dbtemp = LedgerSMB::Template::DB->get(%$request);
+    LedgerSMB::Template->new(
+        user     => $request->{_user},
+        locale   => $request->{_locale},
+        path     => 'UI/templates',
+        template => 'review',
+        format   => 'HTML'
+    )->render($dbtemp);
+}
+
+=head2 edit($request)
+
+Displays a screen for editing the template
+
+=cut
+
+sub edit {
+    my ($request) = @_;
+    my $dbtemp = LedgerSMB::Template::DB->get(%$request);
+    LedgerSMB::Template->new(
+        user     => $request->{_user},
+        locale   => $request->{_locale},
+        path     => 'UI/templates',
+        template => 'edit',
+        format   => 'HTML'
+    )->render($dbtemp);
+}
+
+=head2 save($request)
+
+Saves the template.
+
+=cut
+
+sub save {
+    my ($request) = @_;
+    my $dbtemp = LedgerSMB::Template::DB->get(%$request);
+    $dbtemp->save();
+    display($request);
+}
+
+=head1 COPYRIGHT
+
+Copyright (C) 2014 The LedgerSMB Core Team.
+
+This file may be re-used under the terms of the GNU General Public License 
+version 2 or at your option any later version.  Please see the included 
+LICENSE.txt for details.
+
+=cut
+
+1;
