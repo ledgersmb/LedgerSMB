3470d232d964f4a62eba8f6de107ba31dae4dc53
Chris Travers <chris.travers@gmail.com>
2014-01-02 07:03:49 +0000

    Initial DB loader for templates
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@6342 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB/Template/DB.pm
+++ LedgerSMB/Template/DB.pm
@@ -2,6 +2,12 @@
 
 LedgerSMB::Template::DB - Template administration functions for LedgerSMB
 
+=cut
+
+package LedgerSMB::Template::DB;
+use Moose;
+with 'LedgerSMB::DBObject_Moose';
+
 =head1 SYNPOPSIS
 
 To retrieve template data as a scalar ref (for TT):
@@ -35,7 +41,84 @@ To save:
 
 =head2 format text (required)
 
+=cut
+
+has id             => (is => 'ro', isa => 'Int', required => 0);
+has template_name  => (is => 'ro', isa => 'Str', required => 1);
+has language_code  => (is => 'ro', isa => 'Str', required => 0);
+has format         => (is => 'ro', isa => 'Str', required => 1);
+has template       => (is => 'ro', isa => 'Str', required => 1);
+
 =head1 METHODS
+
+=head2 get_template(template_name, language_code, format)
+
+Returns a scalar ref to the template text so that Template Toolkit can run it.
+
+=cut
+
+sub get_template {
+    my ($module, $template_name, $language_code, $format) = @_;
+    my ($temp) = __PACKAGE__->call_procedure(
+         procname => 'template__get'
+         args => [$template_name, $language_code, $format]
+    );
+    my $text = $temp->{template};
+    return \$text;
+}
+
+=head2 get_by_id(id)
+
+Gets the template by ID.  Returns a whole template object.
+
+=cut
+
+sub get_by_id {
+    my ($module, $id) = @_;
+    my ($temp) = __PACKAGE__->call_procedure(
+         procname => 'template__get_by_id'
+         args => [$id]
+    );
+    return __PACKAGE__->new(%$temp);
+}
+
+
+=head2 get(hash args)
+
+Gets the template by args (for editing or management).  Args are:
+
+=over
+
+=item template_name
+
+=item language_code
+
+=item format
+
+=back
+
+=cut
+
+sub get {
+    my $module = shift @_;
+    my %args = @_;
+    my ($temp) = __PACKAGE__->call_procedure(
+         procname => 'template__get'
+         args => [$args{template_name}, $args{language_code}, $args{format}]
+    );
+    return __PACKAGE__->new(%$temp);
+}
+
+=head2 save
+
+Saves the current object
+
+=cut
+
+sub save {
+    my ($self) = @_;
+    $self->exec_method(funcname => 'template__save');
+}
 
 =head1 COPYRIGHT
 
