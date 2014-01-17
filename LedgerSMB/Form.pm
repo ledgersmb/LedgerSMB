113958065c9c8afd6478c3854de61f3f53e99565
Chris Travers <chris.travers@gmail.com>
2014-01-17 10:34:15 +0000

    Fixing bug 929, problem 1, db error adding shipto to invoice
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.3@6504 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB/Form.pm
+++ LedgerSMB/Form.pm
@@ -1840,7 +1840,7 @@ $dbh is unused.
 
 sub add_shipto {
   
-  	my ( $self,$dbh,$id ) = @_;
+  	my ( $self,$dbh,$id, $oe) = @_;
         if (! $self->{locationid}) {
 		return;
 	}
@@ -1851,10 +1851,18 @@ sub add_shipto {
 			|;
 
         my $sth = $self->{dbh}->prepare($query) || $self->dberror($query);
-
+        my $trans_id;
+        my $oe_id;
+        if ($oe){
+           $trans_id = undef;
+           $oe_id = $id;
+        } else {
+           $trans_id = $id;
+           $oe_id = undef;
+        }  
         $sth->execute(
-                        undef,                     
-			$self->{id},
+                        $trans_id,                     
+			$oe_id,
 			$self->{locationid}
              
 		      ) || $self->dberror($query);
