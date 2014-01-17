2e07cc8a95c1c3743e2523ccfe30f44172bbdd2b
Chris Travers <chris.travers@gmail.com>
2014-01-17 08:04:32 +0000

    Updating POD for location API changes.
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@6499 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB/Entity/Location.pm
+++ LedgerSMB/Entity/Location.pm
@@ -254,6 +254,10 @@ sub save {
 
 Deletes the current location
 
+This can be called from $self->delete() if you have  a location object, or it
+can be called as LedgerSMB::Entity::Locateion::delete($hashref) if the hashref 
+contains either entity_id or credit_id, and location_id, and location class.
+
 =cut
 
 sub delete{
