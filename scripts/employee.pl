56b634aa7a29cefb0212f7132b48b174a27dafc8
Chris Travers <chris.travers@gmail.com>
2014-02-05 12:22:36 +0000

    fixing bug 979 for 1.3 based on 1.4 patch
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.3@6763 4979c152-3d1c-0410-bac9-87ea11338e46

--- scripts/employee.pl
+++ scripts/employee.pl
@@ -295,7 +295,7 @@ sub search_results {
     $request->{title} = $locale->text('Search Results');
     for my $col (qw(l_position l_id l_employeenumber l_salutation 
                     l_first_name l_middle_name l_last_name l_dob 
-                    l_startdate l_enddate l_role l_ssn l_sales l_manager_id
+                    l_start_date l_end_date l_role l_ssn l_sales l_manager_id
                     l_manager_first_name l_manager_last_name)){
         if ($request->{$col}){
            my $pcol = $col;
@@ -311,8 +311,8 @@ employeenumber=> $locale->text('Employee Number'),
   middle_name => $locale->text('Middle Name'),
     last_name => $locale->text('Last Name'),
           dob => $locale->text('DOB'),
-    startdate => $locale->text('Start Date'),
-      enddate => $locale->text('End Date'),
+   start_date => $locale->text('Start Date'),
+     end_date => $locale->text('End Date'),
          role => $locale->text('Role'),
           ssn => $locale->text('SSN'),
         sales => $locale->text('Sales'),
