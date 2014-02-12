7748115469507963e2860e798470825f7e4bf36d
Chris Travers <chris.travers@gmail.com>
2014-02-12 02:51:12 +0000

    Adding internal and incoming file classes.  No workflows yet.
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/trunk@6846 4979c152-3d1c-0410-bac9-87ea11338e46

---
+++ LedgerSMB/File/Incoming.pm
@@ -0,0 +1,56 @@
+=pod
+
+=head1 NAME
+
+LedgerSMB::File::Incoming - Incoming queue for not-yet-associated file 
+attachments
+
+=head1 SYNOPSIS
+
+TODO
+
+=head1 INHERITS
+
+=over
+
+=item  LedgerSMB::File
+
+Provides all properties and accessors.  This subclass provides additional 
+methods only
+
+=back
+
+=cut
+
+package LedgerSMB::File::Incoming;
+use Moose;
+extends 'LedgerSMB::File';
+
+=head1 METHODS
+
+=over
+
+=item attach
+
+Attaches or links a specific file to the given transaction.
+
+=cut
+
+sub attach {
+    my ($self, $args) = @_;
+    $self->exec_method({funcname => 'file__save_incoming'});
+}
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright (C) 2011-2014 The LedgerSMB Core Team
+
+This file is licensed under the Gnu General Public License version 2, or at your
+option any later version.  A copy of the license should have been included with
+your software.
+
+=cut
+
+1;
---
+++ LedgerSMB/File/Internal.pm
@@ -0,0 +1,55 @@
+=pod
+
+=head1 NAME
+
+LedgerSMB::File::Internal - Files for Internal processing
+
+=head1 SYNOPSIS
+
+TODO
+
+=head1 INHERITS
+
+=over
+
+=item  LedgerSMB::File
+
+Provides all properties and accessors.  This subclass provides additional 
+methods only
+
+=back
+
+=cut
+
+package LedgerSMB::File::Internal;
+use Moose;
+extends 'LedgerSMB::File';
+
+=head1 METHODS
+
+=over
+
+=item attach
+
+Attaches or links a specific file to the given transaction.
+
+=cut
+
+sub attach {
+    my ($self, $args) = @_;
+    $self->exec_method({funcname => 'file__save_internal'});
+}
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright (C) 2011-2014 The LedgerSMB Core Team
+
+This file is licensed under the Gnu General Public License version 2, or at your
+option any later version.  A copy of the license should have been included with
+your software.
+
+=cut
+
+1;
