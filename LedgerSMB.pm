468dd9c483422afb8d0be099c62622582cc87616
Chris Travers <chris.travers@gmail.com>
2014-03-26 02:53:54 +0000

    Version number changes in preparation for 1.3.39
    git-svn-id: svn://svn.code.sf.net/p/ledger-smb/code/branches/1.3@6949 4979c152-3d1c-0410-bac9-87ea11338e46

--- LedgerSMB.pm
+++ LedgerSMB.pm
@@ -223,7 +223,7 @@ $CGI::Simple::POST_MAX = -1;
 
 package LedgerSMB;
 use base 'LedgerSMB::Request';
-our $VERSION = '1.3.38';
+our $VERSION = '1.3.39';
 
 my $logger = Log::Log4perl->get_logger('LedgerSMB');
 
