c88973dd1ea684114f5fd68343e6abab7d3f1b50
ehuelsmann <none@none>
2014-05-22 23:08:17 +0200

    Address ticket #1145: Qty in search result zero, but OH correct elsewhere.    The Qty column isn't the On hand column, but can be easily mistaken.    Putting On hand in the search result by default instead.

--- LedgerSMB/Report/Inventory/Search.pm
+++ LedgerSMB/Report/Inventory/Search.pm
@@ -138,7 +138,7 @@ period.
 
 =cut
 
-has purchase_invpices => (is => 'ro', isa => 'Bool', required => 0);
+has purchase_invoices => (is => 'ro', isa => 'Bool', required => 0);
 
 =item sales_orders
 
@@ -196,13 +196,13 @@ sub columns {
        type => 'text',
        name => LedgerSMB::Report::text('Description'),},
    
-    {col_id => 'unit',
-       type => 'text',
-       name => LedgerSMB::Report::text('Unit'),},
-   
     {col_id => 'onhand',
        type => 'text',
        name => LedgerSMB::Report::text('On Hand'),},
+   
+    {col_id => 'unit',
+       type => 'text',
+       name => LedgerSMB::Report::text('Unit'),},
    
     {col_id => 'rop',
        type => 'text',
@@ -263,7 +263,7 @@ sub columns {
     {col_id => 'microfiche',
        type => 'text',
        name => LedgerSMB::Report::text('Microfiche'),},
- 
+
     {col_id => 'notes',
        type => 'text',
        name => LedgerSMB::Report::text('Notes'),},
