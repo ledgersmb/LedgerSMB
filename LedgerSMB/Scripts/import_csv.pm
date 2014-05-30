be270aa09daac3274c9a8d064da358ded079077a
Chris Travers <chris@metatrontech.com>
2014-05-30 00:27:57 -0700

    Avoiding double render after import on failure

--- LedgerSMB/Scripts/import_csv.pm
+++ LedgerSMB/Scripts/import_csv.pm
@@ -100,6 +100,7 @@ our $aa_multi = sub {
                       
                        AA->post_transaction($request->{_user}, $form);
                    }
+                   return 1;
                };
 our $process = {
    gl       => sub {
@@ -373,9 +374,10 @@ sub run_import {
     if (ref($preprocess->{$request->{type}}) eq 'CODE'){
         $preprocess->{$request->{type}}($request, \@entries);
     }
-    $process->{$request->{type}}($request, \@entries) || begin_import($request);
-    if (ref($postprocess->{$request->{type}}) eq 'CODE'){
-        $postprocess->{$request->{type}}($request, \@entries);
+    if ($process->{$request->{type}}($request, \@entries)){
+        if (ref($postprocess->{$request->{type}}) eq 'CODE'){
+            $postprocess->{$request->{type}}($request, \@entries);
+        }
     }
     begin_import($request);
 }
