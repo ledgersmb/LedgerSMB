08c2adefa9d93ea83db3e9f0f08087b47148778b
ehuelsmann <none@none>
2014-05-31 19:31:00 +0200

    Follow-up to fix #1150; now we have a newcode section, do we still need to fallback to oldcode... (grafted from 8eba9525b879f012f730df14e7ef90a7e75e5f67)
    --HG--
    extra : source : 8eba9525b879f012f730df14e7ef90a7e75e5f67

--- LedgerSMB/Scripts/user.pm
+++ LedgerSMB/Scripts/user.pm
@@ -41,11 +41,10 @@ Displays the preferences screen.  No inputs needed.
 sub preference_screen {
     my ($request) = @_;
     my $user = LedgerSMB::DBObject::User->new({base => $request});
-    $user->get;
     $user->get_option_data;
 
     my $template = LedgerSMB::Template->new(
-            user     => $user, 
+            user     => $user->{_user}, 
             locale   => $request->{_locale},
             path     => 'UI/users',
             template => 'preferences',
@@ -55,6 +54,7 @@ sub preference_screen {
     my $creds = LedgerSMB::Auth::get_credentials();
     $user->{login} = $creds->{login};
     $user->{password_expires} =~ s/:(\d|\.)*$//;
+    $user->{user} = $user->{_user};
     $template->render($user);
 }
 
