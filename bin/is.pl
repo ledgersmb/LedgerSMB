34361be26e9009eaa4183976efa8b70fe9a8689d
Chris Travers <chris@efficito.com>
2014-06-05 07:04:58 -0700

    Adding logic to reverse manual taxes
    --HG--
    extra : source : dd0565e2f8e622951dee4cbd71d5bb4bccdd70b0

--- bin/is.pl
+++ bin/is.pl
@@ -641,6 +641,9 @@ sub void {
         );
         delete $form->{paid_1};
     }
+    if ($form->{manual_tax}){
+        $form->{"mt_amount_$_"} *= -1 for keys %{$form->{taxes}};
+    }
     &post_as_new;
 }
 
