210cea5206266e708c9d651beba6f6d0530bd598
Chris Travers <chris@efficito.com>
2014-08-22 00:49:17 -0700

    Beginning move to PGObject

---
+++ LedgerSMB/PGOld.pm
@@ -0,0 +1,15 @@
+=head1 NAME
+
+LedgerSMB::PGOld - Old DBObject replacement for 1.3-era LedgerSMB code
+
+=head1 SYNPOSIS
+
+This is like DBObject but uses the PGObject::Simple for base functionality.
+
+=head1 METHODS
+
+See PGObject::Simple
+
+=cut
+
+1; 
