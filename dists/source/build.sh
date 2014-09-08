eed301518dcbb8a433730700ef6bb6c74c1fd389
Chris Travers <chris@efficito.com>
2014-09-08 05:53:24 -0700

    Updating version numbers for 1.3.43

--- dists/source/build.sh
+++ dists/source/build.sh
@@ -2,7 +2,7 @@
 
 # Simple script to prepare for release
 
-version="1.3.42";
+version="1.3.43";
 build_d="../release";
 
 if test -d $build_d/ledgersmb; then
--- sql/Pg-database.sql
+++ sql/Pg-database.sql
@@ -1027,7 +1027,7 @@ sinumber|1
 sonumber|1
 yearend|1
 businessnumber|1
-version|1.3.42
+version|1.3.43
 closedto|\N
 revtrans|1
 ponumber|1
