cb50b4e926b9c33871773499d9ba21c31628bb95
Chris Travers <chris@efficito.com>
2014-09-16 03:45:56 -0700

    fixing accidental input-sourced syntax error

--- LedgerSMB/Report/Listings/Business_Unit.pm
+++ LedgerSMB/Report/Listings/Business_Unit.pm
@@ -93,8 +93,7 @@ sub name { LedgerSMB::Report::text('Business Unit List'); }
 sub run_report {
     my $self = shift;
     $self->rows([
-      h
-map { { %$_, row_id => $_->{id}, } }
+      map { { %$_, row_id => $_->{id}, } }
        $self->call_dbmethod(funcname => 'business_unit__list_by_class',
                               args => { class_id => $self->id } )
     ]);
