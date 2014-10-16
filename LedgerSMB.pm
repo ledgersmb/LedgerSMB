be172d4d52aa1c616e81ce81cad488813a925584
Chris Travers <chris@efficito.com>
2014-10-16 07:14:15 -0700

    Updating version numbers for 1.3.45

--- LedgerSMB.pm
+++ LedgerSMB.pm
@@ -223,7 +223,7 @@ $CGI::Simple::POST_MAX = -1;
 
 package LedgerSMB;
 use base 'LedgerSMB::Request';
-our $VERSION = '1.3.44';
+our $VERSION = '1.3.45';
 
 my $logger = Log::Log4perl->get_logger('LedgerSMB');
 
--- LedgerSMB/Form.pm
+++ LedgerSMB/Form.pm
@@ -166,8 +166,8 @@ sub new {
     #menubar will be deprecated, replaced with below
     $self->{lynx} = 1 if ( ( defined $self->{path} ) && ( $self->{path} =~ /lynx/i ) );
 
-    $self->{version}   = "1.3.44";
-    $self->{dbversion} = "1.3.44";
+    $self->{version}   = "1.3.45";
+    $self->{dbversion} = "1.3.45";
 
     bless $self, $type;
 
