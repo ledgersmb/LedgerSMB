7f62d620b468afd65a05a6f3bc26fff0869ae03d
Chris Travers <chris@efficito.com>
2014-11-08 17:25:56 -0800

    Fix for 1271
    --HG--
    extra : source : 0b83356d20d4cf6ec4dc8a63796610319c44d66e

--- bin/ir.pl
+++ bin/ir.pl
@@ -436,7 +436,7 @@ function on_return_submit(event){
     $form->hide_form(
         qw(id title vc type terms creditlimit creditremaining closedto locked 
            shipped oldtransdate recurring reverse batch_id subtype form_id
-           nextsub default_reportable address city)
+           nextsub default_reportable address city cash_accno)
     );
 
     print qq|
