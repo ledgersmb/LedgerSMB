2ba7b1b6c1e91e8c40dd80900780976e03217d60
Chris Travers <chris@efficito.com>
2014-11-19 09:56:27 -0800

    Adding gapless ar setting, for gapless invoice numbering
    --HG--
    extra : source : 2482ce245828d74395183c8107bc5912a4ef87ae

--- sql/modules/Fixes.sql
+++ sql/modules/Fixes.sql
@@ -122,3 +122,10 @@ UPDATE invoice SET vendor_sku = (select min(partnumber) from partsvendor
                                 )
  WHERE trans_id in (select id from ap);
 COMMIT;
+
+BEGIN;
+ALTER TABLE ar ADD setting_sequence TEXT;
+ALTER TABLE ar DROP CONSTRAINT ar_invnumber_key;
+ALTER TABLE ar ADD CHECK(invnumber is not null OR not approved);
+CREATE UNIQUE INDEX ar_invnumber_key_p ON ar(invnumber) where invnumber is not null;
+COMMIT;
