4a6299da7376586b64dc9ce44bfd9eba71db56fc
Chris Travers <chris@efficito.com>
2014-12-17 22:10:42 -0800

    Removing some usage of check printing where it doesn't make sense anymore.
    --HG--
    extra : source : 35907d051a18b47e2921baa3618e10e2fe3ec565

--- sql/modules/Fixes.sql
+++ sql/modules/Fixes.sql
@@ -124,11 +124,11 @@ UPDATE invoice SET vendor_sku = (select min(partnumber) from partsvendor
 COMMIT;
 
 BEGIN;
-ALTER TABLE ar ADD setting_sequence TEXT;
 ALTER TABLE ar DROP CONSTRAINT ar_invnumber_key;
 ALTER TABLE ar ADD CHECK(invnumber is not null OR not approved);
 CREATE UNIQUE INDEX ar_invnumber_key_p ON ar(invnumber) where invnumber is not null;
 COMMIT;
+ALTER TABLE ar ADD setting_sequence TEXT;
 
 BEGIN;
 UPDATE menu_attribute SET value = 'contact.pl'
