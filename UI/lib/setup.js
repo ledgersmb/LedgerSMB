ec57246c30fb179fe91e44d2d98407c872ebf509
Erik Huelsmann <ehuels@gmail.com>
2015-06-06 18:49:18 +0000

    * Require the parser explicitly so it can be called implicitly.

--- UI/lib/setup.js
+++ UI/lib/setup.js
@@ -1,28 +1,8 @@
-/* construct_form_node(query, cls, registry,
- *                     textbox, checkbox, radio, select, button, textarea,
- *                     input)
- * This constructs the appropriate dojo/dijit object from the input provided and
- * returns it to the calling function.  query and cls are needed for select box
- * and textbox class detection.  input is the node.  The others are appropriate
- * dijit/dojo classes for the widgets.
- */
-/*
 
-/* Set up form.tabular forms.  
- * Supports the following additional classes for setting columns
- * cols-1
- * cols-2
- * cols-3
- *
- * Normally tabular will attach to the form element in most simple forms, but
- * for more complex ones, you can use div instead.
- *
- * Also sets up textboxes, checkboxes, and date pickers in the forms.
- *
- * As of first commit only setting up table containers.
- */
-
-require(['lsmb/lib/Loader', 'dojo/cookie', 'dojo/domReady!'],
+/* Note, this is the first code being executed. If we don't required
+   the parser here, the "onLoad" parse event isn't going to fire. */
+require(['lsmb/lib/Loader', 'dojo/cookie', 'dojo/parser',
+	 'dojo/domReady!'],
 function(l){
     if (location.search.indexOf('&dojo=no') != -1) {
         dojo.cookie("lsmb-dojo-disable", "yes", {});
