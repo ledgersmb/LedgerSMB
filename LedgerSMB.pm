335567bd00f3965bb630890f00fa6cad20aecb3c
Erik Huelsmann <ehuels@gmail.com>
2015-08-28 09:41:59 -0600

    Update version

--- LedgerSMB.pm
+++ LedgerSMB.pm
@@ -179,7 +179,7 @@ use Try::Tiny;
 use DBI;
 
 use base qw(LedgerSMB::Request);
-our $VERSION = '1.4.15-dev';
+our $VERSION = '1.4.15';
 
 my $logger = Log::Log4perl->get_logger('LedgerSMB');
 
--- dists/rpm/build.sh
+++ dists/rpm/build.sh
@@ -2,7 +2,7 @@
 
 # One must run the source file build first.
 
-VERSION="1.4.15-dev"
+VERSION="1.4.15"
 hg archive -ttgz ~/ledgersmb.tar.gz
 
 # Remove Dojo
