a1b37598508a287df71b70bd5de51ddbfa4e2f4e
Erik Huelsmann <ehuels@gmail.com>
2015-08-31 11:09:28 +0000

    * Add translations to the 'chart' view (meaning nearly everywhere) (grafted from 3c36365ac6385edd3d54fc00607f6469f0cbb8ac)

--- sql/Pg-database.sql
+++ sql/Pg-database.sql
@@ -171,22 +171,34 @@ CREATE TABLE account_link (
 );
 
 CREATE VIEW chart AS
-SELECT id, accno, description,
+SELECT ah.id, ah.accno, coalesce(ht.description, ah.description) as description,
        'H' as charttype, NULL as category, NULL as link,
-       parent_id as account_heading,
+       ah.parent_id as account_heading,
        null as gifi_accno, false as contra,
        false as tax
-  from account_heading
+  from account_heading ah
+  LEFT JOIN (SELECT ht.trans_id, ht.description FROM account_heading_translation ht
+                                    INNER JOIN user_preference up ON
+                                          up.language = ht.language_code
+                                    INNER JOIN users ON up.id = users.id
+                                    WHERE users.username = SESSION_USER) ht
+         ON ah.id = ht.trans_id
 UNION
-select c.id, c.accno, c.description,
+select c.id, c.accno, coalesce(at.description, c.description),
        'A' as charttype, c.category, concat_colon(l.description) as link,
        heading, gifi_accno, contra,
        tax
   from account c
   left join account_link l
     ON (c.id = l.account_id)
-group by c.id, c.accno, c.description, c.category, c.heading,
-         c.gifi_accno, c.contra, c.tax;
+  LEFT JOIN (SELECT at.trans_id, at.description FROM account_translation at
+                                    INNER JOIN user_preference up ON
+                                          up.language = at.language_code
+                                    INNER JOIN users ON up.id = users.id
+                                    WHERE users.username = SESSION_USER) at
+         ON c.id = at.trans_id
+group by c.id, c.accno, coalesce(at.description, c.description), c.category,
+         c.heading, c.gifi_accno, c.contra, c.tax;
 
 GRANT SELECT ON chart TO public;
 
