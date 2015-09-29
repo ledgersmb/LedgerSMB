5f6f64cc8aa5c15bf4b322e904473b4376ee2807
Erik Huelsmann <ehuels@gmail.com>
2015-09-29 13:59:50 -0600

    Update version

--- LedgerSMB/Form.pm
+++ LedgerSMB/Form.pm
@@ -174,8 +174,8 @@ sub new {
     #menubar will be deprecated, replaced with below
     $self->{lynx} = 1 if ( ( defined $self->{path} ) && ( $self->{path} =~ /lynx/i ) );
 
-    $self->{version}   = "1.4.16-dev";
-    $self->{dbversion} = "1.4.16-dev";
+    $self->{version}   = "1.4.16";
+    $self->{dbversion} = "1.4.16";
 
     bless $self, $type;
 
--- dists/rpm/build.sh
+++ dists/rpm/build.sh
@@ -2,7 +2,7 @@
 
 # One must run the source file build first.
 
-VERSION="1.4.16-dev"
+VERSION="1.4.16"
 hg archive -ttgz ~/ledgersmb.tar.gz
 
 # Remove Dojo
