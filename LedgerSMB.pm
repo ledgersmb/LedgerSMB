f41904c314a8a419bf8c363e5b171ea370ea1a4f
Chris Travers <chris@efficito.com>
2015-10-17 14:44:09 +0200

    Removed broken handling of old datestyle settings

--- LedgerSMB.pm
+++ LedgerSMB.pm
@@ -337,15 +337,6 @@ sub new {
     }
     $self->get_user_info;
 
-    my %date_setting = (
-        'mm/dd/yy' => "ISO, MDY",
-        'mm-dd-yy' => "ISO, MDY",
-        'dd/mm/yy' => "ISO, DMY",
-        'dd-mm-yy' => "ISO, DMY",
-        'dd.mm.yy' => "ISO, DMY",
-    );
-
-    $self->{dbh}->do("set DateStyle to '".$date_setting{$self->{_user}->{dateformat}}."'");
     $self->{_locale}=LedgerSMB::Locale->get_handle($self->{_user}->{language})
      or $self->error(__FILE__.':'.__LINE__.": Locale not loaded: $!\n");
 
