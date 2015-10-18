2b0886d946832326757e853942020520177feee8
Chris Travers <chris@efficito.com>
2015-10-18 10:52:45 +0200

    fixed #883, need to create users with no new perms

--- LedgerSMB/Scripts/setup.pm
+++ LedgerSMB/Scripts/setup.pm
@@ -931,8 +931,6 @@ sub save_user {
                                             "users_manage",
                                          ]
         );
-    } else {
-        $request->error($request->{_locale}->text('No Permissions Assigned'));
    }
    $request->{dbh}->commit;
    $request->{dbh}->begin_work;
@@ -1089,6 +1087,7 @@ sub create_initial_user {
    @{$request->{perm_sets}} = (
        {id => '0', label => $locale->text('Manage Users')},
        {id => '1', label => $locale->text('Full Permissions')},
+       {id => '-1', label => $locale->text('No changes')},
    );
     my $template = LedgerSMB::Template->new(
                    path => 'UI/setup',
