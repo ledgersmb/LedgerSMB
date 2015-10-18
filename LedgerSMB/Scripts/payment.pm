bdc77312635afd85b403e78786f66ddbc413016c
Chris Travers <chris@efficito.com>
2015-10-18 13:35:23 +0200

    silencing critic

--- LedgerSMB/Scripts/payment.pm
+++ LedgerSMB/Scripts/payment.pm
@@ -483,7 +483,7 @@ sub display_payments {
             $invoice->[5] = $invoice->[5]->to_output(money  => 1);
             $invoice->[6] = $invoice->[6]->to_output(money  => 1);
             my $fld = "payment_" . $invoice->[0];
-           
+
             if (defined $payment->{"net_$invoice->[0]"} ){
                 $invoice->[6] = $payment->{"$fld"};
             } else {
