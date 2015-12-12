c12ecd279169928b40ee2efb1443c38ba4444652
Erik Huelsmann <ehuels@gmail.com>
2015-12-12 22:49:22 +0000

    * Always initialize variable (log silencing)

--- LedgerSMB/Report/Balance_Sheet.pm
+++ LedgerSMB/Report/Balance_Sheet.pm
@@ -75,7 +75,7 @@ sub run_report {
     my @lines = $self->call_dbmethod(funcname => 'report__balance_sheet');
     my ($row) = $self->call_procedure(funcname => 'setting_get',
                                       args => [ 'earn_id' ]);
-    my $earn_id = ($row) ? $row->{value} : -1;
+    my $earn_id = ($row && $row->{value}) ? $row->{value} : -1;
     my $row_map = ($self->gifi) ?
         sub { my ($line) = @_;
               return ($line->{account_type} eq 'H')
