617de294c62cfb910ac1055c4e60b550e08732bc
Erik Huelsmann <ehuels@gmail.com>
2015-12-14 20:56:44 +0000

    * Move db function call to PGObject paradigm

--- LedgerSMB/Scripts/reports.pm
+++ LedgerSMB/Scripts/reports.pm
@@ -101,7 +101,7 @@ sub start_report {
         funcname => 'employee__all_salespeople'
     );
     @{$request->{languages}} = $request->call_procedure(
-        procname => 'person__list_languages'
+        funcname => 'person__list_languages'
         );
 
     $request->{earn_id} = LedgerSMB::Setting->get('earn_id');
