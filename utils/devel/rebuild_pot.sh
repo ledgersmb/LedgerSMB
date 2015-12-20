dc0cfbf8ace63c90ec47db31b7c00870fbcc5051
Erik Huelsmann <ehuels@gmail.com>
2015-12-20 17:09:23 +0000

    * Extract translatable strings from templates and SQL (deprecates DB extraction)

--- utils/devel/rebuild_pot.sh
+++ utils/devel/rebuild_pot.sh
@@ -1,33 +1,23 @@
-#!/usr/bin/bash
-
-# EXTRACTING DB STRINGS
-echo "select 'text(''' || label || ''')' FROM menu_node" | 
- psql -U postgres > tools/dbstrings
-
-echo "select 'text(''' || class || ''')' FROM entity_class" | 
- psql -U postgres >> tools/dbstrings
-
-echo "select 'text(''' || class || ''')' FROM batch_class" | 
- psql -U postgres >> tools/dbstrings
-
-echo "select 'text(''' || class || ''')' FROM contact_class" | 
- psql -U postgres >> tools/dbstrings
-
-echo "select 'text(''' || class || ''')' FROM location_class" | 
- psql -U postgres >> tools/dbstrings
+#!/bin/sh
 
 
 # SETTING UP FILE LIST
 find . -name '*.pl' | grep -v blib > tools/files
 find . -name '*.html' | grep -v blib >> tools/files
-find . -name '*.pm' | grep -v blib | grep -v Num2text | 
+find . -name '*.tex' | grep -v blib >> tools/files
+find . -name '*.csv' | grep -v blib >> tools/files
+find . -name '*.pm' | grep -v blib | grep -v Num2text | \
   grep -v LaTeX >> tools/files
 
 # EXTRACT STRINGS AND MERGE WITH .POT
 
-xgettext -f tools/files -ktext -o locale/LedgerSMB.pot 
-
-xgettext -ktext -j -o locale/LedgerSMB.pot -a tools/dbstrings --language=perl
+xgettext -f tools/files -ktext -o locale/LedgerSMB.pot
+find UI/ -name '*.html' | grep -v dojo/ | \
+   utils/devel/extract-template-translations > locale/templates.pot
+msgmerge locale/LedgerSMB.pot locale/templates.pot -o locale/LedgerSMB.pot-tmp
+utils/devel/extract-sql < sql/Pg-database.sql > locale/sql.pot
+msgmerge locale/LedgerSMB.pot-tmp locale/sql.pot -o locale/LedgerSMB.pot
+xgettext -ktext -j -o locale/LedgerSMB.pot --language=perl
 
 # Merge with .po files
 
