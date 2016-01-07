4330c990e41e0684e52cbbeb21ef37988e8548ab
Erik Huelsmann <ehuels@gmail.com>
2016-01-07 20:16:34 +0000

    * Fix 1.3 recurrence migration

--- sql/upgrade/1.3-1.4.sql
+++ sql/upgrade/1.3-1.4.sql
@@ -541,8 +541,12 @@ INSERT INTO user_preference
 SELECT id, language, stylesheet, printer, dateformat, numberformat
   FROM lsmb13.user_preference;
 update user_preference set dateformat = dateformat || 'yy' where length(dateformat) = 8;
---INSERT INTO recurring SELECT * FROM lsmb13.recurring;--tshvr4 fields differ
-INSERT INTO recurring(id,reference,startdate,nextdate,enddate,howmany,payment) SELECT id,reference,startdate,nextdate,enddate,howmany,payment FROM lsmb13.recurring;
+
+INSERT INTO recurring (id, reference, startdate, nextdate,
+                       enddate, howmany, payment, recurring_interval)
+ SELECT id, reference, startdate, nextdate, enddate, howmany, payment,
+        repeat || ' ' || unit as recurring_interval
+   FROM lsmb13.recurring;
 INSERT INTO payment_type SELECT * FROM lsmb13.payment_type;
 INSERT INTO recurringemail SELECT * FROM lsmb13.recurringemail;
 INSERT INTO recurringprint SELECT * FROM lsmb13.recurringprint;
