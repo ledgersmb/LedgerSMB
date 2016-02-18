1e3ee0ab9ce02ca9612d0d615adb4cd532acd1ec
Erik Huelsmann <ehuels@gmail.com>
2016-02-18 03:39:52 -0700

    Update version

--- dists/rpm/build.sh
+++ dists/rpm/build.sh
@@ -2,7 +2,7 @@
 
 # One must run the source file build first.
 
-VERSION="1.4.25-dev"
+VERSION="1.4.25"
 hg archive -ttgz ~/ledgersmb.tar.gz
 
 # Remove Dojo
--- dists/source/build.sh
+++ dists/source/build.sh
@@ -2,7 +2,7 @@
 
 # Simple script to prepare for release
 
-version="1.4.25-dev";
+version="1.4.25";
 build_d="../release";
 
 if test -d $build_d/ledgersmb; then
