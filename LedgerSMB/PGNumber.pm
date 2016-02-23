e811097c4d1cde00dc401d57f14e74d2f49a43b7
Chris Travers <chris@efficito.com>
2016-02-23 07:40:55 +0100

    Fixing at least one case where the sales order crashes with a bad number error

--- LedgerSMB/PGNumber.pm
+++ LedgerSMB/PGNumber.pm
@@ -130,8 +130,7 @@ The input is formatted.
 =cut
 
 sub from_input {
-    my $self   = shift @_;
-    my $string = shift @_;
+    my ($self, $string) = @_;
     return $string if eval { $string->isa(__PACKAGE__) };
     #tshvr4 avoid 'Use of uninitialized value $string in string eq'
     if(!defined $string || $string eq ''){
