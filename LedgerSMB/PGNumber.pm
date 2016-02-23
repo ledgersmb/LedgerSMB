14b3196de3c0c55ccc9c5aa5d12b0bebb6d91c02
Chris Travers <chris@efficito.com>
2016-02-23 07:59:39 +0100

    reverting stylistic change that caused problem

--- LedgerSMB/PGNumber.pm
+++ LedgerSMB/PGNumber.pm
@@ -137,7 +137,8 @@ The input is formatted.
 =cut
 
 sub from_input {
-    my ($self, $string) = @_;
+    my $self = shift;
+    my $string = shift;
     { # pre-5.14 compatibility block
         local ($@); # pre-5.14, do not die() in this block
         return $string if eval { $string->isa(__PACKAGE__) };
