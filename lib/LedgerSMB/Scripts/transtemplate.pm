4bbdce4b8402fd2b345ef58f969252e9a32628d3
Chris Travers <chris@efficito.com>
2016-03-08 20:11:06 +0100

    fixing xsrf issues

--- lib/LedgerSMB/Scripts/transtemplate.pm
+++ lib/LedgerSMB/Scripts/transtemplate.pm
@@ -51,6 +51,7 @@ sub view {
     my $transtemplate = LedgerSMB::DBObject::TransTemplate->new({base => $request});
     $transtemplate->get;
     my $script = $template_dispatch->{$request->{entry_type}}->{script};
+    die "No dispatch entry for type $request->{entry_type}" unless $script;
     $form->{script} = $script;
     $form->{script} =~ s/(bin|scripts)\///;
     delete $form->{id};
@@ -82,6 +83,7 @@ object for old code.
 sub convert_to_form{
     my ($trans, $form, $type) = @_;
     my %myconfig;
+    $form->{session_id} = $trans->{session_id};
     if ($type eq 'gl'){
         $form->{reference} = $trans->{reference};
         $form->{description} = $trans->{description};
