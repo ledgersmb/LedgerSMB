e1939c4d4b1f836c1d2390826d9066e0f32626b6
Erik Huelsmann <ehuels@gmail.com>
2016-07-07 23:55:55 +0200

    * Replace read-into-memory-once parts store with more consistent 'always reload'

---
+++ UI/js-src/lsmb/parts/PartRestStore.js
@@ -0,0 +1,46 @@
+define([
+    'dojo/store/JsonRest',
+    'dojo/store/Observable',
+    'dojo/request',
+    'dojo/_base/array',
+    'dojo/_base/declare',
+    'dojo/Evented',
+    'dojo/request'
+    ], function(
+        JsonRest,
+        Observable,
+        request,
+        array,
+        declare,
+        Evented,
+        xhr
+    ){
+        console.log('creating store');
+        var partsRest =
+            declare('lsmb/parts/PartRestStore',[JsonRest],
+                    {
+                        get: function(id) {
+                            var self = this;
+                            var r = request.get(this.target, {
+                                handleAs: "json",
+                                headers: this.headers
+                            });
+                            var rv = r.then(function(data){
+                                var theOne;
+                                array.forEach(data, function(item) {
+                                    if (id == item[self.idProperty]) {
+                                        theOne = item;
+                                    }
+                                });
+                                return theOne;
+                            });
+                            return rv;
+                        }
+                    });
+        var store = new Observable(new partsRest({
+            idProperty: 'partnumber',
+            target: '/parts.pl?action=partslist_json'
+        }));
+        console.log('done creating store');
+        return store;
+    });
--- UI/js-src/lsmb/parts/PartSelector.js
+++ UI/js-src/lsmb/parts/PartSelector.js
@@ -3,12 +3,14 @@ define([
     'dojo/_base/declare',
     'dojo/aspect',
     'dojo/topic',
-    'lsmb/parts/PartStore'
+    'dojo/when',
+    'lsmb/parts/PartRestStore'
     ], function(
       Select,
         declare,
         aspect,
         topic,
+        when,
         store
       ){
         var mySelect = new declare('lsmb/parts/PartSelector',[Select],{
@@ -16,7 +18,7 @@ define([
             queryExpr: "*${0}*",
             style: 'width: 15ex',
             highlightMatch: 'all',
-            searchAttr: 'text',
+            searchAttr: 'partnumber',
             labelAttr: 'label',
             autoComplete: false,
             initialValue:null,
@@ -28,9 +30,6 @@ define([
           postCreate:function(){
            var mySelf=this;
               this.inherited(arguments);
-              store.emitter.on("partstore_loadcomplete",function(){
-                  mySelf.set('value',mySelf.initialValue);
-           });
           },//postCreate
             startup:function(){
                 var self = this;
@@ -42,17 +41,20 @@ define([
                             self.set('value',selected[self.searchAttr]);
                         }));
                 this.on('change', function(newValue) {
-                    topic.publish('/invoice/part-select/'+self.linenum,
-                                  self.item);
+                    when(self.store.get(newValue),function(value){
+                        topic.publish('/invoice/part-select/'+self.linenum,
+                                      value);
+                    });
                 });
             }
         });
 
         aspect.around(mySelect, '_announceOption', function(orig) {
             return function (node) {
-                this.searchAttr = 'label';
+                var savedSearchAttr = this.searchAttr;
+                this.searchAttr = this.labelAttr;
                 var r = orig.call(this, node);
-                this.searchAttr = 'text';
+                this.searchAttr = savedSearchAttr;
                 return r;
             }
         });
