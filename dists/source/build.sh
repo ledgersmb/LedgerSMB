b3e29fb9e234635688cc8539f79e1c0c9d323ca1
Erik Huelsmann <ehuels@gmail.com>
2016-07-18 01:51:36 -0600

    Update version

--- dists/source/build.sh
+++ dists/source/build.sh
@@ -2,7 +2,7 @@
 
 # Simple script to prepare for release
 
-version="1.4.30";
+version="1.4.31-dev";
 build_d="../release";
 
 if test -d $build_d/ledgersmb; then
--- sql/Pg-database.sql
+++ sql/Pg-database.sql
@@ -1362,7 +1362,7 @@ sinumber|1
 sonumber|1
 yearend|1
 businessnumber|1
-version|1.4.30
+version|1.4.31-dev
 closedto|\N
 revtrans|1
 ponumber|1
