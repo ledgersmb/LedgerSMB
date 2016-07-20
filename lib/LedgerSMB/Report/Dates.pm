c4c5d624bdd65437646209bffb20fb5be2ecf941
Chris Travers <chris@efficito.com>
2016-07-20 16:56:40 +0200

    Making reports opt-in for setting to/from dates to min/max dates on acc_trans

--- lib/LedgerSMB/Report/Dates.pm
+++ lib/LedgerSMB/Report/Dates.pm
@@ -65,36 +65,39 @@ has date_from => (is => 'ro', lazy => '1', builder => 'from_date');
 
 has date_to => (is => 'ro', lazy => '1', builder => 'to_date');
 
+sub get_bracket_dates {
+    my ($self) = @_;
+    my $return_hashref = {};
+    my ($ref) = $self->call_dbmethod(funcname => 'lsmb__min_date');
+    if ($ref->{lsmb__min_date}){
+        my $dt = LedgerSMB::PGDate->from_db($ref->{lsmb__min_date});
+        $dt->is_time(0);
+        $return_hashref->{from_date} = $dt
+    }
+    ($ref) = $self->call_dbmethod(funcname => 'lsmb__max_date');
+    if ($ref->{lsmb__max_date}){
+         my $dt = LedgerSMB::PGDate->from_db($ref->{lsmb__max_date});
+         $dt->is_time(0);
+        $return_hashref->{to_date} = $dt
+    }
+    return $return_hashref;
+}
+
+
 sub _get_from_date {
     my ($self) = @_;
     if ($self->from_month and $self->from_year){
         my $date_string = $self->from_year . "-" .  $self->from_month . '-01';
         return LedgerSMB::PGDate->from_db($date_string, 'date');
     } else {
-        my ($ref) = $self->call_dbmethod(funcname => 'lsmb__min_date');
-        if ($ref->{lsmb__min_date}){
-            my $dt = LedgerSMB::PGDate->from_db($ref->{lsmb__min_date});
-            $dt->is_time(0);
-            return $dt;
-        } else {
-            return LedgerSMB::PGDate->from_db();
-        }
-
+        return LedgerSMB::PGDate->from_db();
     }
 }
 
 sub _get_to_date {
     my ($self) = @_;
     if (!$self->from_month or !$self->from_year or $self->interval eq 'none'){
-        my ($ref) = $self->call_dbmethod(funcname => 'lsmb__max_date');
-        if ($ref->{lsmb__max_date}){
-             my $dt = LedgerSMB::PGDate->from_db($ref->{lsmb__max_date});
-             $dt->is_time(0);
-             return $dt;
-        } else {
-            return LedgerSMB::PGDate->from_db();
-        }
-
+        return LedgerSMB::PGDate->from_db();
     }
     my $dateobj = $self->from_date;
     my $date = LedgerSMB::PGDate->from_db($dateobj->to_db); # copy, round trip
