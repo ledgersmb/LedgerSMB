cdf612c8452a3c4673256a0a94af1b1871451ffb
Chris Travers <chris@efficito.com>
2016-08-07 15:11:03 +0200

    proof of concept

--- UI/js-src/lsmb/users/ChangePassword.js
+++ UI/js-src/lsmb/users/ChangePassword.js
@@ -4,26 +4,46 @@ define(['lsmb/TabularForm',
          'dijit/_WidgetBase',
          'dijit/_TemplatedMixin',
          'dijit/_WidgetsInTemplateMixin',
+         'dijit/Registry',
          'dijit/_Container'],
        function(tabform, declare, _widgebase, _templatemixin, 
-                _widgets_in_templat_mixin, _container) {
+                _widgets_in_templat_mixin, registry, _container) {
              return declare ('lsmb/users/ChangePassword', 
                              [_widgetbase, templatemixin],
                  {
-                    templateString = 
+                    templateString :
 '<div data-dojo-type="dojox.form.PasswordValidator" name="pwValidate">' +
+  '<div id="pwtitle" width="100%" class="listheading">' +
+  '<div id="pwfeedback" class="lsmb-feedback hidden"></div>' +
   '<div data-dojo-type="lsmb.TabularForm" name="pwcontainer">' +
     '<div class="input-line">' +
-      '<input type="password" pwType="old" />' +
+      '<div id="old-pw" data-dojo-type="password" pwType="old"></div>' +
     '</div><div class="input-line">' +
-      '<input type="password" pwType="new" />' +
+      '<div id="new-pw" data-dojo-type="password" pwType="new"></div>' +
+    '</div><div class="input-line" id="pw-strength">' +
     '</div><div class="input-line">' +
+      '<div id="verify-pw" data-dojo-type="password" pwType="verify" ></div>' +
     '</div><div class="input-line">' +
-      '<input type="password" pwType="verify" />' +
-    '</div><div class="input-line">' +
+      '<button data-dojo-type="lsmb.PasswdButton" id="pw-change"></button>' +
     '</div><div class="input-line">' +
  '</div>' +
 '</div>';
+                   lstrings : {
+                            'title':        "Change Password"
+                            "old password": "Old Password",
+                            "new password": "New password",
+                            "verify":       "Verify",
+                            "change":       "Change Password",
+                            "strength":     "Strength" // TODO
+                   };
+                   buildRendering: function(){
+                       this.inherited(arguments);
+                       document.getElementById('pwtitle').innerHTML = this.lstrings.title;
+                       r.byId('old-pw').set('title', this.lstrings['old password']);
+                       r.byId('new-pw').set('title', this.lstrings['new password']);
+                       r.byId('verify-pw').set('title', this.lstrings['verify password']);
+                       r.byId('old-pw').set('title', this.lstrings.change;
+                   }
                  }
              );
        }
