1f9dfbd3929296baea31861f931dbd00d6cd76a2
Erik Huelsmann <ehuels@gmail.com>
2016-08-07 06:54:10 -0600

    Update version

--- dists/rpm/build.sh
+++ dists/rpm/build.sh
@@ -2,7 +2,7 @@
 
 # One must run the source file build first.
 
-VERSION="1.4.32-dev"
+VERSION="1.4.32"
 hg archive -ttgz ~/ledgersmb.tar.gz
 
 # Remove Dojo
--- sql/Pg-database.sql
+++ sql/Pg-database.sql
@@ -1362,7 +1362,7 @@ sinumber|1
 sonumber|1
 yearend|1
 businessnumber|1
-version|1.4.32-dev
+version|1.4.32
 closedto|\N
 revtrans|1
 ponumber|1
