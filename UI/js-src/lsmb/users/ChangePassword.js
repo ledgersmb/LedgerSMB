838fa3f26e52c9cee73c9da011a6b5c102e34d01
Chris Travers <chris@efficito.com>
2016-08-07 15:45:27 +0200

    Some progress

--- UI/js-src/lsmb/users/ChangePassword.js
+++ UI/js-src/lsmb/users/ChangePassword.js
@@ -4,45 +4,48 @@ define(['lsmb/TabularForm',
          'dijit/_WidgetBase',
          'dijit/_TemplatedMixin',
          'dijit/_WidgetsInTemplateMixin',
-         'dijit/Registry',
+         'dijit/registry',
          'dijit/_Container'],
-       function(tabform, declare, _widgebase, _templatemixin, 
+       function(tabform, declare, _widgetbase, _templatemixin, 
                 _widgets_in_templat_mixin, registry, _container) {
              return declare ('lsmb/users/ChangePassword', 
-                             [_widgetbase, templatemixin],
+                             [_widgetbase, _templatemixin],
                  {
-                    templateString :
-'<div data-dojo-type="dojox.form.PasswordValidator" name="pwValidate">' +
-  '<div id="pwtitle" width="100%" class="listheading">' +
-  '<div id="pwfeedback" class="lsmb-feedback hidden"></div>' +
-  '<div data-dojo-type="lsmb.TabularForm" name="pwcontainer">' +
-    '<div class="input-line">' +
-      '<div id="old-pw" data-dojo-type="password" pwType="old"></div>' +
-    '</div><div class="input-line">' +
-      '<div id="new-pw" data-dojo-type="password" pwType="new"></div>' +
-    '</div><div class="input-line" id="pw-strength">' +
-    '</div><div class="input-line">' +
-      '<div id="verify-pw" data-dojo-type="password" pwType="verify" ></div>' +
-    '</div><div class="input-line">' +
-      '<button data-dojo-type="lsmb.PasswdButton" id="pw-change"></button>' +
-    '</div><div class="input-line">' +
- '</div>' +
-'</div>';
+                    templateString : '\
+  <div>\
+  <div id="pwtitle" width="100%" class="listheading"></div> \
+  <div id="pwfeedback" class="lsmb-feedback hidden"></div> \
+<div data-dojo-type="dojox.form.PasswordValidator" id="pwValidate"> \
+  <div data-dojo-type="lsmb/TabularForm" name="pwcontainer" data-dojo-props="cols:1"> \
+    <div class="input-line"> \
+      <input id="old-pw" data-dojo-type="password" pwType="old"/> \
+    </div><div class="input-line"> \
+      <div id="new-pw" data-dojo-type="password" pwType="new"></div> \
+    </div><div class="input-line" id="pw-strength"> \
+    </div><div class="input-line"> \
+      <div id="verify-pw" data-dojo-type="password" pwType="verify" ></div> \
+    </div><div class="input-line"> \
+      <button data-dojo-type="lsmb.PasswdButton" id="pw-change"></button> \
+    </div><div class="input-line"> \
+ </div> \
+</div></div>',
                    lstrings : {
-                            'title':        "Change Password"
+                            'title':        "Change Password",
                             "old password": "Old Password",
                             "new password": "New password",
                             "verify":       "Verify",
                             "change":       "Change Password",
                             "strength":     "Strength" // TODO
-                   };
-                   buildRendering: function(){
+                   },
+                   startup: function(){
                        this.inherited(arguments);
                        document.getElementById('pwtitle').innerHTML = this.lstrings.title;
-                       r.byId('old-pw').set('title', this.lstrings['old password']);
-                       r.byId('new-pw').set('title', this.lstrings['new password']);
-                       r.byId('verify-pw').set('title', this.lstrings['verify password']);
-                       r.byId('old-pw').set('title', this.lstrings.change;
+                       document.getElementById('old-pw').title = this.lstrings['old password'];
+/*
+                       registry.byId('old-pw').set('title', this.lstrings['old password']);
+                       registry.byId('new-pw').set('title', this.lstrings['new password']);
+                       registry.byId('verify-pw').set('title', this.lstrings['verify password']);
+                       registry.byId('old-pw').set('title', this.lstrings.change);*/
                    }
                  }
              );
