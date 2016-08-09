906d0270381a2160eb37a44c046ea3d581e2f314
Chris Travers <chris@efficito.com>
2016-08-09 07:08:31 +0200

    blacklist creation tool

---
+++ tools/makeblacklist.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+use FindBin;
+use strict;
+use warnings;
+use lib "$FindBin::Bin/../lib";
+use 5.010; # say makes things easier
+no lib '.'; # can run from anywhere
+
+my %func = (); # set of functions as keys
+
+my $order;
+open ($order, '<', "$FindBin::Bin/../sql/modules/LOADORDER");
+for my $mod (<$order>) {
+    chomp($mod);
+    $mod =~ s/(\s+|#.*)//g;
+    next unless $mod; # skipping comment-only, whitespace-only, and blank lines
+    %func = (%func, process_mod($mod));
+    write_blacklist(sort keys %func); 
+}
+close ($order); ### return failure to execute the script?
+
+sub process_mod {
+    my ($mod) = @_;
+    open my $mod_h, '<', "$FindBin::Bin/../sql/modules/$mod";
+    my %func =  map { /FUNCTION (\w+)\(/i; ($1 => 1) }
+                grep { /CREATE (OR REPLACE )?FUNCTION \w+\(/i }  <$mod_h>;
+    close $mod_h;
+    return %func;
+}
+
+sub write_blacklist {
+    my @funcs = @_;
+    open my $bl, '>', "$FindBin::Bin/../sql/modules/BLACKLIST";
+    say $bl $_ for @funcs;
+    close $bl;
+}
