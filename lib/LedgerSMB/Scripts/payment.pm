13fa45c75b7ef973993c785a1b1386200735a6fc
Chris Travers <chris@efficito.com>
2016-08-19 15:40:08 +0200

    fixing overpayment posting

--- lib/LedgerSMB/Scripts/payment.pm
+++ lib/LedgerSMB/Scripts/payment.pm
@@ -1152,9 +1152,9 @@ for my $ref (0 .. $#array_options) {
          # same names are used for ap/ar accounts w/o the cash prefix.
          #
      my $sign = "$array_options[$ref]->{due_fx}" <=> 0;
-     if ( LedgerSMB::PGNumber($sign * $array_options[$ref]->{due_fx})->from_input->bround($LedgerSMB::Company_Config::decimal_places)
+     if ( $sign * LedgerSMB::PGNumber->from_input($array_options[$ref]->{due_fx})->bround($LedgerSMB::Company_Config::decimal_places)
             <
-          LedgerSMB::PGNumber($sign * $request_topay_fx_bigfloat)->from_input->bround($LedgerSMB::Company_Config::decimal_places)
+          $sign * LedgerSMB::PGNumber->from_input($request_topay_fx_bigfloat)->bround($LedgerSMB::Company_Config::decimal_places)
      ){
          # We need to store all the overpayments so we can use it on a new payment2 screen
          $unhandled_overpayment = $unhandled_overpayment + $request_topay_fx_bigfloat + $temporary_discount - $array_options[$ref]->{amount} ;
