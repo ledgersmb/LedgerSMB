48a14fb466f2f81cb575f2e07e46184c36f7ab04
Erik Huelsmann <ehuels@gmail.com>
2016-11-20 18:26:51 +0100

    * Follow-up naming the actual sequence

--- sql/changes/1.4/business_unit_class_id_seq-fix.sql
+++ sql/changes/1.4/business_unit_class_id_seq-fix.sql
@@ -1,2 +1,3 @@
 
-SELECT pg_catalog.setval('', (SELECT MAX(id) FROM business_unit_class), true);
+SELECT pg_catalog.setval('business_unit_class_id_seq',
+                         (SELECT MAX(id) FROM business_unit_class), true);
