f39ddf760a89ce7f2b4deeea1ff3c74423e3f5bd
Erik Huelsmann <ehuels@gmail.com>
2017-01-01 17:16:39 +0100

    * Fix #2462: Can't import Timecard ('action not defined: run_import')

--- lib/LedgerSMB/Scripts/import_csv.pm
+++ lib/LedgerSMB/Scripts/import_csv.pm
@@ -491,7 +491,7 @@ sub begin_import {
     # $request->{page_id} =~ s/_/-/;
     # $request->{page_id} .= '-import';
     $request->{page_id} = 'batch-import';
-    return $template->render_to_psgi($request);
+    return $template->render_to_psgi({ request => $request });
 }
 
 =head2 run_import
