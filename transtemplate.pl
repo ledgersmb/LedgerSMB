99dbcb2f7c374a0ce6d8494df225e9f8ed17c0ac
Erik Huelsmann <ehuels@gmail.com>
2017-02-08 15:48:48 +0100

    * Follow-up fixing up transtemplate's missing 'no lib' and 'use lib' statements
    Note:
    
    * Create level playing field by removing '.' on all Perls
    * Fix code to put install directory in @INC

--- transtemplate.pl
+++ transtemplate.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl
 
-require "lsmb-request.pl";
-#!/usr/bin/perl
+
+no lib '.';
+use FindBin;
+BEGIN {
+  unshift @INC, $FindBin::Bin unless $ENV{mod_perl}
+}
 
 require "lsmb-request.pl";
-#!/usr/bin/perl
 
-require "lsmb-request.pl";
