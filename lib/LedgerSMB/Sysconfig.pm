6c520d18dd675295b5b8e050b3a56dd0cf425cd0
sbts <info@sbts.com.au>
2017-02-20 01:39:52 +0800

    drop the envvar name from tempdir definintion, re-add setting HOME= to the end of Sysconfig.pm   improve handling of default for tempdir

--- lib/LedgerSMB/Sysconfig.pm
+++ lib/LedgerSMB/Sysconfig.pm
@@ -228,8 +228,7 @@ def 'fs_cssdir',
 # Temporary files stored at"
 def 'tempdir',
     section => 'main', # SHOULD BE 'paths' ????
-    default => sub { $ENV{TEMP} || '/tmp/ledgersmb' },
-    envvar => 'TEMP',
+    default => sub { $ENV{TEMP} && "$ENV{TEMP}/ledgersmb" || '/tmp/ledgersmb' }, # We can't specify envvar=>'TEMP' as that would overwrite TEMP with anything set in ledgersmb.conf. Conversely we need to use TEMP as the prefix for the default
     suffix => "-$EUID",
     doc => qq||;
 
@@ -475,6 +474,9 @@ sub check_permissions {
 
 
     my $tempdir = LedgerSMB::Sysconfig::tempdir();
+    # commit 6978b88 added this line to resolve issues if HOME isn't set
+    $ENV{HOME} = $tempdir;
+
 
     if(!(-d "$tempdir")){
         die_pretty( "$tempdir wasn't created.",
