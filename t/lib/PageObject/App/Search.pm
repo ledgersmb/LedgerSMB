3b79d090f3857489f33d446b86007751aced0bf5
Erik Huelsmann <ehuels@gmail.com>
2017-04-19 21:44:29 +0200

    * Implement PageObjects for search result access
    Includes adjustments to Dynatable to comply with maindiv requirements

--- t/lib/PageObject/App/Search.pm
+++ t/lib/PageObject/App/Search.pm
@@ -23,6 +23,7 @@ sub search {
             ->send_keys($args{$input});
     }
     $self->find('*button', text => $self->_search_btn_title)->click;
+    $self->session->page->body->maindiv->wait_for_content;
 }
 
 __PACKAGE__->meta->make_immutable;
