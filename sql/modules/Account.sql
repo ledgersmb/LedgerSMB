436217b9045bd1d07f22975b6f74e6540c244f29
Nick Prater <nick@npbroadcast.com>
2017-05-14 10:10:50 +0100

    Remove records from account_checkpoint before deleting an account
    Closing books creates zero entries in the account_checkpoint table for accounts
    which have never been used. We allow deletion of such accounts, so the associated
    zero-value account_checkpoint records must be deleted before the account to avoid
    foreign-key errors.

--- sql/modules/Account.sql
+++ sql/modules/Account.sql
@@ -494,10 +494,20 @@ CREATE OR REPLACE FUNCTION account__delete(in_id int)
 RETURNS BOOL AS
 $$
 BEGIN
-DELETE FROM tax WHERE chart_id = in_id;
-DELETE FROM account_link WHERE account_id = in_id;
-DELETE FROM account WHERE id = in_id;
-RETURN FOUND;
+    /* We only allow deletion of unused accounts.
+       Any account_checkpoint rows remaining will cause the final
+       DELETE FROM account to fail and this operation to be rolled back.
+     */
+    DELETE FROM account_checkpoint
+    WHERE account_id = in_id
+    AND amount = 0
+    AND debits = 0
+    AND credits = 0;
+
+    DELETE FROM tax WHERE chart_id = in_id;
+    DELETE FROM account_link WHERE account_id = in_id;
+    DELETE FROM account WHERE id = in_id;
+    RETURN FOUND;
 END;
 $$ LANGUAGE PLPGSQL;
 
