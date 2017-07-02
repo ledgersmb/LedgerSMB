28d19a68bae621cab2df8e3c0c540b37b0b58f8c
sbts <info@sbts.com.au>
2017-07-03 03:22:37 +0800

    Always Show Plack Logs on Failure

--- utils/test/Is_LSMB_running.sh
+++ utils/test/Is_LSMB_running.sh
@@ -1,6 +1,39 @@
 #!/bin/bash
 
-source utils/test/sysexits.shlib
+[[ -r utils/test/sysexits.shlib ]] && source utils/test/sysexits.shlib || \
+    [[ -r sysexits.shlib ]] && source sysexits.shlib
+
+if [[ -z $EX_NOHOST ]]; then
+    echo '=================================='
+    echo '=================================='
+    echo "== sysexits.shlib wasn't loaded =="
+    echo '=================================='
+    echo '=================================='
+    exit 99;
+fi
+
+# You can add to either of these two variables to skip this test during travis setup.
+# If it's skipped during this early run, it should also be run later in xt/60 which will never be skipped.
+Repo_early_skip_list+=('https://github.com/ylavoie/LedgerSMB.git');
+#Repo_early_skip_list+=('https://github.com/sbts/LedgerSMB.git');
+Repo_owner_early_skip_list+=('ylavoie');
+#Repo_owner_early_skip_list+=('sbts');
+
+[[ -x `which git` ]] && Repo_URL=`git config --get remote.origin.url` || Repo_URL='unknown'
+[[ "${Repo_URL,,}" == "unknown" ]] && {
+    [[ -r .git/config ]] && {
+        InSection=false;
+        while read -srt5 A B C; do
+            [[ "${A}${B}${C}" =~ .*[[].*[]] ]] && InSection=false;
+            $InSection && {
+                [[ "${A,,}" == 'url' ]] && Repo_URL="$C"
+                break;
+            }
+            [[ "${A,,}${B,,}${C,,}" =~ remote.*origin ]] && InSection=true;
+        done < .git/config
+    }
+}
+Repo_Slug_Owner="${TRAVIS_REPO_SLUG%%/*}"; Repo_Slug_Owner="${Repo_Slug_Owner:-unknown}"
 
 HELP() {
     E=${1:-$EX_USAGE}; shift;
@@ -29,6 +62,7 @@ HELP() {
 
 if [[ $1 == '--help' ]]; then HELP $EX_OK; shift; fi
 if [[ $1 == '--update' ]]; then UPDATE=true; shift; else UPDATE=false; fi
+if [[ $1 == '--early' ]]; then EARLY=true; shift; else EARLY=false; fi
 if (( ${#@} >1 )); then shift; HELP $EX_USAGE "unknown argument $@"; fi # we should have zero or one arguments left at this point.
 MaxDiff=${1:-1};    # maximum number of added or removed lines in the setup.pl static html
                     # Must always be greater than 1 to allow for site variations in admin users
@@ -40,17 +74,6 @@ current='/tmp/Is_LSMB_running.html'
 
 printf -v LF "\n";
 
-DIE() {
-    E=$1; shift;
-    T=$1; shift
-    echo '=============================================';
-    echo "$T";
-    echo '=============================================';
-    echo "$@";
-    echo '=============================================';
-    exit $E;
-}
-
 DUMPfile() {
     echo "== $1";
     if [[ -r "$1" ]]; then
@@ -59,20 +82,71 @@ DUMPfile() {
     echo '=============================';
 }
 
-[[ -e /tmp/Is_LSMB_running.log ]] && rm /tmp/Is_LSMB_running.log
-[[ -e /tmp/Is_LSMB_running.html ]] && rm /tmp/Is_LSMB_running.html
-
-if curl --progress-bar localhost:5001/setup.pl 2>/tmp/Is_LSMB_running.log >/tmp/Is_LSMB_running.html ; then
-    echo "Starman/Plack is Running";
-else    # fail early if starman is not running
-    E=$?;
-    echo '=============================';
-    echo "  Starman/plack Not running";
-    echo '=============================';
+DIE() {
+    E=$1; shift;
+    T=$1; shift
+    echo '=============================================';
+    echo "$T";
+    echo '=============================================';
+    echo "$@";
+    echo '=============================================';
     DUMPfile /tmp/Is_LSMB_running.log
     DUMPfile /tmp/Is_LSMB_running.html
     DUMPfile /tmp/plackup-error.log;
+    DUMPfile /tmp/plackup-access.log;
+
     exit $E;
+}
+
+SkipEarly() {
+    if $EARLY; then
+        if [[ "${Repo_early_skip_list[@]}" =~ ${Repo_URL} ]]; then DIE 0 "Skipping Test Is_LSMB_running" "repo $Repo_URL is in the 'skip early' list"; fi
+        if [[ "${Repo_owner_early_skip_list[@]}" =~ ${Repo_Slug_Owner} ]]; then DIE 0 "Skipping Test Is_LSMB_running" "Repo Owner $Repo_Slug_Owner is in the 'owner skip early' list"; fi
+    fi
+    echo "Commencing Early test for LSMB IS RUNNING"
+}
+
+[[ -e /tmp/Is_LSMB_running.log ]] && rm /tmp/Is_LSMB_running.log
+[[ -e /tmp/Is_LSMB_running.html ]] && rm /tmp/Is_LSMB_running.html
+
+WaitForPlackup() {
+    local -i seconds=0;
+    local processrunning=false;
+    local httpdrunning=false;
+    while (( i++ < 100 )); do # wait up to 10 seconds for plack or starman process to start
+        pgrep -f 'plackup' >/dev/null && { processrunning=true; echo "plackup started after $i * 0.1 seconds"; break; }
+        pgrep -f 'starman.*8080' >/dev/null && { processrunning=true; echo "starman started after $i * 0.1 seconds"; break; }
+        sleep 0.1;
+    done
+    if ! $processrunning; then
+        echo "The starman/plack process didn't start before the timeout";
+        return 1;
+    fi
+    i=0;
+    while (( i++ < 100 )); do # wait up to 10 seconds for plack or starman server to respond to a curl
+        pgrep -f 'plackup' >/dev/null
+        curl --max-time 60 --connect-timeout 60 --fail --silent localhost:5001/setup.pl 2>&1 >/dev/null && {
+            httpdrunning=true;
+            echo "starman/plackup responded after $i * 0.1 seconds"; 
+            break;
+        } #|| echo -en "\r$i"
+        sleep 0.1;
+    done
+    if ! $httpdrunning; then
+        echo "The starman/plack httpd didn't respond before the timeout";
+        return 1;
+    fi
+}
+
+SkipEarly
+WaitForPlackup || DIE $EX_NOHOST "ERROR: plackup or starman didn't start for some reason" "Check these logs for more info"
+
+
+if curl --max-time 60 --connect-timeout 60 --progress-bar localhost:5001/setup.pl 2>/tmp/Is_LSMB_running.log >/tmp/Is_LSMB_running.html ; then
+    echo "Starman/Plack is Running";
+else    # fail early if starman is not running
+    E=$?;
+    DIE $E "ERROR: Starman/plack Not running";
 fi
 
 if $UPDATE; then
