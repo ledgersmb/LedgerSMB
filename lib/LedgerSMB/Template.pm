e39a9cb16f716a9ab2abf5ef940d8329c69b2bc3
Yves Lavoie <ylavoie@yveslavoie.com>
2017-08-10 23:45:20 -0400

    Add decorated text translation

--- lib/LedgerSMB/Template.pm
+++ lib/LedgerSMB/Template.pm
@@ -568,6 +568,7 @@ sub _render {
     $cleanvars->{UNESCAPE} = sub { return $unescape->(@_); }
         if ($unescape && !$self->{no_escape});
     $cleanvars->{text} = sub { return $self->_maketext($escape, @_); };
+    $cleanvars->{decorated_text} = sub { return $self->{locale}->maketext(@_); };
     $cleanvars->{tt_url} = \&_tt_url;
     $cleanvars->{$_} = $self->{additional_vars}->{$_}
         for (keys %{$self->{additional_vars}});
