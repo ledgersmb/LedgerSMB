c2ee7f9e93707d77084c44ce428bf6b4b1ea1959
Yves Lavoie <ylavoie@yveslavoie.com>
2017-09-17 14:22:14 -0400

    Move country changes in change section

--- sql/changes/1.6/country_date_format.sql
+++ sql/changes/1.6/country_date_format.sql
@@ -1,6 +1,20 @@
 ALTER TABLE country
   ADD date_format TEXT;
 
+INSERT INTO country(short_name,name) VALUES ('BL','St. Barthelemy');
+INSERT INTO country(short_name,name) VALUES ('BQ','Caribbean Netherlands');
+INSERT INTO country(short_name,name) VALUES ('CW','Curacao');
+INSERT INTO country(short_name,name) VALUES ('DG','Diego Garcia');
+INSERT INTO country(short_name,name) VALUES ('EA','Ceuta & Melilla');
+INSERT INTO country(short_name,name) VALUES ('GG','Guernsey');
+INSERT INTO country(short_name,name) VALUES ('IC','Canary Islands');
+INSERT INTO country(short_name,name) VALUES ('ME','Montenegro');
+INSERT INTO country(short_name,name) VALUES ('MF','St. Martin');
+INSERT INTO country(short_name,name) VALUES ('SS','South Sudan');
+INSERT INTO country(short_name,name) VALUES ('SX','Sint Maarten');
+INSERT INTO country(short_name,name) VALUES ('TL','Timor-Leste');
+INSERT INTO country(short_name,name) VALUES ('XK','Kosovo');
+
 --UPDATE country SET date_format = '' WHERE short_name = 'AC'; -- Ascension Island
 --UPDATE country SET date_format = '' WHERE short_name = 'AD'; -- Andorra
 UPDATE country SET date_format = 'dd/MM/yyyy' WHERE short_name = 'AE'; -- United Arab Emirates
@@ -228,7 +242,7 @@ UPDATE country SET date_format = 'dd/MM/yyyy' WHERE short_name = 'SY'; -- Syria
 --UPDATE country SET date_format = '' WHERE short_name = 'TD'; -- Chad
 --UPDATE country SET date_format = '' WHERE short_name = 'TF'; -- French Southern Territories
 --UPDATE country SET date_format = '' WHERE short_name = 'TG'; -- Togo
-UPDATE country SET date_format = 'd/M/2555' WHERE short_name = 'TH'; -- Thailand
+UPDATE country SET date_format = 'd/M/yyyy' WHERE short_name = 'TH'; -- Thailand
 --UPDATE country SET date_format = '' WHERE short_name = 'TJ'; -- Tajikistan
 --UPDATE country SET date_format = '' WHERE short_name = 'TK'; -- Tokelau
 --UPDATE country SET date_format = '' WHERE short_name = 'TL'; -- Timor-Leste
