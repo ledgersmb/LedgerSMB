08bc6f5a58bb063ae9ab6806e57b7077f10678a4
Yves Lavoie <ylavoie@yveslavoie.com>
2017-10-06 22:27:21 +0000

    Remove SQL notices on DROP IF EXISTS

--- sql/changes/1.4/drop-je-functions.sql
+++ sql/changes/1.4/drop-je-functions.sql
@@ -1,2 +1,4 @@
+SET LOCAL client_min_messages=warning;
 DROP FUNCTION IF EXISTS je_get_default_lines();
 DROP FUNCTION IF EXISTS je_set_default_lines(integer);
+RESET client_min_messages;
--- sql/changes/1.5/trial_balance_cleanup.sql
+++ sql/changes/1.5/trial_balance_cleanup.sql
@@ -1,8 +1,10 @@
 BEGIN;
 
+SET LOCAL client_min_messages=warning;
 DROP TABLE IF EXISTS trial_balance__account_to_report;
 DROP TABLE IF EXISTS trial_balance__heading_to_report;
 DROP FUNCTION IF EXISTS trial_balance__list();
 DROP TABLE IF EXISTS trial_balance;
+RESET client_min_messages;
 
 COMMIT;
