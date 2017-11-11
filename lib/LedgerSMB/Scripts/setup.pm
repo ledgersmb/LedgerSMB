cba4b20dc64c61bdc5ea57fa3ef21fab912cf81d
Erik Huelsmann <ehuels@gmail.com>
2017-11-11 14:41:56 +0100

    * Fix database name used to add role prefix after copying database
    Note that due to using the wrong name key (company_name), the
      role prefix was added to the database being copied instead
      of the new one.
    
    Found by @sbts.

--- lib/LedgerSMB/Scripts/setup.pm
+++ lib/LedgerSMB/Scripts/setup.pm
@@ -327,7 +327,7 @@ sub copy_db {
     my $rc = $database->copy($request->{new_name})
            || die 'An error occurred. Please check your database logs.' ;
     my $dbh = LedgerSMB::Database->new(
-           +{%$database, (company_name => $request->{new_name})}
+           +{%$database, (dbname => $request->{new_name})}
     )->connect({ PrintError => 0, AutoCommit => 0 });
     $dbh->prepare(q{SELECT setting__set('role_prefix',
                                coalesce((setting_get('role_prefix')).value, ?))}
