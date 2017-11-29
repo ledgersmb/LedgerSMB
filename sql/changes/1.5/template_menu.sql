27eb50ef398a17c9fc19100fe539de99cd42a308
Erik Huelsmann <ehuels@gmail.com>
2017-11-29 13:46:28 +0100

    * Fix #3289: sql/changes/ infrasturcture incorrectly handling db transactions
    Close #3240: Eliminates errors from 'change loading' phase of database creation.
    
    Before this change, 'no transaction' changes were loaded in a single transaction due to
    how DBD::Pg works. Other changes (schema changes) were being wrapped with a
    BEGIN; ... COMMIT; block -- leading to a 'double transaction' being applied
    (causing the errors being output).
    
    Since 'no transaction' changes need to be executed statement-by-statement,
    individual statements need to be extracted from the change file which explains
    the need for an 'SQL parser' (better: recogniser).

--- sql/changes/1.5/template_menu.sql
+++ sql/changes/1.5/template_menu.sql
@@ -1,11 +1,11 @@
-INSERT INTO menu_node(id, parent, position, label)
+INSERT INTO menu_node (id, parent, position, label)
 values (29, 156, 18, 'Payment'), -- printPayment.html
        (30, 172, 18, 'Check Base'), -- check_base.tex
        (31, 172, 19, 'Multiple Checks'), -- check_multiple.tex
        (32, 172, 20, 'Envelope'), -- envelope
        (33, 172, 21, 'Shipping Label'); -- shipping_label.tex
 
-INSERT INTO menu_attribute(id, node_id, attribute, value)
+INSERT INTO menu_attribute (id, node_id, attribute, value)
 VALUES (256, 29, 'action', 'display'),
        (257, 29, 'format', 'html'),
        (258, 29, 'module', 'template.pl'),
