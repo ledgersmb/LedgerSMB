f730d15f904fc0f40cf9ceb204c39e5a88234aa4
Erik Huelsmann <ehuels@gmail.com>
2017-12-08 16:55:17 +0000

    Update version

--- dists/source/build.sh
+++ dists/source/build.sh
@@ -2,7 +2,7 @@
 
 # Simple script to prepare for release
 
-version="1.5.14";
+version="1.5.15-dev";
 build_d="../release";
 
 hg archive -t tgz $build_d/ledgersmb-${version}.tar.gz
--- lib/LedgerSMB.pm
+++ lib/LedgerSMB.pm
@@ -168,7 +168,7 @@ use Carp;
 use DBI;
 
 use base qw(LedgerSMB::Request);
-our $VERSION = '1.5.14';
+our $VERSION = '1.5.15-dev';
 
 my $logger = Log::Log4perl->get_logger('LedgerSMB');
 
