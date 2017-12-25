2e014b2b23b44d4885555ce7e8ae6cca45b8dcfa
Erik Huelsmann <ehuels@gmail.com>
2017-12-25 22:26:22 +0100

    * Factor entry-point loading into a PSGI middleware
    Note that this both simplifies the primary request handling as well as
     open up opportunities to reduce the responsibilities of LedgerSMB.pm
     e.g. by factoring out authentication and session management.

---
+++ lib/LedgerSMB/Middleware/DynamicLoadWorkflow.pm
@@ -0,0 +1,105 @@
+
+package LedgerSMB::Middleware::DynamicLoadWorkflow;
+
+=head1 NAME
+
+LedgerSMB::Middleware::DynamicLoadWorkflow - Workflow script loader
+
+=head1 SYNOPSIS
+
+ builder {
+   enable "+LedgerSMB::Middleware::DynamicLoadWorkflow";
+   $app;
+ }
+
+=head1 DESCRIPTION
+
+LedgerSMB::Middleware::DynamicLoadWorkflow makes sure the new-style
+workflow scripts have successfully been loaded before being dispatched to.
+
+This module implements the C<Plack::Middleware> protocol.
+
+=cut
+
+use strict;
+use warnings;
+use parent qw ( Plack::Middleware );
+
+use Module::Runtime qw/ use_module /;
+use List::Util qw{ none any };
+
+use LedgerSMB::PSGI::Util;
+
+=head1 METHODS
+
+=head2 $self->call($env)
+
+Implements C<Plack::Middleware->call()>.
+
+=cut
+
+
+sub call {
+    my $self = shift;
+    my ($env) = @_;
+
+    my $script_name = $env->{SCRIPT_NAME};
+    $script_name =~ m/([^\/\\\?]*)\.pl$/;
+    my $module = "LedgerSMB::Scripts::$1";
+    my $script = "$1.pl";
+
+    return LedgerSMB::PSGI::Util::internal_server_error(
+        'No workflow module specified!'
+        )
+        unless $module;
+
+    return LedgerSMB::PSGI::Util::internal_server_error(
+        "Unable to open module $module : $! : $@"
+        )
+        unless use_module($module);
+
+    my $req = Plack::Request->new($env);
+    my $action_name =
+        eval { $req->parameters->get_one('action') } // '__default';
+    my $action = $module->can($action_name);
+    return  LedgerSMB::PSGI::Util::internal_server_error(
+        "Action Not Defined: $action_name"
+        )
+        unless $action;
+
+    # This authorization stuff seems to belong elsewhere...
+    # but it's very much tied to our current style of request handling.
+    ###TODO: factor out in its own 'authentication middleware'
+    my $clear_session_actions =
+        $module->can('clear_session_actions');
+    $env->{'lsmb.want_cleared_session'} =
+        $clear_session_actions
+        && ( ! none { $_ eq $action_name }
+               $clear_session_actions->() );
+
+    my $no_db_actions = $module->can('no_db_actions');
+    $env->{'lsmb.want_db'} =
+        ! ($module->can('no_db')
+           || ($no_db_actions &&
+               any { $_ eq $action_name } $no_db_actions->()));
+
+    $env->{'lsmb.module'} = $module;
+    $env->{'lsmb.script'} = $script;
+    $env->{'lsmb.action'} = $action;
+    $env->{'lsmb.action_name'} = $action_name;
+    return $self->app->($env);
+}
+
+
+=head1 COPYRIGHT
+
+Copyright (C) 2017 The LedgerSMB Core Team
+
+This file is licensed under the Gnu General Public License version 2, or at your
+option any later version.  A copy of the license should have been included with
+your software.
+
+=cut
+
+
+1;
--- lib/LedgerSMB/PSGI.pm
+++ lib/LedgerSMB/PSGI.pm
@@ -17,12 +17,11 @@ use warnings;
 use LedgerSMB;
 use LedgerSMB::App_State;
 use LedgerSMB::Auth;
+use LedgerSMB::PSGI::Util;
 use LedgerSMB::Setting;
-use HTTP::Status qw( HTTP_OK HTTP_SEE_OTHER
-   HTTP_UNAUTHORIZED HTTP_INTERNAL_SERVER_ERROR HTTP_FOUND);
+use HTTP::Status qw( HTTP_OK HTTP_SEE_OTHER HTTP_UNAUTHORIZED HTTP_FOUND);
 
 use CGI::Emulate::PSGI;
-use Module::Runtime qw/ use_module /;
 use Try::Tiny;
 use List::Util qw{  none };
 
@@ -84,88 +83,45 @@ in LedgerSMB::Scripts::*.
 
 =cut
 
-sub _internal_server_error {
-    my ($msg, $title, $company, $dbversion) = @_;
-
-    $title //= 'Error!';
-    my @body_lines = ( '<html><body>',
-                       q{<h2 class="error">Error!</h2>},
-                       "<p><b>$msg</b></p>" );
-    push @body_lines, "<p>dbversion: $dbversion, company: $company</p>"
-        if $company || $dbversion;
-
-    push @body_lines, '</body></html>';
-
-    return [ HTTP_INTERNAL_SERVER_ERROR,
-             [ 'Content-Type' => 'text/html; charset=UTF-8' ],
-             \@body_lines ];
-}
-
 
 sub psgi_app {
     my $env = shift;
 
     my $auth = LedgerSMB::Auth::factory($env);
-    my $script_name = $env->{SCRIPT_NAME};
-    $script_name =~ m/([^\/\\\?]*)\.pl$/;
-    my $module = "LedgerSMB::Scripts::$1";
-    my $script = "$1.pl";
 
     my $psgi_req = Plack::Request->new($env);
-    my $request = LedgerSMB->new($psgi_req->parameters, $script,
+    my $request = LedgerSMB->new($psgi_req->parameters, $env->{'lsmb.script'},
                                  $env->{QUERY_STRING},
                                  $psgi_req->uploads, $psgi_req->cookies,
                                  $auth);
-    $request->{action} ||= '__default';
     my $locale = $request->{_locale};
     $LedgerSMB::App_State::Locale = $locale;
 
-    $request->{_script_handle} = $module;
-
-    return _internal_server_error('No workflow module specified!')
-        unless $module;
-
-    return _internal_server_error("Unable to open module $module : $! : $@")
-        unless use_module($module);
-
-    my $action = $module->can($request->{action});
-    return _internal_server_error("Action Not Defined: $request->{action}")
-        unless $action;
-
+    $request->{action} = $env->{'lsmb.action_name'};
+    my $action = $env->{'lsmb.action'};
     my ($status, $headers, $body);
     try {
-        my $clear_session_actions =
-            $module->can('clear_session_actions');
+        $request->clear_session
+            if $env->{'lsmb.want_cleared_session'};
 
-        if ($clear_session_actions
-            && ( !none{ $_ eq $request->{action} }
-                    $clear_session_actions->() )
-        ) {
-            $request->clear_session;
-        }
-        if (! $module->can('no_db')) {
-            my $no_db = $module->can('no_db_actions');
-
-            if (!$no_db
-                || ( $no_db && none { $_ eq $request->{action} } $no_db->())) {
-                if (! $request->_db_init()) {
-                    ($status, $headers, $body) =
-                        ( HTTP_UNAUTHORIZED,
-                          [ 'Content-Type' => 'text/plain; charset=utf-8',
-                            'WWW-Authenticate' => 'Basic realm=LedgerSMB' ],
-                          [ 'Please enter your credentials' ]
-                        );
-                    return; # exit 'try' scope
-                }
-                if (! $request->verify_session()) {
-                    ($status, $headers, $body) =
-                        ( HTTP_SEE_OTHER,
-                          [ 'Location' => 'login.pl?action=logout&reason=timeout' ],
-                          [] );
-                    return; # exit 'try' scope
-                }
-                $request->initialize_with_db();
+        if ($env->{'lsmb.want_db'}) {
+            if (! $request->_db_init()) {
+                ($status, $headers, $body) =
+                    ( HTTP_UNAUTHORIZED,
+                      [ 'Content-Type' => 'text/plain; charset=utf-8',
+                        'WWW-Authenticate' => 'Basic realm=LedgerSMB' ],
+                      [ 'Please enter your credentials' ]
+                    );
+                return; # exit 'try' scope
             }
+            if (! $request->verify_session()) {
+                ($status, $headers, $body) =
+                    ( HTTP_SEE_OTHER,
+                      [ 'Location' => 'login.pl?action=logout&reason=timeout' ],
+                      [] );
+                return; # exit 'try' scope
+            }
+            $request->initialize_with_db();
         }
         else {
             # Some default settings as we run without a user
@@ -199,9 +155,9 @@ sub psgi_app {
         eval { LedgerSMB::App_State->cleanup(); };
         if ($error !~ /^Died at/) {
             ($status, $headers, $body) =
-                @{_internal_server_error($_, 'Error!',
-                                         $request->{dbversion},
-                                         $request->{company})};
+                @{LedgerSMB::PSGI::Util::internal_server_error(
+                      $_, 'Error!',
+                      $request->{dbversion}, $request->{company})};
         }
     };
 
@@ -274,8 +230,11 @@ sub setup_url_space {
         mount "/$_.pl" => $old_app
             for ('aa', 'am', 'ap', 'ar', 'gl', 'ic', 'ir', 'is', 'oe', 'pe');
 
-        mount "/$_" => $psgi_app
-            for  (@LedgerSMB::Sysconfig::newscripts);
+        mount "/$_" => builder {
+            enable '+LedgerSMB::Middleware::DynamicLoadWorkflow';
+            $psgi_app;
+        }
+        for  (@LedgerSMB::Sysconfig::newscripts);
 
         mount '/stop.pl' => sub { exit; }
             if $coverage;
