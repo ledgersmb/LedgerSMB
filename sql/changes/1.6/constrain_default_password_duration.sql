d3e4d2531ebcd7006a43d2b78e1beebeaa7f1e39
Nick Prater <nick@npbroadcast.com>
2018-01-10 18:59:18 +0100

    Change sql file from C-style multiline comments to sql --

--- sql/changes/1.6/constrain_default_password_duration.sql
+++ sql/changes/1.6/constrain_default_password_duration.sql
@@ -1,27 +1,26 @@
-/* Clear an invalid password_duration setting,
- * which prevents user passwords from being changed.
- *
- * Bad values are replaced with NULL, which sets
- * 'infinity' as the password expiry for future
- * password changes.
- * 
- * There is no benefit in retaining the old, bad
- * values as they do nothing other than prevent
- * passwords being changed.
- */
+-- Clear an invalid password_duration setting,
+-- which prevents user passwords from being changed.
+--
+-- Bad values are replaced with NULL, which sets
+-- 'infinity' as the password expiry for future
+-- password changes.
+-- 
+-- There is no benefit in retaining the old, bad
+-- values as they do nothing other than prevent
+-- passwords being changed.
+
 UPDATE defaults
 SET value = NULL
 WHERE setting_key = 'password_duration' AND (
   value ~ '^([0-9]+[.]?[0-9]*|[.][0-9]+)$' OR
   value::numeric <= 0 OR
   value::numeric >= 3654
-)
+);
 
 
-/* Add a constraint that enforces a valid
- * password duration and constrains its value
- * to a sane range.
- */
+-- Add a constraint that enforces a valid
+-- password duration and constrains its value
+-- to a sane range.
 ALTER TABLE defaults
 ADD CONSTRAINT defaults_password_duration_check
 CHECK(
@@ -31,6 +30,6 @@ CHECK(
   (
     value ~ '^([0-9]+[.]?[0-9]*|[.][0-9]+)$' AND
     value::numeric > 0 AND
-    value::numeric < 3654  /* abitrary maximum 10 years */
+    value::numeric < 3654  -- abitrary maximum 10 years
   )
 );
