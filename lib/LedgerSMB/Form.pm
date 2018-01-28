1590af3a5ea147e0def530f1b30aa82d47327e6b
Erik Huelsmann <ehuels@gmail.com>
2018-01-28 18:07:29 +0000

    Update version

--- lib/LedgerSMB/Form.pm
+++ lib/LedgerSMB/Form.pm
@@ -181,8 +181,8 @@ sub new {
     #menubar will be deprecated, replaced with below
     $self->{lynx} = 1 if ( ( defined $self->{path} ) && ( $self->{path} =~ /lynx/i ) );
 
-    $self->{version}   = "1.5.16-dev";
-    $self->{dbversion} = "1.5.16-dev";
+    $self->{version}   = "1.5.16";
+    $self->{dbversion} = "1.5.16";
 
     bless $self, $type;
 
