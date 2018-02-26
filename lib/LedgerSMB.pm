884cfe81c4f64f9dab6d94e5cad2d57abf865667
Erik Huelsmann <ehuels@gmail.com>
2018-02-26 16:56:10 +0000

    Update version

--- lib/LedgerSMB.pm
+++ lib/LedgerSMB.pm
@@ -168,7 +168,7 @@ use Carp;
 use DBI;
 
 use base qw(LedgerSMB::Request);
-our $VERSION = '1.5.17-dev';
+our $VERSION = '1.5.17';
 
 my $logger = Log::Log4perl->get_logger('LedgerSMB');
 
--- sql/Pg-database.sql
+++ sql/Pg-database.sql
@@ -1347,7 +1347,7 @@ sinumber|1
 sonumber|1
 yearend|1
 businessnumber|1
-version|1.5.17-dev
+version|1.5.17
 closedto|\N
 revtrans|1
 ponumber|1
