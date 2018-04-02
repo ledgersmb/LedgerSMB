42edbc47d5bc4468c10e282975f58cda2b7363a0
Erik Huelsmann <ehuels@gmail.com>
2018-04-02 18:56:49 +0000

    Update version

--- lib/LedgerSMB.pm
+++ lib/LedgerSMB.pm
@@ -168,7 +168,7 @@ use Carp;
 use DBI;
 
 use base qw(LedgerSMB::Request);
-our $VERSION = '1.5.19-dev';
+our $VERSION = '1.5.19';
 
 my $logger = Log::Log4perl->get_logger('LedgerSMB');
 
--- lib/LedgerSMB/Form.pm
+++ lib/LedgerSMB/Form.pm
@@ -181,8 +181,8 @@ sub new {
     #menubar will be deprecated, replaced with below
     $self->{lynx} = 1 if ( ( defined $self->{path} ) && ( $self->{path} =~ /lynx/i ) );
 
-    $self->{version}   = "1.5.19-dev";
-    $self->{dbversion} = "1.5.19-dev";
+    $self->{version}   = "1.5.19";
+    $self->{dbversion} = "1.5.19";
 
     bless $self, $type;
 
