ledgersmb/LedgerSMB

commit cfefb9c117d9d5cbc2d562df10671b9f94ae63f1

* Hash elements can be array references only (not arrays)

lib/LedgerSMB/Scripts/payment.pm
@@ -1062,8 +1062,8 @@ sub payment2 {
             name  => 'account_class',
             value => $Payment->{account_class}
         },
-        project =>  @project ? @project : '' ,        # WE NEED TO VERIFY THAT THE ARRAY EXISTS, IF IT DOESNT,
-        department => @department ? @department : '', # WE WILL PASS A NULL STRING, THIS FIXES THE ISSUES
+        project =>  @project ? \@project : '' ,        # WE NEED TO VERIFY THAT THE ARRAY EXISTS, IF IT DOESNT,
+        department => @department ? \@department : '', # WE WILL PASS A NULL STRING, THIS FIXES THE ISSUES
         # I WAS HAVING WITH THE NULL ARRAYS, STILL UGLY :P
         account => \@account_options,
         selected_account => $request->{account},
@@ -1111,7 +1111,7 @@ sub payment2 {
            name => 'MEDIA',
            options => \@media_options
         },
-        exrate => @currency_options,
+        exrate => \@currency_options,
         selectedcheckboxes => @selected_checkboxes  ? \@selected_checkboxes : '',
         notes => $request->{notes},
         overpayment         => \@overpayment,

commit b6b94a9e2e05585efcd433d139b9f944093c119d

* Fix exchange rate entry broken by cfefb9c

lib/LedgerSMB/Scripts/payment.pm
@@ -737,7 +737,7 @@ sub payment2 {
     my @project;
     my @selected_checkboxes;
     my @department;
-    my @currency_options;
+    my $currency_options;
     my $exchangerate;
     my $module;
     my $b_units;
@@ -833,7 +833,7 @@ sub payment2 {
         }
 
         if ($exchangerate) {
-            @currency_options = {
+            $currency_options = {
                 name => 'exrate',
                 #THERE IS A STRANGE BEHAVIOUR WITH THIS,
                 #IF I DONT USE THE DOUBLE QUOTES, IT WILL PRINT THE ADDRESS
@@ -842,14 +842,14 @@ sub payment2 {
                 text =>  "$exchangerate"
             };
         } else {
-            @currency_options = { name => 'exrate'};
+            $currency_options = { name => 'exrate'};
         }
 
     } else {
         # WE MUST SET EXCHANGERATE TO 1 FOR THE MATHS SINCE WE
         # ARE USING THE DEFAULT CURRENCY
         $exchangerate = 1;
-        @currency_options = {
+        $currency_options = {
             name => 'exrate',
             value => 1,
             text =>  1
@@ -1122,7 +1122,7 @@ sub payment2 {
            name => 'MEDIA',
            options => \@media_options
         },
-        exrate => \@currency_options,
+        exrate => $currency_options,
         notes => $request->{notes},
         overpayment         => \@overpayment,
         overpayment_account => \@overpayment_account,