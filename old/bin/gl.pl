ledgersmb/LedgerSMB

commit 4d65f09c24cbae8e55c8d7a300da596369b3bbdc

* Don't instantiate LedgerSMB objects in old code
Note that the 'base' argument to the DBObject constructor isn't accessed
as an object at all, let alone that it requires the object to be of
LedgerSMB class.

old/bin/gl.pl
@@ -51,6 +51,8 @@ use LedgerSMB::PE;
 use LedgerSMB::Template;
 use LedgerSMB::Setting::Sequence;
 use LedgerSMB::Company_Config;
+use LedgerSMB::DBObject::Draft;
+use LedgerSMB::DBObject::TransTemplate;
 
 require 'old/bin/bridge.pl'; # needed for voucher dispatches
 require "old/bin/arap.pl";
@@ -90,24 +92,15 @@ $form->{login} = 'test';
 
 
 sub edit_and_save {
-    use LedgerSMB::DBObject::Draft;
-    use LedgerSMB;
     check_balanced($form);
-    my $lsmb = LedgerSMB->new();
-    $lsmb->merge($form);
-    my $draft = LedgerSMB::DBObject::Draft->new({base => $lsmb});
+    my $draft = LedgerSMB::DBObject::Draft->new({base => $form});
     $draft->delete();
     GL->post_transaction( \%myconfig, \%$form, $locale);
     edit();
 }
 
 sub approve {
-    use LedgerSMB::DBObject::Draft;
-    use LedgerSMB;
-    my $lsmb = LedgerSMB->new();
-    $lsmb->merge($form);
-
-    my $draft = LedgerSMB::DBObject::Draft->new({base => $lsmb});
+    my $draft = LedgerSMB::DBObject::Draft->new({base => $form});
     $draft->approve();
     if ($form->{callback}){
         print "Location: $form->{callback}\n";
@@ -351,9 +344,6 @@ sub display_form
 
 
 sub save_temp {
-    use LedgerSMB;
-    use LedgerSMB::DBObject::TransTemplate;
-    my $lsmb = LedgerSMB->new();
     my ($department_name, $department_id) = split/--/, $form->{department};
     $lsmb->{department_id} = $department_id;
     $lsmb->{reference} = $form->{reference};
@@ -375,14 +365,13 @@ sub save_temp {
                   };
         }
     }
-    $template = LedgerSMB::DBObject::TransTemplate->new({base => $lsmb});
+    $template = LedgerSMB::DBObject::TransTemplate->new({base => $form});
     $template->save;
     $form->redirect( $locale->text('Template Saved!') );
 }
 
 
 sub display_row {
-
     my ($init) = @_;
     $form->{totaldebit}  = 0;
     $form->{totalcredit} = 0;
@@ -666,9 +655,7 @@ sub post {
 sub delete {
     $form->error($locale->text('Cannot delete posted transaction'))
        if ($form->{approved});
-    my $lsmb = LedgerSMB->new();
-    $lsmb->merge($form);
-    my $draft = LedgerSMB::DBObject::Draft->new({base => $lsmb});
+    my $draft = LedgerSMB::DBObject::Draft->new({base => $form});
     $draft->delete();
     delete $form->{id};
     delete $form->{reference};

old/bin/aa.pl
@@ -45,9 +45,14 @@
 #======================================================================
 
 package lsmb_legacy;
+use LedgerSMB::Form;
+use LedgerSMB::IR;
+use LedgerSMB::IS;
 use LedgerSMB::Setting;
 use LedgerSMB::Tax;
 use LedgerSMB::Company_Config;
+use LedgerSMB::DBObject::Draft;
+use LedgerSMB::DBObject::TransTemplate;
 
 require 'old/bin/bridge.pl'; # needed for voucher dispatches
 # any custom scripts for this one
@@ -104,11 +109,10 @@ sub copy_to_new{
 }
 
 sub new_screen {
-    use LedgerSMB::Form;
     my @reqprops = qw(ARAP vc dbh stylesheet batch_id script);
     $oldform = $form;
     $form = {};
-    bless $form, Form;
+    bless $form, 'Form';
     for (@reqprops){
         $form->{$_} = $oldform->{$_};
     }
@@ -1090,10 +1094,7 @@ sub on_hold {
 
 
 sub save_temp {
-    use LedgerSMB;
-    use LedgerSMB::DBObject::TransTemplate;
-    my $lsmb = LedgerSMB->new();
-    $lsmb->merge($form);
+    my $lsmb = { %$form };
     $lsmb->{is_invoice} = 1;
     $lsmb->{due} = $form->{invtotal};
     $lsmb->{credit_id} = $form->{customer_id} // $form->{vendor_id};
@@ -1127,11 +1128,7 @@ sub save_temp {
 }
 
 sub edit_and_save {
-    use LedgerSMB::DBObject::Draft;
-    use LedgerSMB;
-    my $lsmb = LedgerSMB->new();
-    $lsmb->merge($form);
-    my $draft = LedgerSMB::DBObject::Draft->new({base => $lsmb});
+    my $draft = LedgerSMB::DBObject::Draft->new({base => $form});
     $draft->delete();
     delete $form->{id};
     AA->post_transaction( \%myconfig, \%$form );
@@ -1139,13 +1136,9 @@ sub edit_and_save {
 }
 
 sub approve {
-    use LedgerSMB::DBObject::Draft;
-    use LedgerSMB;
-    my $lsmb = LedgerSMB->new();
-    $lsmb->merge($form);
     $form->update_invnumber;
 
-    my $draft = LedgerSMB::DBObject::Draft->new({base => $lsmb});
+    my $draft = LedgerSMB::DBObject::Draft->new({base => $form});
 
     $draft->approve();
 

old/bin/io.pl
@@ -45,6 +45,7 @@ use LedgerSMB::Template;
 use LedgerSMB::Sysconfig;
 use LedgerSMB::Setting;
 use LedgerSMB::Company_Config;
+use LedgerSMB::DBObject::Draft;
 use LedgerSMB::File;
 use List::Util qw(max reduce);
 
@@ -135,13 +136,8 @@ sub _calc_taxes {
 }
 
 sub approve {
-    use LedgerSMB::DBObject::Draft;
-    use LedgerSMB;
     $form->update_invnumber;
-    my $lsmb = LedgerSMB->new();
-    $lsmb->merge($form);
-
-    my $draft = LedgerSMB::DBObject::Draft->new({base => $lsmb});
+    my $draft = LedgerSMB::DBObject::Draft->new({base => $form});
 
     $draft->approve();
     edit();

old/bin/ir.pl
@@ -40,11 +40,13 @@
 
 package lsmb_legacy;
 use List::Util qw(min max);
+use LedgerSMB::Form;
 use LedgerSMB::IR;
 use LedgerSMB::IS;
 use LedgerSMB::PE;
 use LedgerSMB::Tax;
 use LedgerSMB::Setting;
+use LedgerSMB::DBObject::Draft;
 
 require 'old/bin/bridge.pl'; # needed for voucher dispatches
 require "old/bin/io.pl";
@@ -64,11 +66,7 @@ sub copy_to_new{
 }
 
 sub edit_and_save {
-    use LedgerSMB::DBObject::Draft;
-    use LedgerSMB;
-    my $lsmb = LedgerSMB->new();
-    $lsmb->merge($form);
-    my $draft = LedgerSMB::DBObject::Draft->new({base => $lsmb});
+    my $draft = LedgerSMB::DBObject::Draft->new({base => $form});
     $draft->delete();
     delete $form->{id};
     IR->post_invoice( \%myconfig, \%$form );
@@ -76,12 +74,7 @@ sub edit_and_save {
 }
 
 sub approve {
-    use LedgerSMB::DBObject::Draft;
-    use LedgerSMB;
-    my $lsmb = LedgerSMB->new();
-    $lsmb->merge($form);
-
-    my $draft = LedgerSMB::DBObject::Draft->new({base => $lsmb});
+    my $draft = LedgerSMB::DBObject::Draft->new({base => $form});
 
     $draft->approve();
 
@@ -99,11 +92,10 @@ sub approve {
 }
 
 sub new_screen {
-    use LedgerSMB::Form;
     my @reqprops = qw(ARAP vc dbh stylesheet type);
     $oldform = $form;
     $form = {};
-    bless $form, Form;
+    bless $form, 'Form';
     for (@reqprops){
         $form->{$_} = $oldform->{$_};
     }

old/bin/is.pl
@@ -46,11 +46,12 @@
 package lsmb_legacy;
 
 use List::Util qw(max min);
-
+use LedgerSMB::Form;
 use LedgerSMB::IS;
 use LedgerSMB::PE;
 use LedgerSMB::Tax;
 use LedgerSMB::Setting;
+use LedgerSMB::DBObject::Draft;
 
 require 'old/bin/bridge.pl'; # needed for voucher dispatches
 require "old/bin/arap.pl";
@@ -73,11 +74,7 @@ sub copy_to_new{
 }
 
 sub edit_and_save {
-    use LedgerSMB::DBObject::Draft;
-    use LedgerSMB;
-    my $lsmb = LedgerSMB->new();
-    $lsmb->merge($form);
-    my $draft = LedgerSMB::DBObject::Draft->new({base => $lsmb});
+    my $draft = LedgerSMB::DBObject::Draft->new({base => $form});
     $draft->delete();
     delete $form->{id};
     IS->post_invoice( \%myconfig, \%$form );
@@ -85,11 +82,10 @@ sub edit_and_save {
 }
 
 sub new_screen {
-    use LedgerSMB::Form;
     my @reqprops = qw(ARAP vc dbh stylesheet type);
     $oldform = $form;
     $form = {};
-    bless $form, Form;
+    bless $form, 'Form';
     for (@reqprops){
         $form->{$_} = $oldform->{$_};
     }

commit 6627abddae3f80d657aa963bcf2eb0d9770fad38

Fix #3754 GL Save Template gives error
Fixes bug introduced by commit 4d65f09c24cbae8e55c8d7a300da596369b3bbdc

Save Template from GL transaction gives an error `null value in
column "post_date" violates not-null constraint`.

TransTemplate was being created with form data, rather than the
prepared transaction data.

Code refactored to make it clearer that transaction data is being
prepared as a simple hash. In 1.5 and earlier, a full-fat LedgerSMB
object was used.

old/bin/gl.pl
@@ -323,27 +323,32 @@ sub display_form
 
 sub save_temp {
     my ($department_name, $department_id) = split/--/, $form->{department};
-    $lsmb->{department_id} = $department_id;
-    $lsmb->{reference} = $form->{reference};
-    $lsmb->{description} = $form->{description};
-    $lsmb->{department_id} = $department_id;
-    $lsmb->{post_date} = $form->{transdate};
-    $lsmb->{type} = 'gl';
-    $lsmb->{journal_lines} = [];
+
+    my $data = { 
+        department_id => $department_id,
+        reference => $form->{reference},
+        description => $form->{description},
+        department_id => $department_id,
+        post_date => $form->{transdate},
+        type => 'gl',
+        journal_lines => [],
+    };
+
     for my $iter (0 .. $form->{rowcount}){
         if ($form->{"accno_$iter"} and
                   (($form->{"credit_$iter"} != 0) or ($form->{"debit_$iter"} != 0))){
              my ($acc_id, $acc_name) = split /--/, $form->{"accno_$iter"};
              my $amount = $form->{"credit_$iter"} || ( $form->{"debit_$iter"}
                                                      * -1 );
-             push @{$lsmb->{journal_lines}},
+             push @{$data->{journal_lines}},
                   {accno => $acc_id,
                    amount => $amount,
                    cleared => false,
                   };
         }
     }
-    $template = LedgerSMB::DBObject::TransTemplate->new({base => $form});
+
+    $template = LedgerSMB::DBObject::TransTemplate->new({base => $data});
     $template->save;
     $form->redirect( $locale->text('Template Saved!') );
 }