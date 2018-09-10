1ab61f3b2810f6d54dbe8207aa818c60c5b499b4
Nick Prater <nick@npbroadcast.com>
2018-09-10 13:30:28 +0200

    Add module PageObject::App::Cash::Vouchers::Payment

---
+++ xt/lib/PageObject/App/Cash/Vouchers/Payment.pm
@@ -0,0 +1,29 @@
+package PageObject::App::Cash::Vouchers::Payment;
+
+use strict;
+use warnings;
+
+use Carp;
+use PageObject;
+
+use Moose;
+use namespace::autoclean;
+extends 'PageObject';
+
+__PACKAGE__->self_register(
+              'cash-vouchers-payment',
+              './/div[@id="create-new-batch"]',
+              tag_name => 'div',
+              attributes => {
+                  id => 'create-new-batch',
+              });
+
+sub _verify {
+    my ($self) = @_;
+
+    return $self;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
