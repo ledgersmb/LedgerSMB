b525e33de0cde64ac971bde7225e32e52deb32c9
Erik Huelsmann <ehuels@gmail.com>
2018-09-15 09:24:08 +0200

    * Implement budget and journal migration

--- sql/changes/mc/migrate-new-constraints.sql
+++ sql/changes/mc/migrate-new-constraints.sql
@@ -1,4 +1,9 @@
 
+-- In the pricematrix code, NULL currency values get defaulted
+-- to the company default currency. Since NULL is no longer
+-- allowed as a currency value, set the default that was used
+-- anyway.
+
 UPDATE partsvendor
    SET curr = (select value from defaults where setting_key = 'curr')
  WHERE curr IS NULL;
