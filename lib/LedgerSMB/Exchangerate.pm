2e769c0bd170ac592d5c2699945a67c9e1fe08fe
Erik Huelsmann <ehuels@gmail.com>
2018-09-19 10:22:33 +0200

    * Adjust LINCENSE AND COPYRIGHT section to pass tests

--- lib/LedgerSMB/Exchangerate.pm
+++ lib/LedgerSMB/Exchangerate.pm
@@ -159,8 +159,11 @@ sub delete {
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (C) 2012 The LedgerSMB Core Team.  This module may be used under the
-GNU GPL in accordance with the LICENSE file listed.
+Copyright (C) 2015 The LedgerSMB Core Team
+
+This file is licensed under the GNU General Public License version 2, or at your
+option any later version.  A copy of the license should have been included with
+your software.
 
 =cut
 
