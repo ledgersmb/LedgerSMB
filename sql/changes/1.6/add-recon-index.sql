db99a5620097201522af9cb111f8c8285ca04273
Erik Huelsmann <ehuels@gmail.com>
2018-12-13 00:04:36 +0100

    Stop caching the last-inspected entry_id
    Instead, consult a partial index to find the actual value instead
    of the cached proxy.
    
    Fix #3904 and fix ##3909.

---
+++ sql/changes/1.6/add-recon-index.sql
@@ -0,0 +1,8 @@
+
+
+CREATE INDEX acc_trans_recon_idx ON acc_trans (chart_id, entry_id)
+    WHERE not cleared;
+
+COMMENT ON INDEX acc_trans_recon_id IS
+$$This index serves to optimize finding the minimum entry_id
+of uncleared journal lines for a given account.$$;
