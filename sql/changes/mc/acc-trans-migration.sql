9c7f3a2e54ccc2e8333282a6fd4727921dfdd605
Yves Lavoie <ylavoie@yveslavoie.com>
2019-05-03 18:39:14 -0400

    Sum transactions and FX in reconciliation reports simplified

--- sql/changes/mc/acc-trans-migration.sql
+++ sql/changes/mc/acc-trans-migration.sql
@@ -134,13 +134,13 @@ BEGIN
                           where ltd.id = crl.ledger_id);
   UPDATE cr_report_line crl
      SET our_balance = our_balance
-                     + (select our_balance from cr_report_line cl
+                     + (select sum(our_balance) from cr_report_line cl
                            join lines_to_delete ltd on cl.ledger_id = ltd.id
-                          where ltd.id = crl.ledger_id or ltd.assoc = crl.ledger_id),
+                          where ltd.assoc = crl.ledger_id),
          their_balance = their_balance
-                       + (select their_balance from cr_report_line cl
+                       + (select sum(their_balance) from cr_report_line cl
                            join lines_to_delete ltd on cl.ledger_id = ltd.id
-                          where ltd.id = crl.ledger_id or ltd.assoc = crl.ledger_id)
+                          where ltd.assoc = crl.ledger_id)
    WHERE EXISTS (select 1 from lines_to_delete ltd where ltd.assoc = crl.ledger_id);
   DELETE FROM cr_report_line crl
    WHERE EXISTS (select 1 from lines_to_delete ltd
