b6ce9e6f1e337811993a2f469738edff3e8bfab6
Erik Huelsmann <ehuels@gmail.com>
2019-08-18 20:23:38 +0000

    Update version
    [skip ci]

--- dists/source/build.sh
+++ dists/source/build.sh
@@ -2,7 +2,7 @@
 
 # Simple script to prepare for release
 
-version="1.5.25";
+version="1.5.26-dev";
 build_d="../release";
 
 hg archive -t tgz $build_d/ledgersmb-${version}.tar.gz
--- lib/LedgerSMB/Form.pm
+++ lib/LedgerSMB/Form.pm
@@ -181,8 +181,8 @@ sub new {
     #menubar will be deprecated, replaced with below
     $self->{lynx} = 1 if ( ( defined $self->{path} ) && ( $self->{path} =~ /lynx/i ) );
 
-    $self->{version}   = "1.5.25";
-    $self->{dbversion} = "1.5.25";
+    $self->{version}   = "1.5.26-dev";
+    $self->{dbversion} = "1.5.26-dev";
 
     bless $self, $type;
 
