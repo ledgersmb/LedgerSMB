1dce48100db3af6f97ff81eae4c619f317cc1af7
Yves Lavoie <ylavoie@yveslavoie.com>
2019-08-26 13:32:58 -0400

    Freshen up circleci custom images if the PR altered them

--- .circleci/images/perl/build.sh
+++ .circleci/images/perl/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 
-for v in 5.28 5.26 5.24 5.22 5.20 5.18 ; do
-  docker build -t ylavoie/ledgersmb_circleci-perl:$v --build-arg perl=$v.0 .
+for v in 5.30 5.28 5.26 5.24 5.22 ; do
+  docker build -t ylavoie/ledgersmb_circleci-perl:$v \
+    --build-arg perl=$v.0 \
+    --build-arg SHA=$(curl -s 'https://api.github.com/repos/ledgersmb/LedgerSMB/branches/master' | jq -r '.commit.sha') .
   docker push ylavoie/ledgersmb_circleci-perl:$v
 done
