cfdd69249231a36f0ff4510cc383d0ba8e3c596c
Erik Huelsmann <ehuels@gmail.com>
2019-11-02 15:00:31 +0000

    Update version
    [skip ci]

--- lib/LedgerSMB/Form.pm
+++ lib/LedgerSMB/Form.pm
@@ -181,8 +181,8 @@ sub new {
     #menubar will be deprecated, replaced with below
     $self->{lynx} = 1 if ( ( defined $self->{path} ) && ( $self->{path} =~ /lynx/i ) );
 
-    $self->{version}   = "1.5.28";
-    $self->{dbversion} = "1.5.28";
+    $self->{version}   = "1.5.29-dev";
+    $self->{dbversion} = "1.5.29-dev";
 
     bless $self, $type;
 
--- sql/Pg-database.sql
+++ sql/Pg-database.sql
@@ -1347,7 +1347,7 @@ sinumber|1
 sonumber|1
 yearend|1
 businessnumber|1
-version|1.5.28
+version|1.5.29-dev
 closedto|\N
 revtrans|1
 ponumber|1
