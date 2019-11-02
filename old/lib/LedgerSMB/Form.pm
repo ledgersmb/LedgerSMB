bb720b368ab548a9385a822ed36c2cb2259ca40e
Erik Huelsmann <ehuels@gmail.com>
2019-11-02 16:00:13 +0000

    Update version

--- old/lib/LedgerSMB/Form.pm
+++ old/lib/LedgerSMB/Form.pm
@@ -180,8 +180,8 @@ sub new {
             split(/:/, $self->{cookie});
     }
 
-    $self->{version}   = "1.7.2-dev";
-    $self->{dbversion} = "1.7.2-dev";
+    $self->{version}   = "1.7.2";
+    $self->{dbversion} = "1.7.2";
 
     bless $self, $type;
 
