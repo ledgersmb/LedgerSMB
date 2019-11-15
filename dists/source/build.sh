ce0a42d3ecf084f725fce57a639652af5759b468
Erik Huelsmann <ehuels@gmail.com>
2019-11-15 16:22:59 +0000

    Update version

--- dists/source/build.sh
+++ dists/source/build.sh
@@ -2,7 +2,7 @@
 
 # Simple script to prepare for release
 
-version="1.5.29-dev";
+version="1.5.29";
 build_d="../release";
 
 hg archive -t tgz $build_d/ledgersmb-${version}.tar.gz
--- sql/Pg-database.sql
+++ sql/Pg-database.sql
@@ -1347,7 +1347,7 @@ sinumber|1
 sonumber|1
 yearend|1
 businessnumber|1
-version|1.5.29-dev
+version|1.5.29
 closedto|\N
 revtrans|1
 ponumber|1
