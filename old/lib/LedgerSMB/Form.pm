ec18f40cd48258c12c46396b70b3eec98aad20d2
Erik Huelsmann <ehuels@gmail.com>
2019-12-26 17:32:18 +0000

    Update version

--- old/lib/LedgerSMB/Form.pm
+++ old/lib/LedgerSMB/Form.pm
@@ -180,8 +180,8 @@ sub new {
             split(/:/, $self->{cookie});
     }
 
-    $self->{version}   = "1.6.17-dev";
-    $self->{dbversion} = "1.6.17-dev";
+    $self->{version}   = "1.6.17";
+    $self->{dbversion} = "1.6.17";
 
     bless $self, $type;
 
--- sql/Pg-database.sql
+++ sql/Pg-database.sql
@@ -1361,7 +1361,7 @@ sinumber|1
 sonumber|1
 yearend|1
 businessnumber|1
-version|1.6.17-dev
+version|1.6.17
 closedto|\N
 revtrans|1
 ponumber|1
