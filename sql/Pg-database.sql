e9a8fba8f8b279c540ea2b1ef2cc36cabaa9a970
Erik Huelsmann <ehuels@gmail.com>
2019-12-27 21:02:47 +0100

    Remove 'chart' view from Pg-database.sql to end confusion
    Also, for newly created databases, it's not very relevant to create a
    view which is going to be dropped seconds later.

--- sql/Pg-database.sql
+++ sql/Pg-database.sql
@@ -4829,39 +4829,6 @@ CREATE TYPE trial_balance__entry AS (
 ALTER TABLE cr_report_line ADD FOREIGN KEY(ledger_id) REFERENCES acc_trans(entry_id);
 
 
-CREATE VIEW chart AS
-SELECT ah.id, ah.accno, coalesce(ht.description, ah.description) as description,
-       'H' as charttype, NULL as category, NULL as link,
-       ah.parent_id as account_heading,
-       null as gifi_accno, false as contra,
-       false as tax
-  from account_heading ah
-  LEFT JOIN (SELECT ht.trans_id, ht.description FROM account_heading_translation ht
-                                    INNER JOIN user_preference up ON
-                                          up.language = ht.language_code
-                                    INNER JOIN users ON up.id = users.id
-                                    WHERE users.username = SESSION_USER) ht
-         ON ah.id = ht.trans_id
-UNION
-select c.id, c.accno, coalesce(at.description, c.description),
-       'A' as charttype, c.category, concat_colon(l.description) as link,
-       heading, gifi_accno, contra,
-       tax
-  from account c
-  left join account_link l
-    ON (c.id = l.account_id)
-  LEFT JOIN (SELECT at.trans_id, at.description FROM account_translation at
-                                    INNER JOIN user_preference up ON
-                                          up.language = at.language_code
-                                    INNER JOIN users ON up.id = users.id
-                                    WHERE users.username = SESSION_USER) at
-         ON c.id = at.trans_id
-group by c.id, c.accno, coalesce(at.description, c.description), c.category,
-         c.heading, c.gifi_accno, c.contra, c.tax;
-
-COMMENT ON VIEW chart IS $$Compatibility chart for 1.2 and earlier.$$;
-
-
 CREATE VIEW tx_report AS
 SELECT id, reference, null::int as entity_credit_account, 'gl' as table,
        approved
