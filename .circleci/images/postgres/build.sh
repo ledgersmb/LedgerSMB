f80cace4eed00fd3491d3bc4749d751dfde56883
Erik Huelsmann <ehuels@gmail.com>
2020-02-27 08:23:19 +0100

    Move CircleCI images to their own repo with builds on Docker Hub
    This way, all devs with write access to the LedgerSMB project on GitHub
    can cooperate on building the images and update them as required.

--- .circleci/images/postgres/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for v in 9.5 9.6 10 11 ; do
-  docker build -t ylavoie/ledgersmb_circleci-postgres:$v --build-arg version=$v .
-  docker push ylavoie/ledgersmb_circleci-postgres:$v
-done
