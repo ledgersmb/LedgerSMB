6f1b5be3c0894c6839ea7fe95c04640f70403a94
Yves Lavoie <ylavoie@yveslavoie.com>
2020-04-03 23:55:15 -0400

    Linted lsmb/InvoiceLine

--- UI/js-src/lsmb/InvoiceLine.js
+++ UI/js-src/lsmb/InvoiceLine.js
@@ -1,12 +1,13 @@
-define(["dojo/_base/declare",
-         "dijit/_WidgetBase",
-         "dijit/_TemplatedMixin",
-         "dijit/_WidgetsInTemplateMixin",
-         "dijit/_Container"
-        ],
-        function (declare, _WidgetBase, _TemplatedMixin,
-                  _WidgetsInTemplateMixin, _Container) {
-            return declare("lsmb/InvoiceLine",
-                           [_WidgetBase, _Container], {
-                            });
-        });
+/** @format */
+
+define([
+   "dojo/_base/declare",
+   "dijit/_WidgetBase",
+   "dijit/_Container",
+], function (
+   declare,
+   _WidgetBase,
+   _Container
+) {
+   return declare("lsmb/InvoiceLine", [_WidgetBase, _Container], {});
+});
