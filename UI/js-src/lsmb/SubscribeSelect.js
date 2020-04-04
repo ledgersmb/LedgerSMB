ccaab334b3b53dfcbaca56c0981ac9e0884f1cd2
Yves Lavoie <ylavoie@yveslavoie.com>
2020-04-04 02:12:44 -0400

    Linted lsmb/SubscribeSelect

--- UI/js-src/lsmb/SubscribeSelect.js
+++ UI/js-src/lsmb/SubscribeSelect.js
@@ -1,26 +1,28 @@
-define(["dojo/_base/declare",
-        "dojo/on",
-        "dojo/topic",
-        "dijit/form/Select"],
-       function(declare, on, topic, Select) {
-           return declare("lsmb/SubscribeSelect", [Select], {
-               topic: "",
-               topicMap: {},
-               update: function(targetValue) {
-                   var newValue = this.topicMap[targetValue];
-                   if (newValue) {
-                       this.set("value", newValue);
-                   }
-               },
-               postCreate: function() {
-                   var self = this;
-                   this.inherited(arguments);
+/** @format */
 
-                   this.own(
-                       topic.subscribe(self.topic,function(targetValue) {
-                           self.update(targetValue);
-                       })
-                   );
-               }
-           });
-       });
+define(["dojo/_base/declare", "dojo/topic", "dijit/form/Select"], function (
+   declare,
+   topic,
+   select
+) {
+   return declare("lsmb/SubscribeSelect", [select], {
+      topic: "",
+      topicMap: {},
+      update: function (targetValue) {
+         var newValue = this.topicMap[targetValue];
+         if (newValue) {
+            this.set("value", newValue);
+         }
+      },
+      postCreate: function () {
+         var self = this;
+         this.inherited(arguments);
+
+         this.own(
+            topic.subscribe(self.topic, function (targetValue) {
+               self.update(targetValue);
+            })
+         );
+      }
+   });
+});
