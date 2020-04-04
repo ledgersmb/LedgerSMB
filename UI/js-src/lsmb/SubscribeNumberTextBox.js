e56eefa8c0d75cbed1ce945d46d6dbf985ac091d
Yves Lavoie <ylavoie@yveslavoie.com>
2020-04-03 21:00:52 -0400

    Linted lsmb/SubscribeNumberTextBox

--- UI/js-src/lsmb/SubscribeNumberTextBox.js
+++ UI/js-src/lsmb/SubscribeNumberTextBox.js
@@ -1,22 +1,24 @@
-define(["dojo/_base/declare",
-        "dojo/on",
-        "dojo/topic",
-        "dijit/form/NumberTextBox"],
-       function(declare, on, topic, NumberTextBox) {
-           return declare("lsmb/SubscribeNumberTextBox", NumberTextBox, {
-               topic: "",
-               update: function(targetValue) {
-                   this.set("value",targetValue);
-               },
-               postCreate: function() {
-                   var self = this;
-                   this.inherited(arguments);
+/** @format */
 
-                   this.own(
-                       topic.subscribe(self.topic,function(targetValue) {
-                           self.update(targetValue);
-                       })
-                   );
-               }
-           });
-       });
+define([
+   "dojo/_base/declare",
+   "dojo/topic",
+   "dijit/form/NumberTextBox",
+], function (declare, topic, NumberTextBox) {
+   return declare("lsmb/SubscribeNumberTextBox", NumberTextBox, {
+      topic: "",
+      update: function (targetValue) {
+         this.set("value", targetValue);
+      },
+      postCreate: function () {
+         var self = this;
+         this.inherited(arguments);
+
+         this.own(
+            topic.subscribe(self.topic, function (targetValue) {
+               self.update(targetValue);
+            })
+         );
+      },
+   });
+});
