03a6a788000626fb88876b09362a8e421b96898d
Yves Lavoie <ylavoie@yveslavoie.com>
2020-04-04 01:45:57 -0400

    Linted lsmb/parts/PartRestStore

--- UI/js-src/lsmb/parts/PartRestStore.js
+++ UI/js-src/lsmb/parts/PartRestStore.js
@@ -1,57 +1,52 @@
+/** @format */
+/*
+TODO: remaining issues
+  37:13  error  Assignment to function parameter 'query'                     no-param-reassign
+  40:13  error  Assignment to function parameter 'query'                     no-param-reassign
+  46:11  error  A constructor name should not start with a lowercase letter  new-cap
+*/
+
 define([
-    "dojo/store/JsonRest",
-    "dojo/store/Observable",
-    "dojo/request",
-    "dojo/_base/array",
-    "dojo/_base/declare",
-    "dojo/Evented",
-    "dojo/request",
-    "dojo/io-query"
-    ], function(
-        JsonRest,
-        Observable,
-        request,
-        array,
-        declare,
-        Evented,
-        xhr,
-        io
-    ){
-        console.log("creating store");
-        var partsRest =
-            declare("lsmb/parts/PartRestStore",[JsonRest],
-                    {
-                        get: function(id) {
-                            var self = this;
-                            var r = request.get(this.target, {
-                                handleAs: "json",
-                                headers: this.headers
-                            });
-                            var rv = r.then(function(data){
-                                var theOne;
-                                array.forEach(data, function(item) {
-                                    if (id == item[self.idProperty]) {
-                                        theOne = item;
-                                    }
-                                });
-                                return theOne;
-                            });
-                            return rv;
-                        },
-                        query: function(query, options) {
-                            if (query && typeof query == "object") {
-                                query = "?" + io.objectToQuery(query);
-                            }
-                            if (options && options.type) {
-                                query = "?type=" + options.type + "&" + query
-                            }
-                            return this.inherited(arguments, [query, options]);
-                        }
-                    });
-        var store = new Observable(new partsRest({
-            idProperty: "partnumber",
-            target: "erp/api/v0/goods/"
-        }));
-        console.log("done creating store");
-        return store;
-    });
+   "dojo/store/JsonRest",
+   "dojo/store/Observable",
+   "dojo/request",
+   "dojo/_base/array",
+   "dojo/_base/declare",
+   "dojo/io-query",
+], function (JsonRest, Observable, request, array, declare, ioQuery) {
+   var partsRest = declare("lsmb/parts/PartRestStore", [JsonRest], {
+      get: function (id) {
+         var self = this;
+         var r = request.get(this.target, {
+            handleAs: "json",
+            headers: this.headers,
+         });
+         var rv = r.then(function (data) {
+            var theOne;
+            array.forEach(data, function (item) {
+               if (id === item[self.idProperty]) {
+                  theOne = item;
+               }
+            });
+            return theOne;
+         });
+         return rv;
+      },
+      query: function (query, options) {
+         if (query && typeof query === "object") {
+            query = "?" + ioQuery.objectToQuery(query);
+         }
+         if (options && options.type) {
+            query = "?type=" + options.type + "&" + query;
+         }
+         return this.inherited(arguments, [query, options]);
+      },
+   });
+   var store = new Observable(
+      new partsRest({
+         idProperty: "partnumber",
+         target: "erp/api/v0/goods/",
+      })
+   );
+   return store;
+});
