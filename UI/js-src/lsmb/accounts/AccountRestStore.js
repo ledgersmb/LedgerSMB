c6ffaeefbf4b62128561c2991e70dfabaee56b59
Yves Lavoie <ylavoie@yveslavoie.com>
2020-04-04 02:20:50 -0400

    Linted lsmb/accounts/AccountRestStore

--- UI/js-src/lsmb/accounts/AccountRestStore.js
+++ UI/js-src/lsmb/accounts/AccountRestStore.js
@@ -1,48 +1,38 @@
+/** @format */
+
 define([
-    "dojo/store/JsonRest",
-    "dojo/store/Observable",
-    "dojo/store/Memory",
-    "dojo/store/Cache",
-    "dojo/request",
-    "dojo/_base/array",
-    "dojo/_base/declare",
-    "dojo/Evented",
-    "dojo/request"
-    ], function(
-      JsonRest,
-        Observable,
-        Memory,
-        Cache,
-      request,
-      array,
-      declare,
-      Evented,
-      xhr
-    ){
-        var accountsRest =
-            declare("lsmb/accounts/AccountRestStore",[JsonRest],
-                    {
-                        get: function(id) {
-                            var self = this;
-                            var r = request.get(this.target, {
-                                handleAs: "json",
-                                headers: this.headers
-                            });
-                            var rv = r.then(function(data){
-                                var theOne;
-                                array.forEach(data, function(item) {
-                                    if (id == item[self.idProperty]) {
-                                        theOne = item;
-                                    }
-                                });
-                                return theOne;
-                            });
-                            return rv;
-                        }
-                    });
-        var store = new Cache(new accountsRest({
-            idProperty: "accno",
-            target: "/erp/api/v0/accounts/"
-        }), new Memory());
-        return store;
-    });
+   "dojo/store/JsonRest",
+   "dojo/store/Memory",
+   "dojo/store/Cache",
+   "dojo/request",
+   "dojo/_base/array",
+   "dojo/_base/declare",
+], function (jsonRest, memory, cache, request, array, declare) {
+   var accountsRest = declare("lsmb/accounts/AccountRestStore", [jsonRest], {
+      get: function (id) {
+         var self = this;
+         var r = request.get(this.target, {
+            handleAs: "json",
+            headers: this.headers,
+         });
+         var rv = r.then(function (data) {
+            var theOne;
+            array.forEach(data, function (item) {
+               if (id === item[self.idProperty]) {
+                  theOne = item;
+               }
+            });
+            return theOne;
+         });
+         return rv;
+      },
+   });
+   var store = new cache(
+      new accountsRest({
+         idProperty: "accno",
+         target: "/erp/api/v0/accounts/",
+      }),
+      new memory()
+   );
+   return store;
+});
