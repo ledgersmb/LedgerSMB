8ad95967059a20c66e3082032aa9815ade1d2f78
Erik Huelsmann <ehuels@gmail.com>
2020-05-23 22:54:09 +0200

    Fix #4645: manual taxes double-counted on invoice (but not on-screen)
    Note that this issue seems to be caused by double-calling of
    IS/IR->retrieve_invoice(), which next to in the current sub is also called
    in &invoice_links().

--- old/bin/io.pl
+++ old/bin/io.pl
@@ -1119,13 +1119,6 @@ sub print {
     if ($form->{type} eq 'invoice') {
         &invoice_links;
         &prepare_invoice;
-
-        if ($form->{vc} eq 'vendor') {
-            IR->retrieve_invoice(\%myconfig, $form);
-        }
-        else {
-            IS->retrieve_invoice(\%myconfig, $form);
-        }
     }
     else {
         &order_links;
