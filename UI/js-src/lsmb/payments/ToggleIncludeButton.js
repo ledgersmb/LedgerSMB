314a3912cfda29d4558d546c5b4fb402736ed450
Erik Huelsmann <ehuels@gmail.com>
2020-06-07 12:22:54 +0200

    Fix the search scope of the toggle button on the payment screen

--- UI/js-src/lsmb/payments/ToggleIncludeButton.js
+++ UI/js-src/lsmb/payments/ToggleIncludeButton.js
@@ -5,7 +5,7 @@ define(["dojo/_base/declare", "dijit/form/Button"], function (declare, button) {
    return declare("lsmb/payments/ToggleIncludeButton", [button], {
       onClick: function () {
          dojo
-            .query('input[name^="checkbox_"]', this.domNode)
+            .query('input[name^="checkbox_"]', this.valueNode.form)
             .forEach(function (node) {
                var n = dijit.getEnclosingWidget(node);
                n.set("checked", !n.get("checked"));
