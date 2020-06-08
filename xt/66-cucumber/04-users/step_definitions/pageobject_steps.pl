5ecc3d74b335589e72ab5b0c2ba502ddea37e868
Erik Huelsmann <ehuels@gmail.com>
2020-06-08 23:04:03 +0200

    Improve page-waiting reliability

--- xt/66-cucumber/04-users/step_definitions/pageobject_steps.pl
+++ xt/66-cucumber/04-users/step_definitions/pageobject_steps.pl
@@ -7,6 +7,7 @@ use warnings;
 use Test::More;
 use Test::BDD::Cucumber::StepFile;
 
+use PageObject::App::Contacts::EditContact;
 
 When qr/^I click Control Code "(.*)"$/, sub {
     my $page = S->{ext_wsl}->page->body->maindiv->content;
@@ -16,7 +17,9 @@ When qr/^I click Control Code "(.*)"$/, sub {
         qq{.//td[contains(\@class,"entity_control_code")]/a[.="$link_text"]}
     ) or die "failed to find link for Control Code $link_text";
 
-    ok($link->click, "clicked link for Control Code $link_text");
+    $link->click;
+    ok(1, "clicked link for Control Code $link_text");
+    S->{ext_wsl}->page->body->maindiv->wait_for_content(replaces => $link);
 };
 
 1;
