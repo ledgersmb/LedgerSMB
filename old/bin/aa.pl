7ad7dd2b3a452adc10963f839bbee8e82e7a7121
Erik Huelsmann <ehuels@gmail.com>
2020-09-06 19:29:24 +0200

    Follow-up to 75c6e07e6a8f, restore loading account from db
    Without this commit, transactions loaded from the database don't show
    the account as it was saved, but the default account.

--- old/bin/aa.pl
+++ old/bin/aa.pl
@@ -147,6 +147,7 @@ sub add {
 sub edit {
 
     &create_links;
+    $form->{$form->{ARAP}} = $form->{"$form->{ARAP}_1"};
     $form->{title} = $locale->text("Edit");
     if ($form->{reverse}){
         if ($form->{ARAP} eq 'AR'){
