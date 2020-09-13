eb4f7883b81524b7895373374f9215c22b167dc8
Erik Huelsmann <ehuels@gmail.com>
2020-09-13 19:07:49 +0000

    Update version
    [skip ci]

--- dists/source/build.sh
+++ dists/source/build.sh
@@ -2,7 +2,7 @@
 
 # Simple script to prepare for release
 
-version="1.6.25";
+version="1.6.26-dev";
 build_d="../release";
 
 hg archive -t tgz $build_d/ledgersmb-${version}.tar.gz
--- old/lib/LedgerSMB/Form.pm
+++ old/lib/LedgerSMB/Form.pm
@@ -181,8 +181,8 @@ sub new {
             split(/:/, $self->{cookie});
     }
 
-    $self->{version}   = "1.6.25";
-    $self->{dbversion} = "1.6.25";
+    $self->{version}   = "1.6.26-dev";
+    $self->{dbversion} = "1.6.26-dev";
 
     bless $self, $type;
 
