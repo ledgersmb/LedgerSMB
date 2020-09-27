59e53d3169fc09ff43b0951f4c5413c78a0a94df
Erik Huelsmann <ehuels@gmail.com>
2020-09-27 19:14:26 +0000

    Update version
    [skip ci]

--- lib/LedgerSMB.pm
+++ lib/LedgerSMB.pm
@@ -206,7 +206,7 @@ use LedgerSMB::Setting;
 use LedgerSMB::Template;
 use LedgerSMB::Template::UI;
 
-our $VERSION = '1.8.4';
+our $VERSION = '1.8.5-dev';
 
 my $logger = Log::Log4perl->get_logger('LedgerSMB');
 
--- old/lib/LedgerSMB/Form.pm
+++ old/lib/LedgerSMB/Form.pm
@@ -118,8 +118,8 @@ sub new {
         $self->{nextsub} //= '';
         $self->{action} //= $self->{nextsub};
     }
-    $self->{version}   = '1.8.4';
-    $self->{dbversion} = '1.8.4';
+    $self->{version}   = '1.8.5-dev';
+    $self->{dbversion} = '1.8.5-dev';
 
 
     $self;
