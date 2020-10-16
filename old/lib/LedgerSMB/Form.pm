d408bec12c66aa40b41416bc373a7409fac74aee
Erik Huelsmann <ehuels@gmail.com>
2020-10-16 17:05:33 +0000

    Update version
    [skip ci]

--- old/lib/LedgerSMB/Form.pm
+++ old/lib/LedgerSMB/Form.pm
@@ -118,8 +118,8 @@ sub new {
         $self->{nextsub} //= '';
         $self->{action} //= $self->{nextsub};
     }
-    $self->{version}   = '1.8.6';
-    $self->{dbversion} = '1.8.6';
+    $self->{version}   = '1.8.7-dev';
+    $self->{dbversion} = '1.8.7-dev';
 
 
     $self;
