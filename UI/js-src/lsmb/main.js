ef969fff59129de0eb2f15aeb508bd15f5d7abca
Erik Huelsmann <ehuels@gmail.com>
2020-12-06 19:36:15 +0100

    Add tracking of historic state linked to the BACK button
    Closes #1582.

--- UI/js-src/lsmb/main.js
+++ UI/js-src/lsmb/main.js
@@ -24,16 +24,36 @@ define([
     domStyle
 ) {
     return declare("lsmb/main", [_WidgetBase, _Container], {
+        history: {},
+        addHistory: function (fn) {
+            var h = "__";
+            var chars =
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            for (var i = 0; i < 25; i++) {
+                h += chars.charAt(Math.floor(Math.random() * chars.length));
+            }
+            this.history[h] = fn;
+
+            return h;
+        },
         startup: function () {
+            var self = this;
             this.inherited(arguments);
 
             var mainDiv = registry.byId("maindiv");
             if (mainDiv != null) {
                 if (window.location.hash) {
-                    mainDiv.load_link(hash());
+                    var h = hash();
+                    if (!h.startsWith("__")) {
+                        mainDiv.load_link(hash());
+                    }
                 }
                 topic.subscribe("/dojo/hashchange", function (_hash) {
-                    mainDiv.load_link(_hash);
+                    if (_hash in self.history) {
+                        self.history[_hash]();
+                    } else if (!_hash.startsWith("__")) {
+                        mainDiv.load_link(_hash);
+                    }
                 });
             }
 
