34d6efff6623daa06a7952e2155cbc784c455548
Yves Lavoie <ylavoie@yveslavoie.com>
2020-12-06 23:07:59 -0500

    babel/polyfill is deprecated

--- UI/js-src/polyfills.js
+++ UI/js-src/polyfills.js
@@ -1,3 +1,7 @@
 /** @format */
 
-import "babel-polyfill";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+// Ensure that we polyfill ES6 compat for anything web-related, if it exists.
+import "core-js/web";
