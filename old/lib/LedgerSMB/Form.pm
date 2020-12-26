14e9f3c61d343b45b0fe9fb23c76d6393333c3c8
Erik Huelsmann <ehuels@gmail.com>
2020-12-26 14:34:56 +0000

    Update version

--- old/lib/LedgerSMB/Form.pm
+++ old/lib/LedgerSMB/Form.pm
@@ -118,8 +118,8 @@ sub new {
         $self->{nextsub} //= '';
         $self->{action} //= $self->{nextsub};
     }
-    $self->{version}   = '1.8.9-dev';
-    $self->{dbversion} = '1.8.9-dev';
+    $self->{version}   = '1.8.9';
+    $self->{dbversion} = '1.8.9';
 
 
     $self;
--- sql/Pg-database.sql
+++ sql/Pg-database.sql
@@ -1348,7 +1348,7 @@ sinumber|1
 sonumber|1
 yearend|1
 businessnumber|1
-version|1.8.9-dev
+version|1.8.9
 closedto|\N
 revtrans|1
 ponumber|1
