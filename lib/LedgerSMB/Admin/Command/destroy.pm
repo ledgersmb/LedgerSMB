50ce99ebd66dd6807239512af99bedf99edf9252
Yves Lavoie <ylavoie@yveslavoie.com>
2021-03-21 15:43:31 -0400

    Update copyright

--- lib/LedgerSMB/Admin/Command/destroy.pm
+++ lib/LedgerSMB/Admin/Command/destroy.pm
@@ -169,7 +169,7 @@ protocol.
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (C) 2020 The LedgerSMB Core Team
+Copyright (C) 2021 The LedgerSMB Core Team
 
 This file is licensed under the GNU General Public License version 2, or at your
 option any later version.  A copy of the license should have been included with
