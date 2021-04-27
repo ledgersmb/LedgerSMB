8b215c83cfa98799ce922db2d2882a38db4359d2
Erik Huelsmann <ehuels@gmail.com>
2021-04-27 20:47:50 +0200

    Improve upgrade script based on real-life data
    As we found real-life data where the records in the `transactions`
    table were still linked to the `invoice` table and `invoice_tax_form`,
    remove those records before deleting the ones in `transactions`.
    
    Also change the namespace to which the salvaged files are stored,
    in order to prevent collision of multiple databases (as much as
    possible).

--- sql/changes/1.6/track-deleted-transactions.sql
+++ sql/changes/1.6/track-deleted-transactions.sql
@@ -6,6 +6,22 @@ delete from voucher v
                       and not exists (select 1 from gl where t.id = gl.id)
                       and v.trans_id = t.id);
 
+delete from invoice_tax_form itf
+ where exists (select 1 from invoice i
+                where itf.invoice_id = i.id
+                      and exists (select 1 from transactions t
+                                   where not exists (select 1 from ap where t.id = ap.id)
+                                         and not exists (select 1 from ar where t.id = ar.id)
+                                         and not exists (select 1 from gl where t.id = gl.id)
+                                         and i.trans_id = t.id));
+
+delete from invoice i
+ where exists (select 1 from transactions t
+                where not exists (select 1 from ap where t.id = ap.id)
+                      and not exists (select 1 from ar where t.id = ar.id)
+                      and not exists (select 1 from gl where t.id = gl.id)
+                      and i.trans_id = t.id);
+
 delete from transactions t
  where not exists (select 1 from ap where t.id = ap.id)
        and not exists (select 1 from ar where t.id = ar.id)
