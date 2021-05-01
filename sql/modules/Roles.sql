d206cf2492149f55dc66a28b316338723eee00f8
Yves Lavoie <ylavoie@yveslavoie.com>
2021-05-01 18:35:16 -0400

    Lsmb roles check (#5407)
    * Validate role parameters
    
    * Reduce default test database name to prevent roles from having more than 63 bytes
    
    * Add role validation tests
    
    * Adjust test count

--- sql/modules/Roles.sql
+++ sql/modules/Roles.sql
@@ -16,6 +16,12 @@ BEGIN
   -- Make sure to evaluate the role once because the optimizer
   -- uses it as a filter on every row otherwise
   SELECT lsmb__role(in_role) INTO t_in_role;
+  IF LENGTH(t_in_role) > 63 THEN
+    RAISE EXCEPTION
+      'Role % has more than 63 bytes. Truncation of % bytes will happen',
+      t_in_role,LENGTH(t_in_role)-63
+      USING ERRCODE = 'name_too_long';
+  END IF;
   PERFORM rolname FROM pg_roles WHERE rolname = t_in_role;
   IF FOUND THEN
      RETURN TRUE;
@@ -32,7 +38,21 @@ DROP FUNCTION IF EXISTS lsmb__grant_role(text, text);
 CREATE OR REPLACE FUNCTION lsmb__grant_role(in_child text, in_parent text)
 RETURNS BOOL LANGUAGE PLPGSQL SECURITY INVOKER AS
 $$
+DECLARE
+  t_in_role text;
 BEGIN
+   SELECT lsmb__role(in_parent) INTO t_in_role;
+   PERFORM rolname FROM pg_roles WHERE rolname = t_in_role;
+   IF NOT FOUND THEN
+      RAISE EXCEPTION 'Role % not found', t_in_role
+      USING ERRCODE = 'invalid_role_specification';
+   END IF;
+   SELECT lsmb__role(in_child) INTO t_in_role;
+   PERFORM rolname FROM pg_roles WHERE rolname = t_in_role;
+   IF NOT FOUND THEN
+      RAISE EXCEPTION 'Role % not found', t_in_role
+      USING ERRCODE = 'invalid_role_specification';
+   END IF;
    EXECUTE 'GRANT ' || quote_ident(lsmb__role(in_parent)) || ' TO '
    || quote_ident(lsmb__role(in_child));
    RETURN TRUE;
@@ -42,7 +62,15 @@ $$;
 CREATE OR REPLACE FUNCTION lsmb__grant_exec(in_role text, in_func text)
 RETURNS BOOL LANGUAGE PLPGSQL SECURITY INVOKER AS
 $$
+DECLARE
+  t_in_role text;
 BEGIN
+   SELECT lsmb__role(in_role) INTO t_in_role;
+   PERFORM rolname FROM pg_roles WHERE rolname = t_in_role;
+   IF NOT FOUND THEN
+      RAISE EXCEPTION 'Role % not found', t_in_role
+      USING ERRCODE = 'invalid_role_specification';
+   END IF;
    EXECUTE 'GRANT EXECUTE ON FUNCTION ' || in_func || ' TO '
    || quote_ident(lsmb__role(in_role));
    RETURN TRUE;
@@ -61,9 +89,18 @@ CREATE OR REPLACE FUNCTION lsmb__grant_perms
 SECURITY INVOKER
 LANGUAGE PLPGSQL AS
 $$
+DECLARE
+  t_in_role text;
 BEGIN
+   SELECT lsmb__role(in_role) INTO t_in_role;
+   PERFORM rolname FROM pg_roles WHERE rolname = t_in_role;
+   IF NOT FOUND THEN
+      RAISE EXCEPTION 'Role % not found', t_in_role
+      USING ERRCODE = 'invalid_role_specification';
+   END IF;
    IF upper(in_perms) NOT IN ('ALL', 'INSERT', 'UPDATE', 'SELECT', 'DELETE') THEN
-      RAISE EXCEPTION 'Invalid permission';
+      RAISE EXCEPTION 'Invalid permission'
+      USING ERRCODE = 'invalid_grantor';
    END IF;
    EXECUTE 'GRANT ' || in_perms || ' ON ' || quote_ident(in_table)
    || ' TO ' ||  quote_ident(lsmb__role(in_role));
@@ -83,9 +120,18 @@ CREATE OR REPLACE FUNCTION lsmb__grant_perms
 SECURITY INVOKER
 LANGUAGE PLPGSQL AS
 $$
+DECLARE
+  t_in_role text;
 BEGIN
+   SELECT lsmb__role(in_role) INTO t_in_role;
+   PERFORM rolname FROM pg_roles WHERE rolname = t_in_role;
+   IF NOT FOUND THEN
+      RAISE EXCEPTION 'Role % not found', t_in_role
+      USING ERRCODE = 'invalid_role_specification';
+   END IF;
    IF upper(in_perms) NOT IN ('ALL', 'INSERT', 'UPDATE', 'SELECT', 'DELETE') THEN
-      RAISE EXCEPTION 'Invalid permission';
+      RAISE EXCEPTION 'Invalid permission'
+      USING ERRCODE = 'invalid_grantor';
    END IF;
    EXECUTE 'GRANT ' || in_perms
    || '(' || array_to_string(quote_ident_array(in_cols), ', ')
@@ -108,7 +154,8 @@ BEGIN
    SELECT lsmb__role(in_role) INTO t_in_role;
    PERFORM rolname FROM pg_roles WHERE rolname = t_in_role;
    IF NOT FOUND THEN
-      RAISE EXCEPTION 'Role not found';
+      RAISE EXCEPTION 'Role % not found', t_in_role
+      USING ERRCODE = 'invalid_role_specification';
    END IF;
    PERFORM * FROM menu_node
      WHERE menu AND id = in_node_id;
@@ -116,7 +163,8 @@ BEGIN
       RAISE EXCEPTION 'Cannot grant to submenu';
    END IF;
    IF in_perm_type NOT IN ('allow', 'deny') THEN
-      RAISE EXCEPTION 'Invalid perm type';
+      RAISE EXCEPTION 'Invalid perm type'
+      USING ERRCODE = 'invalid_grantor';
    END IF;
    PERFORM * FROM menu_acl
      WHERE node_id = in_node_id
