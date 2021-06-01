8f6734d656e32a293a9b9b7a17e23ebcb8962f3e
Yves Lavoie <ylavoie@yveslavoie.com>
2021-06-01 14:29:50 -0400

    Remove useless package regenerator-runtime. Update pakage-lock
    Add source-map for development.

--- UI/js-src/polyfills.js
+++ UI/js-src/polyfills.js
@@ -1,7 +1,6 @@
 /** @format */
 
 import "core-js/stable";
-import "regenerator-runtime/runtime";
 
 // Ensure that we polyfill ES6 compat for anything web-related, if it exists.
 import "core-js/web";
