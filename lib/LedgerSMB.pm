1c2717aed1069011299867a57bb55efd5707e191
Erik Huelsmann <ehuels@gmail.com>
2021-06-03 15:05:21 -0400

    Update version

--- lib/LedgerSMB.pm
+++ lib/LedgerSMB.pm
@@ -206,7 +206,7 @@ use LedgerSMB::Setting;
 use LedgerSMB::Template;
 use LedgerSMB::Template::UI;
 
-our $VERSION = '1.8.14-dev';
+our $VERSION = '1.8.14';
 
 my $logger = Log::Log4perl->get_logger('LedgerSMB');
 
