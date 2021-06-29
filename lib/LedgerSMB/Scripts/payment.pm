731b3bf7f58798069e0a77be26bb356fdc916d10
Aung Zaw Win <aungzwin10@gmail.com>
2021-06-29 18:36:35 +0630

    Storing nulll instead of the empty string

--- lib/LedgerSMB/Scripts/payment.pm
+++ lib/LedgerSMB/Scripts/payment.pm
@@ -1360,7 +1360,7 @@ sub post_payment {
                 push @amount, $temporary_discount;
                 push @cash_account_id, $discount_account_id;
                 push @source, $locale->text('Applied discount');
-                push @memo, "";
+                push @memo, undef;
                 push @transaction_id, $array_options[$ref]->{invoice_id};
             }
 
