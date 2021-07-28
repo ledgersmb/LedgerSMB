f226d4262e440dfbfdad88c29fa3896249853570
Erik Huelsmann <ehuels@gmail.com>
2021-07-28 19:13:12 +0200

    Fix repeated saving of invoices popping up an SQL error
    Due to the "ARAP" key missing in the `$form` object, the IIAA module
    was unable to identify the cash account on which to post the payment.
    
    Fixes #5679.

--- old/bin/ir.pl
+++ old/bin/ir.pl
@@ -64,6 +64,7 @@ sub copy_to_new{
 }
 
 sub edit_and_save {
+    $form->{ARAP} = 'AP';
     my $draft = LedgerSMB::DBObject::Draft->new({base => $form});
     $draft->delete();
     delete $form->{id};
@@ -126,6 +127,7 @@ sub add {
 }
 
 sub edit {
+    $form->{ARAP} = 'AP';
 
     &invoice_links;
     &prepare_invoice;
