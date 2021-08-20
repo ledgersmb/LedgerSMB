34b0cfa5609fdb56b09150eea203f27ba48428a1
Erik Huelsmann <ehuels@gmail.com>
2021-08-20 22:17:33 +0200

    Set Content-Security-Policy on workflow script responses
    Addresses CVE-2021-3731 - Clickjacking vulnerability

--- lib/LedgerSMB/PSGI.pm
+++ lib/LedgerSMB/PSGI.pm
@@ -91,6 +91,9 @@ sub old_app {
         return Plack::Util::response_cb(
             $handler->(@_),
             sub {
+                Plack::Util::header_set($_[0]->[1],
+                                        'Content-Security-Policy',
+                                        q{frame-ancestors 'self'});
                 if (not Plack::Util::header_exists($_[0]->[1],
                                                    'X-LedgerSMB-App-Content')) {
                     Plack::Util::header_push($_[0]->[1],
@@ -159,6 +162,9 @@ sub psgi_app {
         }
     };
 
+    Plack::Util::header_set($res->[1],
+                            'Content-Security-Policy',
+                            q{frame-ancestors 'self'});
     return $res;
 }
 
