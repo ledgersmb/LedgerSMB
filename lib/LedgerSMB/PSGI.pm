5bbc97740996796205fae920895d08430dc43d25
Erik Huelsmann <ehuels@gmail.com>
2021-08-31 20:50:41 +0200

    Fix regression with downloading of backups

--- lib/LedgerSMB/PSGI.pm
+++ lib/LedgerSMB/PSGI.pm
@@ -167,10 +167,13 @@ sub psgi_app {
         }
     };
 
-    Plack::Util::header_set($res->[1],
-                            'Content-Security-Policy',
-                            q{frame-ancestors 'self'});
-    return $res;
+    return Plack::Util::response_cb($res,
+                                    sub {
+                                        my $res = shift;
+                                        Plack::Util::header_set($res->[1],
+                                                                'Content-Security-Policy',
+                                                                q{frame-ancestors 'self'});
+                                    });
 }
 
 sub _run_old {
