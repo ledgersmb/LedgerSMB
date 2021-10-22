ledgersmb/LedgerSMB

commit c9f650a47944f2d11ae5579ed169aaa073a79ad6

Immediate e-mail sending (#6107)
* Support passing parameters on e-mail workflow creation

Move all database-side handling of attachments to the persister: actions
are not triggered on workflow creation -- the persister *is*. (In
addition to this setup being much better separation of concerns.)

* Allow passing context data into an e-mail workflow upon creation

This supports the use-case of sending the e-mail off directly after
the mail workflow has been created, when the "send immediately"
flag is set in the context data.

* Refactor invoice/order e-mail workflow creation

Make use of the fact that context data can be passed into a workflow
upon creation in order to seed (and optionally immediately send) an
e-mail for orders and invoices.

lib/LedgerSMB/Workflow/Persister/Email.pm
@@ -21,6 +21,135 @@ use strict;
 use base qw( LedgerSMB::Workflow::Persister::ExtraData );
 
 use JSON::MaybeXS;
+use Log::Any qw($log);
+use List::Util qw(any);
+
+use LedgerSMB::File::Email;
+use LedgerSMB::Magic qw(FC_EMAIL);
+
+
+my $json = JSON::MaybeXS->new(
+    utf8 => 0, pretty => 0, indent => 0, convert_blessed => 0,
+    allow_bignum => 1, canonical => 0, space_before => 0, space_after => 0
+    );
+
+
+sub _save_email_data {
+    my ($self, $wf) = @_;
+    my $ctx         = $wf->context;
+    my $dbh         = $self->handle;
+    my $old_data    = $ctx->param( '_email_data' );
+    my $expansions  = $ctx->param( 'expansions' );
+    my $data        = {
+        expansions  => ( $expansions ? $json->encode( $expansions ) : undef ),
+        ( map { $_ => $ctx->param( $_ ) }
+          qw(from to cc bcc notify subject body) )
+    };
+
+    # Don't save data if the in-memory data hasn't been updated...
+    if ( not defined $old_data
+         or ( any { ($old_data->{$_} // '') ne ($data->{$_} // '') }
+              qw(from to cc bcc notify subject body expansions) ) ) {
+
+        $dbh->do(
+            q{
+            INSERT INTO email (workflow_id, "from", "to", cc, bcc, "notify",
+                               subject, body, expansions)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+              ON CONFLICT (workflow_id)
+                 DO UPDATE SET "from" = $2, "to" = $3, cc = $4, bcc = $5, "notify" = $6,
+                        subject = $7, body = $8, expansions = $9
+            }, {},
+            $wf->id, $data->@{qw(from to cc bcc notify subject body expansions)})
+            or $log->error($dbh->errstr);
+    }
+}
+
+
+sub _fetch_attachments {
+    my ($self, $wf) = @_;
+
+    my $dbh = $self->handle;
+    my $sth = $dbh->prepare(
+        q{SELECT id, file_name, description,
+                 (select mime_type from mime_type
+                   where id = mime_type_id) as mime_type
+           FROM file_email
+           WHERE ref_key = ?}
+        ) or die $dbh->errstr;
+    $sth->execute( $wf->id )
+        or die $sth->errstr;
+
+    my $rows = $sth->fetchall_arrayref( {} );
+    $sth->finish;
+
+    $wf->context->param( 'attachments' => $rows);
+    for my $row ($rows->@*) {
+        $row->{content} = sub {
+            my %args = @_;
+            return $row->{_content} if $row->{_content};
+
+            my $sth = $dbh->prepare(
+                q{select content from file_email where id = ?})
+                or die $sth->errstr;
+            $sth->execute( $row->{id} )
+                or die $sth->errstr;
+            ($row->{_content}) = $sth->fetchrow_array();
+            if (not defined $row->{_content} and $sth->err) {
+                die $sth->errstr;
+            }
+            $sth->finish;
+
+            return $args{disable_cache} ?
+                delete $row->{_content} : $row->{_content};
+        };
+    }
+}
+
+
+=head2 create_workflow($wf)
+
+Implements Workflow::Persister::DBI protocol; creates a record in the database
+and saves e-mail data from the workflow context, if available.
+
+Additionally, saves any attachments given in the C<attachments> context
+parameter. If it exists, it is assumed to be an array of hashes to be
+passed to the C<attach> method.  On return, the C<attachments> parameter
+has been replaced by the content as specified by C<fetch_extra_workflow_data>.
+
+=cut
+
+
+sub create_workflow {
+    my ( $self, $wf ) = @_;
+
+    my $wf_id  = $self->SUPER::create_workflow( $wf );
+    $self->_save_email_data( $wf );
+
+    if (my $attachments = $wf->context->param( 'attachments' )) {
+        for my $attachment ($attachments->@*) {
+            $self->attach( $wf, $attachment );
+        }
+        $self->_fetch_attachments( $wf );
+    }
+
+    return $wf_id;
+}
+
+=head2 update_workflow($wf)
+
+Implements Workflow::Persister::DBI protocol; saves the e-mail data fields
+when they have changed.
+
+=cut
+
+
+sub update_workflow {
+    my ( $self, $wf ) = @_;
+
+    $self->SUPER::update_workflow( $wf );
+    $self->_save_email_data( $wf );
+}
 
 =head2 fetch_extra_workflow_data($wf)
 
@@ -58,51 +187,59 @@ named argument C<disable_cache> to prevent caching the content in-memory:
 
 =cut
 
-my $json = JSON::MaybeXS->new( utf8 => 0 );
-
 sub fetch_extra_workflow_data {
     my ($self, $wf) = @_;
+    my $ctx = $wf->context;
     $self->SUPER::fetch_extra_workflow_data($wf);
-    if ( my $expansions = $wf->context->param( 'expansions' ) ) {
-        $wf->context->param( 'expansions', $json->decode( $expansions ) );
+    $self->context->param(
+        '_email_data',
+        { map { $_ => $ctx->param( $_ ) }
+          qw(from to cc bcc notify subject body expansions)
+        });
+    if ( my $expansions = $ctx->param( 'expansions' ) ) {
+        $ctx->param( 'expansions', $json->decode( $expansions ) );
     }
 
-    my $dbh = $self->handle;
-    my $sth = $dbh->prepare(
-        q{SELECT id, file_name, description,
-                 (select mime_type from mime_type
-                   where id = mime_type_id) as mime_type
-           FROM file_email
-           WHERE ref_key = ?}
-        ) or die $dbh->errstr;
-    $sth->execute($wf->id)
-        or die $sth->errstr;
-
-    my $rows = $sth->fetchall_arrayref({});
-    $sth->finish;
-
-    $wf->context->param( 'attachments' => $rows);
-    for my $row ($rows->@*) {
-        $row->{content} = sub {
-            my %args = @_;
-            return $row->{_content} if $row->{_content};
-
-            my $sth = $dbh->prepare(
-                q{select content from file_email where id = ?})
-                or die $sth->errstr;
-            $sth->execute($row->{id})
-                or die $sth->errstr;
-            ($row->{_content}) = $sth->fetchrow_array();
-            if (not defined $row->{_content} and $sth->err) {
-                die $sth->errstr;
-            }
-            $sth->finish;
-
-            return $args{disable_cache} ?
-                delete $row->{_content} : $row->{_content};
-        };
-    }
+    $self->_fetch_attachments( $wf );
 }
+
+
+=head2 attach($wf, $atts)
+
+Attaches a file to the e-mail. C<$atts> is a hashref with
+the following keys in the hash:
+
+=over
+
+=item * content
+
+=item * description
+
+=item * file_name
+
+=item * mime_type
+
+=back
+
+=cut
+
+sub attach {
+    my ($self, $wf, $data) = @_;
+    my $file               = LedgerSMB::File::Email->new(
+        dbh            => $self->handle,
+        file_class     => FC_EMAIL,
+        ref_key        => $wf->id,
+        mime_type_text => $data->{mime_type},
+        $data->%{qw( content description file_name )}
+        );
+
+    $file->get_mime_type;
+    $file->attach;
+
+    return;
+}
+
+
 
 
 1;

lib/LedgerSMB/Workflow/Action/Email.pm
@@ -60,13 +60,9 @@ use Email::MessageID;
 use Email::Sender::Simple;
 use Email::Stuffer;
 
-use JSON::MaybeXS;
-
 use Log::Any qw($log);
 use Workflow::Factory qw(FACTORY);
 
-use LedgerSMB::File::Email;
-use LedgerSMB::Magic qw(FC_EMAIL);
 use LedgerSMB::Mailer::TransportSMTP;
 use LedgerSMB::Sysconfig;
 
@@ -148,20 +144,10 @@ keys:
 sub attach {
     my ($self, $wf) = @_;
     my $persister   = FACTORY()->get_persister( $wf->type );
-    my $file        = LedgerSMB::File::Email->new(dbh => $persister->handle);
     my $att         = $wf->context->param( 'attachment' );
 
-    $file->ref_key($wf->id);
-    $file->file_class(FC_EMAIL);
-    $file->file_name($att->{file_name});
-    $file->description($att->{description});
-    $file->content($att->{content});
-    $file->mime_type_text($att->{mime_type});
-    $file->get_mime_type;
-
-    $file->attach;
-
-    FACTORY()->get_persister( $wf->type )->fetch_extra_workflow_data($wf);
+    $persister->attach( $wf, $att );
+    $persister->fetch_extra_workflow_data( $wf );
     return;
 }
 
@@ -267,35 +253,10 @@ All fields in the e-mail are optional for this step.
 
 =cut
 
-my $json = JSON::MaybeXS->new(
-    utf8 => 0, pretty => 0, indent => 0, convert_blessed => 0,
-    allow_bignum => 1, canonical => 0, space_before => 0, space_after => 0
-    );
-
 sub save {
     my ($self, $wf) = @_;
-    my $dbh = $self->_factory->get_persister_for_workflow_type('Email')->handle;
-
-    my @values =
-        map { $wf->context->param($_) } qw(from to cc bcc notify subject body);
-
-    if ( my $expansions = $wf->context->param( 'expansions' ) ) {
-        push @values, $json->encode( $expansions );
-    }
-    else {
-        push @values, undef;
-    }
-    $dbh->do(
-        q{
-        INSERT INTO email (workflow_id, "from", "to", cc, bcc, "notify",
-                           subject, body, expansions)
-            VALUES  (?, ?, ?, ?, ?, ?, ?, ?, ?)
-          ON CONFLICT (workflow_id)
-             DO UPDATE SET "from" = ?, "to" = ?, cc = ?, bcc = ?, "notify" = ?,
-                        subject = ?, body = ?, expansions = ?
-        }, {},
-        $wf->id, @values, @values)
-        or $log->error($dbh->errstr);
+    # Saving is built into the persister and happens automatically
+    # after each successful action; no additional code required
 }
 
 

lib/LedgerSMB/Workflow/Action/SpawnWorkflow.pm
@@ -27,6 +27,8 @@ The action supports the following two
 
 =item * spawn_type
 
+=item * context_param
+
 =item * description
 
 =back
@@ -50,9 +52,10 @@ use warnings;
 use parent qw( Workflow::Action );
 
 use Log::Any qw($log);
+use Workflow::Context;
 use Workflow::Factory qw(FACTORY);
 
-my @PROPS = qw( spawn_type description );
+my @PROPS = qw( spawn_type context_param description );
 __PACKAGE__->mk_accessors(@PROPS);
 
 =head2 init($wf, $params)
@@ -66,6 +69,7 @@ sub init {
     $self->SUPER::init($wf, $params);
 
     $self->spawn_type( $params->{spawn_type} );
+    $self->context_param( $params->{context_param} );
     $self->description( $params->{description}
                         // ('Created new workflow of type: '
                             . $params->{spawn_type}) );
@@ -82,7 +86,16 @@ Implements the C<Workflow::Action> protocol.
 sub execute {
     my ($self, $wf) = @_;
 
-    my $new_wf = FACTORY()->create_workflow( $self->spawn_type );
+    my $context;
+    if ( $self->context_param ) {
+        my $context_data = $wf->context->param( $self->context_param );
+
+        $context = Workflow::Context->new();
+        for my $key (keys $context_data->%*) {
+            $context->param( $key => $context_data->{$key} );
+        }
+    }
+    my $new_wf = FACTORY()->create_workflow( $self->spawn_type, $context );
     my $wf_id  = $new_wf->id;
     $wf->context->param( spawned_workflow => $new_wf );
     $wf->add_history(

old/bin/io.pl
@@ -1430,35 +1430,24 @@ sub print_form {
     totalqty totalship totalweight totalparts totalservices totalweightship
 
     paid subtotal total
-                         ))};
-        if ($order) {
-            ($wf_id) =
-                $form->{dbh}->selectrow_array(
-                    q{select workflow_id from oe where id = ?},
-                    {}, $form->{id});
-
-            my $trans_wf = FACTORY()->fetch_workflow( 'Order/Quote', $wf_id );
-            $trans_wf->execute_action( 'e_mail' );
-            $wf = $trans_wf->context->param( 'spawned_workflow' );
-        }
-        else {
-            ($wf_id) =
-                $form->{dbh}->selectrow_array(
-                    q{select workflow_id from transactions where id = ?},
-                    {}, $form->{id});
-
-            my $trans_wf = FACTORY()->fetch_workflow( 'AR/AP', $wf_id );
-            if (grep { $_ eq 'save' } $trans_wf->get_current_actions) {
-                $trans_wf->execute_action( 'save' );
-            }
-            if (grep { $_ eq 'post' } $trans_wf->get_current_actions) {
-                $trans_wf->execute_action( 'post' );
-            }
-            $trans_wf->execute_action( 'E-mail' );
-            $wf = $trans_wf->context->param( 'spawned_workflow' );
-        }
+                       ))};
         my $body = $template->{output};
         utf8::encode($body) if utf8::is_utf8($body);  ## no critic
+        my $email_data = {
+            immediateSend => $form->{immediate},
+            expansions    => \%expansions,
+            body          => $form->{message},
+            from          => $form->get_setting( 'default_email_from' ),
+            subject       => ($form->{subject}
+                              // qq|$form->{label} $form->{"${inv}number"}|),
+            attachments   => [
+                { content => $body,
+                  mime_type => $template->{mimetype},
+                  file_name => ($form->{formname} . '-'
+                                . $form->{"${inv}number"} . '.'
+                                . lc($form->{format})),
+                } ],
+        };
         my %map = (
             email   => 'to',
             cc      => 'cc',
@@ -1473,29 +1462,37 @@ sub print_form {
             if ( $form->{$type} ) {
                 push @addresses, $form->{$type};
             }
-            $wf->context->param( $map{$type} => join(', ', @addresses) );
+            $email_data->{$map{$type}} = join(', ', @addresses);
         }
-        $wf->context->param( body => $form->{message} );
-        $wf->context->param(
-            subject => ($form->{subject}
-                        // qq|$form->{label} $form->{"${inv}number"}|) );
-        $wf->context->param(
-            expansions => \%expansions
-            );
-        $wf->context->param(
-            attachment => {
-                content => $body,
-                mime_type => $template->{mimetype},
-                file_name =>
-                    $form->{formname} . '-'. $form->{"${inv}number"} . '.' . lc($form->{format}),
-            });
-        $wf->context->param(from => $form->get_setting( 'default_email_from' ));
-        $wf->execute_action( 'Attach' );
 
-        $wf->execute_action( 'Send' ) if $form->{immediate};
+        my $trans_wf;
+        if ($order) {
+            ($wf_id) =
+                $form->{dbh}->selectrow_array(
+                    q{select workflow_id from oe where id = ?},
+                    {}, $form->{id});
 
+            $trans_wf = FACTORY()->fetch_workflow( 'Order/Quote', $wf_id );
+        }
+        else {
+            ($wf_id) =
+                $form->{dbh}->selectrow_array(
+                    q{select workflow_id from transactions where id = ?},
+                    {}, $form->{id});
+
+            $trans_wf = FACTORY()->fetch_workflow( 'AR/AP', $wf_id );
+            if (grep { $_ eq 'save' } $trans_wf->get_current_actions) {
+                $trans_wf->execute_action( 'save' );
+            }
+            if (grep { $_ eq 'post' } $trans_wf->get_current_actions) {
+                $trans_wf->execute_action( 'post' );
+            }
+        }
+
+        $trans_wf->context->param( 'email-data' => $email_data );
+        $trans_wf->execute_action( 'E-mail' );
+        $wf = $trans_wf->context->param( 'spawned_workflow' );
         my $id = $wf->id;
-
         if (not $form->{header}) {
             print "Location: email.pl?id=$id&action=render&callback=$form->{script}%3F"
                 . "id%3D$form->{id}%26action%3Dedit\n";

workflows/arap.actions.xml
@@ -22,7 +22,8 @@ TODO! Check workflow when 'separate duties' is false!
           class="LedgerSMB::Workflow::Action::Null" />
   <action name="E-mail"
           class="LedgerSMB::Workflow::Action::SpawnWorkflow"
-          spawn_type="Email" />
+          spawn_type="Email"
+          context_param="email-data" />
   <action name="print_and_save"
           description="Printed and saved"
           class="LedgerSMB::Workflow::Action::Null" />

workflows/email.conditions.xml
@@ -6,10 +6,17 @@
              class="Workflow::Condition::Evaluate"/>
   <condition name="hasSubject" test="$context->{subject} ? 1 : 0"
              class="Workflow::Condition::Evaluate"/>
+  <condition name="wantImmediate" test="$context->{immediateSend} ? 1 : 0"
+             class="Workflow::Condition::Evaluate"/>
   <condition name="Complete"
              class="Workflow::Condition::LazyAND">
     <param name="condition" value="hasTo" />
     <param name="condition" value="hasBody" />
     <param name="condition" value="hasSubject" />
+  </condition>
+  <condition name="sendImmediately"
+             class="Workflow::Condition::LazyAND">
+    <param name="condition" value="Complete"/>
+    <param name="condition" value="wantImmediate"/>
   </condition>
 </conditions>

workflows/email.workflow.xml
@@ -2,9 +2,18 @@
   <type>Email</type>
   <persister>Email</persister>
  <description>Handles lifecycle of e-mail</description>
- <state name="INITIAL">
-   <action name="Attach" resulting_state="INITIAL"/>
-   <action name="Update" resulting_state="INITIAL" />
+ <state name="INITIAL" autorun="yes">
+   <action name="Send"    resulting_state="EXPANDED">
+     <condition name="sendImmediately" />
+   </action>
+   <!-- Save data as provided at workflow creation time -->
+   <action name="Update" resulting_state="CREATED">
+     <condition name="!sendImmediately"/>
+   </action>
+ </state>
+ <state name="CREATED">
+   <action name="Attach" resulting_state="NOCHANGE"/>
+   <action name="Update" resulting_state="NOCHANGE" />
    <action name="Send"   resulting_state="EXPANDED" />
    <action name="Cancel" resulting_state="CANCELLED"/>
 
@@ -25,7 +34,7 @@
  </state>
  -->
 
- <state name="EXPANDED" autorun="true">
+ <state name="EXPANDED" autorun="yes">
    <action name="do-send" resulting_state="SUCCESS" />
  </state>
  <state name="SUCCESS" />

workflows/order.actions.xml
@@ -8,9 +8,10 @@
   <action name="ship_to"
           description="Added shipping information"
           class="LedgerSMB::Workflow::Action::Null" />
-  <action name="e_mail"
+  <action name="E-mail"
           class="LedgerSMB::Workflow::Action::SpawnWorkflow"
-          spawn_type="Email" />
+          spawn_type="Email"
+          context_param="email-data" />
   <action name="print_and_save"
           description="Printed and saved"
           class="LedgerSMB::Workflow::Action::Null" />

commit 8a9a7e7063fca0eb20d7168b7236bb49fe9f2796

Fix bug introduced in #6107: $self doesn't have a 'context' method
Instead, use the `$ctx` variable that's already initialized.

lib/LedgerSMB/Workflow/Persister/Email.pm
@@ -191,7 +191,7 @@ sub fetch_extra_workflow_data {
     my ($self, $wf) = @_;
     my $ctx = $wf->context;
     $self->SUPER::fetch_extra_workflow_data($wf);
-    $self->context->param(
+    $ctx->param(
         '_email_data',
         { map { $_ => $ctx->param( $_ ) }
           qw(from to cc bcc notify subject body expansions)