ae07cbe4872bb48a3d17fad45c236d8a7db2c4fd
Erik Huelsmann <ehuels@gmail.com>
2021-12-03 20:06:39 +0100

    Move setup.pl back to dojo-only
    Apparently, Vue and Dojo are trempling on each other: checkmark inputs are broken.
    
    Fix this by making Dojo parse-on-load again and *not* load Vue when there's no
    '#main' element in the DOM.

--- UI/js-src/lsmb/main-vue.js
+++ UI/js-src/lsmb/main-vue.js
@@ -7,6 +7,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 const registry = require("dijit/registry");
 const dojoParser = require("dojo/parser");
 const dojoDOM = require("dojo/dom");
+const domClass = require("dojo/dom-class");
 
 import Home from "./Home.vue";
 import ServerUI from "./ServerUI";
@@ -30,14 +31,29 @@ export const app = createApp({
     components: [Home, ServerUI],
     mounted() {
         let m = dojoDOM.byId("main");
-        dojoParser.parse(m);
-        window.__lsmbLoadLink = (url) => this.$router.push(url);
+        this.$nextTick(
+            () => {
+                dojoParser.parse(m);
+                domClass.add(document.body, "done-parsing");
+            });
+        window.__lsmbLoadLink =
+            url => this.$router.push(url);
+
         let r = registry.byId("top_menu");
         if (r) {
             // Setup doesn't have top_menu
             r.load_link = (url) => this.$router.push(url);
         }
+    },
+    beforeUpdate() {
+        domClass.remove(document.body, "done-parsing");
+    },
+    updated() {
+        domClass.add(document.body, "done-parsing");
     }
 })
-    .use(router)
-    .mount("#main");
+    .use(router);
+
+if (dojoDOM.byId("main")) {
+    app.mount("#main");
+}
