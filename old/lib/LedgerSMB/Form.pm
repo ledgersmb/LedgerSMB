347d5ab8e4fc34a19c9d56a8582cd2e7823abeb3
Erik Huelsmann <ehuels@gmail.com>
2022-03-11 08:36:43 -0500

    Update version

--- old/lib/LedgerSMB/Form.pm
+++ old/lib/LedgerSMB/Form.pm
@@ -118,8 +118,8 @@ sub new {
         $self->{nextsub} //= '';
         $self->{action} //= $self->{nextsub};
     }
-    $self->{version}   = '1.8.26-dev';
-    $self->{dbversion} = '1.8.26-dev';
+    $self->{version}   = '1.8.26';
+    $self->{dbversion} = '1.8.26';
 
 
     $self;
