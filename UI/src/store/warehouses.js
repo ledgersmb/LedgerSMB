1d2de7e295806c475596bb5f02d02f5d2bba913d
Erik Huelsmann <ehuels@gmail.com>
2022-06-25 22:08:52 +0200

    Use the warehouse server API to modify the store

--- UI/src/store/warehouses.js
+++ UI/src/store/warehouses.js
@@ -11,32 +11,71 @@ export const useWarehousesStore = defineStore("warehouses", {
     },
     actions: {
         async initialize() {
-            if (this.warehouses.length === 0) {
-                this.warehouses.push({ id: 1, description: "ABC" });
+            const response = await fetch("./erp/api/v0/products/warehouses/", {
+                method: "GET"
+            });
+
+            if (response.ok) {
+                this.warehouses = (await response.json()).sort(
+                    (a, b) => b.id - a.id
+                );
+            } else {
+                throw new Error(`HTTP Error: ${response.status}`);
             }
             return Promise.resolve();
         },
         async add(adding) {
-            this.warehouses.push({
-                id: this.warehouses.length * 2,
-                description: adding.description
+            const response = await fetch("./erp/api/v0/products/warehouses/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(adding)
             });
+
+            if (response.ok) {
+                const added = await response.json();
+                this.warehouses.push(added);
+            } else {
+                throw new Error(`HTTP Error: ${response.status}`);
+            }
         },
         async del(id) {
-            let index = this.warehouses.findIndex((w) => w.id === id);
-            if (index !== -1) {
-                this.warehouses.splice(index, 1);
+            const response = await fetch(
+                `./erp/api/v0/products/warehouses/${id}`,
+                { method: "DELETE" }
+            );
+
+            if (response.ok) {
+                let index = this.warehouses.findIndex((w) => w.id === id);
+                if (index !== -1) {
+                    this.warehouses.splice(index, 1);
+                }
             }
         },
         getById(id) {
             return this.warehouses.find((w) => w.id === id);
         },
         async save(id, data) {
-            let warehouse = this.warehouses.find((w) => w.id === id);
-            this.fields.forEach((f) => {
-                warehouse[f] = data[f];
-            });
-            warehouse.id = id; // prevent overwriting 'id'
+            const response = await fetch(
+                `./erp/api/v0/products/warehouses/${id}`,
+                {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(data)
+                }
+            );
+
+            if (response.ok) {
+                const newData = await response.json();
+                const warehouse = this.warehouses.find((w) => w.id === id);
+                this.fields.forEach((f) => {
+                    warehouse[f] = newData[f];
+                });
+                warehouse.id = id; // prevent overwriting 'id'
+            }
         }
     }
 });
