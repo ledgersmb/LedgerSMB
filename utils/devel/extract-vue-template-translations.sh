a0988ba8585b982502258a4fa6aab4da77414ae4
Erik Huelsmann <ehuels@gmail.com>
2022-07-09 16:03:29 +0200

    Fix english translations being included in the POT (#6614)

--- utils/devel/extract-vue-template-translations.sh
+++ utils/devel/extract-vue-template-translations.sh
@@ -10,16 +10,7 @@ set -x
 # Extract Vue strings and make a pot file
 cp UI/src/locales/en.json /tmp/en.json
 npx vue-i18n-extract-translations -v "./UI/src/**/*.?(js|vue)" -l "/tmp/" --def-locale "en" \
-    --key "i18n" --keep-unused >> /dev/null
-i18next-conv --quiet --language "en" --source /tmp/en.json --target /tmp/en.po
-./utils/devel/extract-vue-template-translations-references.pl < /tmp/en.po > /tmp/_en.po
-msgfilter  --sort-output --width=80 \
-    --input=/tmp/_en.po --output-file=/tmp/en.pot true
+    --key "i18n" --keep-unused > /dev/null
+npx i18next-conv --quiet --pot --project LedgerSMB --language "en" -b /tmp/en.json -s /tmp/en.json --target /tmp/en.po
+./utils/devel/extract-vue-template-translations-references.pl < /tmp/en.po
 
-# Freshen locale/LedgerSMB.pot
-msgcat --output-file=/tmp/LedgerSMB.pot --sort-output --add-location \
-        --width=80 locale/LedgerSMB.pot /tmp/en.pot \
-    || exit 1
-
-mv /tmp/LedgerSMB.pot locale/LedgerSMB.pot
-rm /tmp/*.{po,pot,json}
