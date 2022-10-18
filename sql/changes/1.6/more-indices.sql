d69012d58de27c520337adc7613b89d7d241ce74
Neil <neil@fairwindsoft.com>
2022-10-18 16:25:53 -0500

    Fix bug where upgrade script was trying to create an existing identical index.

--- sql/changes/1.6/more-indices.sql
+++ sql/changes/1.6/more-indices.sql
@@ -6,7 +6,7 @@ create index acc_trans_chart_id_idx on acc_trans(chart_id);
 create index ar_entity_credit_account_idx on ar(entity_credit_account);
 create index ap_entity_credit_account_idx on ap(entity_credit_account);
 
-create index invoice_cogs_idx on invoice(parts_id)
+create index if not exists invoice_cogs_idx on invoice(parts_id)
    where (qty + allocated < 0) OR (qty + allocated > 0);
 create index invoice_parts_id_idx on invoice(parts_id);
 
