afd7b0cbcea2088284dda7f3bbfb2c96ea5668f8
Neil <neil@fairwindsoft.com>
2022-10-22 09:22:04 -0500

    Remove tabs for CI.

--- sql/changes/1.8/add-transdate-to-transactions.sql
+++ sql/changes/1.8/add-transdate-to-transactions.sql
@@ -1,12 +1,12 @@
 
-
 ALTER TABLE transactions
    ADD COLUMN transdate date;
 
 UPDATE transactions AS trn
    SET transdate = (SELECT COALESCE(
-		(select ar.transdate from ar where ar.id = trn.id),
-		(select ap.transdate from ap where ap.id = trn.id),
-		(select gl.transdate from gl where gl.id = trn.id),
-		trn.approved_at
-		));
+                (select ar.transdate from ar where ar.id = trn.id),
+                (select ap.transdate from ap where ap.id = trn.id),
+                (select gl.transdate from gl where gl.id = trn.id),
+                trn.approved_at
+                ));
+
