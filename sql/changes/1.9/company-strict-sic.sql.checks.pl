63a9959d9e15e5221b11e4aa41160089f1c33400
Neil Tiffin <neil@fairwindsoft.com>
2022-10-23 13:41:39 -0500

    Fix database upgrades [using demo15 database] (#6879)
    Fix database upgrades:
    
    * Fix missing foreign key upgrades when recreating entity pkey.
    
    * Correct spelling
    
    * Fix transaction delete trying to delete needed transactions.
    
    * Fix bug where upgrade script was trying to create an existing identical index.
    
    * Create previous versions for SQL files which were missing in the previous commits.
    
    * This fixes a logic problem when adding the transdate to table transactions.
    
    The logic:
    1. The transdate column was added, then the ap, ar, and gl tables were
       searched in order to update transactions.transdate.
       (add-transdate-to-transactions.sql).
    2. Then constrain-transaction-tables.sql.checks.pl was executed to
       manually provide any missing dates to the ap, ar, and gl tables.
    
    The fix involves rescanning the ap, ar, and gl tables after the
    manual ajustments to pick up any added dates which is done in
    constrain-transaction-tables.sql just before adding the NOT NULL
    attributes.
    
    * Remove tabs for CI.
    
    * Remove tabs for CI, but also update to match the code in add-transdate-to-transactions.sql and eliminate readding the date to transactions that already have a date.
    
    * Add user delete step for all SIC codes in the company table that are not between 2 and 6 digit numbers inclusive.
    
    * Add company-strict-sic.precheck. Not working.
    
    * Fix precheck capitalization to match check file. Thanks to Erik for the good eyes.

---
+++ sql/changes/1.9/company-strict-sic.sql.checks.pl
@@ -0,0 +1,64 @@
+package _19_uprade_checks;
+
+use LedgerSMB::Database::ChangeChecks;
+
+check q|Ensure that the company table doesn't contain invalid SIC codes.|,
+    query => q|
+        SELECT  DISTINCT sic_code
+        FROM	company
+        WHERE	sic_code IS NOT NULL
+            AND sic_code !~ '^\d{2,6}$'
+        ORDER BY 1;
+    |,
+    description => q|
+The upgrade process found SIC codes in the company table that 
+contain non-digit characters, are less than 2 digits, or more than 6 digits.
+
+These SIC codes must be removed from the company table as they are prohibited 
+by stricter data integrity rules enforced by the update.
+
+Removal of these entries will not affect accounting data. 
+
+Please delete the invalid values listed in the table below by clicking
+the 'Delete invalid SIC codes' button.
+|,
+    tables => {
+        company => {
+            prim_key => [qw(sic_code)]
+        },
+    },
+    on_failure => sub {
+        my ($dbh, $rows) = @_;
+
+        describe;
+
+        grid $rows,
+            name => 'company SIC codes',
+            table => 'company',
+            columns => [qw(sic_code)];
+
+        confirm delete => 'Delete invalid SIC codes';
+    },
+    on_submit => sub {
+        my ($dbh, $rows) = @_;
+        my $confirm = provided 'confirm';
+
+        if ($confirm eq 'delete') {
+            my $delete = $dbh->prepare(
+                q|UPDATE company SET sic_code = NULL WHERE sic_code = ? AND sic_code !~ '^\d{2,6}$';|
+            ) or die 'ERROR preparing sql to NULL invalid SIC codes in company table: ' . $dbh->errstr;
+
+            foreach my $row(@$rows) {
+                $delete->execute(
+                    $row->{sic_code},
+                ) or die 'Failed to NULL invalid SIC code: ' . $dbh->errstr;
+            }
+        }
+        else {
+          die "Unexpected confirmation value found: $confirm";
+        }
+    }
+;
+
+
+1;
