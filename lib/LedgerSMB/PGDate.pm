bd8222bf8d0192f7a8e33f97a6dd5b918dba996d
Yves Lavoie <ylavoie@yveslavoie.com>
2023-01-01 18:03:28 -0500

    Add stringification to PGDate instead of the one inherited

--- lib/LedgerSMB/PGDate.pm
+++ lib/LedgerSMB/PGDate.pm
@@ -252,6 +252,11 @@ sub _formatter {
 # table (being GL>Search-ed without restrictions)
 memoize('_formatter');
 
+# Provide a stringification routine instead of the one inherited
+use overload (
+    q{""}    => 'to_output',
+);
+
 sub to_output {
     my ($self) = @_;
     return '' if not $self->is_date();
