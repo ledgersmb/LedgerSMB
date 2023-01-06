7e2ec6cd46f2a7a0e803d91a1fe060ceb7eade58
Erik Huelsmann <ehuels@gmail.com>
2023-01-06 14:55:25 +0100

    Based on real-world testing, fix latest menu-addition scripts

--- sql/changes/1.11/country-config.sql
+++ sql/changes/1.11/country-config.sql
@@ -1,6 +1,6 @@
 
-insert into menu_node (parent, "position", label, url)
-            values (128, 10, 'Country', 'countries');
+insert into menu_node (id, parent, "position", label, url)
+            values (264, 128, 10, 'Country', 'countries');
 
 alter table country
   add column last_updated timestamp without time zone not null default now();
