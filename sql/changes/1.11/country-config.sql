509af6b7917791fb5a7f4b7168d774b61df7b3ee
Erik Huelsmann <ehuels@gmail.com>
2023-01-15 00:03:23 +0100

    Fix change data capture in 1.11

--- sql/changes/1.11/country-config.sql
+++ sql/changes/1.11/country-config.sql
@@ -5,7 +5,8 @@ insert into menu_node (id, parent, "position", label, url)
 alter table country
   add column last_updated timestamp without time zone not null default now();
 
-create trigger country_modtimestamp
+drop trigger if exists country_update_last_updated on country;
+create trigger country_update_last_updated
    before update on country
-   for each row execute procedure moddatetime(last_updated);
+   for each row execute procedure cdc_update_last_updated();
 
