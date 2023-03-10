d931bbdb70ef7b26ea4248ede5f54ace237528ec
Yves Lavoie <ylavoie@yveslavoie.com>
2023-03-10 02:51:40 -0500

    Add Jest warehouse store test

--- UI/tests/common/mocks/handlers.js
+++ UI/tests/common/mocks/handlers.js
@@ -4,11 +4,13 @@
 import { businessTypesHandlers} from "./store_business-types_handlers";
 import { countriesHandlers } from "./store_countries_handlers";
 import { languageHandlers } from "./store_language_handlers";
+import { warehousesHandlers } from "./store_warehouses_handlers";
 
 export const handlers = [
   // Stores
   ...businessTypesHandlers,
   ...countriesHandlers,
   ...languageHandlers,
+  ...warehousesHandlers,
 ];
 
