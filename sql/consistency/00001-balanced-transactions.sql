a2aef7dbc28bb5d2220259b8b9669585604f6510
Erik Huelsmann <ehuels@gmail.com>
2023-03-14 23:09:07 +0100

    Use precision from configuration table instead of hardcoding 2-digit precision

--- sql/consistency/00001-balanced-transactions.sql
+++ sql/consistency/00001-balanced-transactions.sql
@@ -5,4 +5,7 @@ title: All transactions are balanced
 select trans_id
   from acc_trans
  group by trans_id
-having abs(sum(amount_bc)) > 0.005
+  having abs(sum(amount_bc)) >= power(10, -1*coalesce((select value::numeric
+                                                        from defaults
+                                                       where setting_key = 'decimal_places'),
+                                                       2))/2
