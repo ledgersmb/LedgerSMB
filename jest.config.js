aebf1db68599515425399a47927b4e92f40da7f9
Yves Lavoie <ylavoie@yveslavoie.com>
2023-03-17 04:20:44 -0400

    Add vue serializer

--- jest.config.js
+++ jest.config.js
@@ -200,7 +200,7 @@ module.exports = {
     slowTestThreshold: 5,
 
     // A list of paths to snapshot serializer modules Jest should use for snapshot testing
-    snapshotSerializers: [],
+    snapshotSerializers: ["jest-serializer-vue"],
 
     // The test environment that will be used for testing
     testEnvironment: "jsdom",
