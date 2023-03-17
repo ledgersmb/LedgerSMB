5ecea9e2533e9fe1e4507b384500791abc26366d
Yves Lavoie <ylavoie@yveslavoie.com>
2023-03-17 04:20:15 -0400

    Add Language view test

--- UI/src/views/Languages.vue
+++ UI/src/views/Languages.vue
@@ -6,7 +6,7 @@ import ConfigTable from "@/components/ConfigTable.vue";
 
 const { t } = useI18n();
 const COLUMNS = [
-    { key: "code", type: "text",            head: t("Code") },
+    { key: "code",        type: "text",     head: t("Code") },
     { key: "description", type: "text",     head: t("Description") },
 ];
 
