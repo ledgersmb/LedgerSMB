23a5b5aef95cf4f73a2e26b97a9d820efdd76af6
Erik Huelsmann <ehuels@gmail.com>
2023-05-28 13:06:39 +0200

    Fix regression while renaming setting with typo

--- sql/changes/1.9/fix-template-images-typo.sql
+++ sql/changes/1.9/fix-template-images-typo.sql
@@ -1,4 +1,10 @@
 
+-- if both keys exist, remove the one with the typo
+DELETE FROM defaults
+ WHERE setting_key = 'template_immages'
+   AND EXISTS (select from defaults
+                where setting_key = 'template_images');
+
 UPDATE defaults
    SET setting_key = 'template_images'
  WHERE setting_key = 'template_immages';
