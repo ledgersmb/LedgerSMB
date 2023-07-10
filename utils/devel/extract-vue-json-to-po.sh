ddbd75b8c1467e24b38b0710b978b9d7dfb79930
Erik Huelsmann <ehuels@gmail.com>
2023-07-10 23:07:43 +0200

    Correct pushd/popd scope

--- utils/devel/extract-vue-json-to-po.sh
+++ utils/devel/extract-vue-json-to-po.sh
@@ -8,8 +8,8 @@ utils/devel/extract-vue-template-translations.sh
 # Pull in locally translated text
 pushd UI/ >/dev/null
 i18next-conv -y --quiet --language "$json" --source src/locales/$json.json --target /tmp/$json.po
+popd >/dev/null
 ./utils/devel/extract-vue-template-translations-references.pl < /tmp/$json.po > /tmp/_$json.po
-popd UI/ >/dev/null
 msgcat --output-file=locale/po/$json.po --sort-output locale/po/$json.po /tmp/_$json.po \
     --add-location \
     || exit 1
