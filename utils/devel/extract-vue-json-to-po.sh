dc39eec6d71525011c94578661f3a7f4b809af42
Erik Huelsmann <ehuels@gmail.com>
2023-07-10 22:43:27 +0200

    Update translations on master

--- utils/devel/extract-vue-json-to-po.sh
+++ utils/devel/extract-vue-json-to-po.sh
@@ -6,8 +6,10 @@ json=${1:-en}
 utils/devel/extract-vue-template-translations.sh
 
 # Pull in locally translated text
-i18next-conv --quiet --language "$json" --source UI/src/locales/$json.json --target /tmp/$json.po
+pushd UI/ >/dev/null
+i18next-conv -y --quiet --language "$json" --source src/locales/$json.json --target /tmp/$json.po
 ./utils/devel/extract-vue-template-translations-references.pl < /tmp/$json.po > /tmp/_$json.po
+popd UI/ >/dev/null
 msgcat --output-file=locale/po/$json.po --sort-output locale/po/$json.po /tmp/_$json.po \
     --add-location \
     || exit 1
--- utils/devel/extract-vue-template-translations.sh
+++ utils/devel/extract-vue-template-translations.sh
@@ -8,9 +8,11 @@ done
 set -x
 
 # Extract Vue strings and make a pot file
-cp UI/src/locales/en.json /tmp/en.json
-npx vue-i18n-extract-translations -v "./UI/src/**/*.?(js|vue)" -l "/tmp/" --def-locale "en" \
+pushd UI/ >/dev/null
+cp src/locales/en.json /tmp/en.json
+npx vue-i18n-extract-translations -v "./src/**/*.?(js|vue)" -l "/tmp/" --def-locale "en" \
     --key "i18n" --keep-unused > /dev/null
 npx i18next-conv --quiet --pot --project LedgerSMB --language "en" -b /tmp/en.json -s /tmp/en.json --target /tmp/en.po
+popd >/dev/null
 ./utils/devel/extract-vue-template-translations-references.pl < /tmp/en.po
 
