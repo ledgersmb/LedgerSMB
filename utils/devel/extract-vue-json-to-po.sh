4e7dfa6cea70afe141fa7393890c12689acb203f
Erik Huelsmann <ehuels@gmail.com>
2023-07-10 23:55:01 +0200

    Update translations on master (#7494)
    * Update translations on master
    
    * Correct pushd/popd scope

--- utils/devel/extract-vue-json-to-po.sh
+++ utils/devel/extract-vue-json-to-po.sh
@@ -6,7 +6,9 @@ json=${1:-en}
 utils/devel/extract-vue-template-translations.sh
 
 # Pull in locally translated text
-i18next-conv --quiet --language "$json" --source UI/src/locales/$json.json --target /tmp/$json.po
+pushd UI/ >/dev/null
+i18next-conv -y --quiet --language "$json" --source src/locales/$json.json --target /tmp/$json.po
+popd >/dev/null
 ./utils/devel/extract-vue-template-translations-references.pl < /tmp/$json.po > /tmp/_$json.po
 msgcat --output-file=locale/po/$json.po --sort-output locale/po/$json.po /tmp/_$json.po \
     --add-location \
