daf6046f597f1b2ec06f895c23965581d4acfa7a
Erik Huelsmann <ehuels@gmail.com>
2023-07-23 20:00:04 +0200

    Account checkpoint balances should equal cumulative transaction totals

---
+++ sql/consistency/00030-account-checkpoint-balances.sql
@@ -0,0 +1,22 @@
+--- yaml frontmatter
+title: Account checkpoint balances
+description: |
+  Account checkpoints summarize the impact of preceeding transactions
+  on account level. The accumulated balance on an account should reconcile
+  with the total stored in account checkpoints.
+---
+
+select *, (select sum(amount_bc)
+             from acc_trans
+            where chart_id = account_id
+              and transdate <= end_date
+              and approved
+              and a.curr = curr) as journal_balance
+from (
+  select account_id, end_date, curr, sum(amount_bc) as checkpoint_balance
+    from account_checkpoint
+   group by account_id, end_date, curr
+) a
+ where checkpoint_balance <> journal_balance
+ order by end_date, account_id;
+
