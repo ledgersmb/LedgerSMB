c8d4b06b5eea4f43576bc75705e296fc55adeb94
Erik Huelsmann <ehuels@gmail.com>
2023-11-11 17:21:04 -0500

    Update version

--- lib/LedgerSMB.pm
+++ lib/LedgerSMB.pm
@@ -249,7 +249,7 @@ use LedgerSMB::Setting;
 use LedgerSMB::Template;
 use LedgerSMB::Template::UI;
 
-our $VERSION = '1.10.25-dev';
+our $VERSION = '1.10.25';
 
 my $logger = Log::Any->get_logger(category => 'LedgerSMB');
 my $expiration_parser = DateTime::Format::Duration::ISO8601->new;
