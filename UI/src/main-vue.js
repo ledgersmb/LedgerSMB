18604a1e54c0b1997cd8d76430e32792c8b145ee
Erik Huelsmann <ehuels@gmail.com>
2024-08-11 23:53:32 +0200

    Fix jslint errors

--- UI/src/main-vue.js
+++ UI/src/main-vue.js
@@ -8,18 +8,19 @@ import LoginPage from "@/views/LoginPage";
 import Main from "@/views/Main";
 import { useSessionUserStore } from "@/store/sessionUser";
 
+const dojoParser = require("dojo/parser");
+
 let locale;
 // the 'en' locale is currently empty, but included so any inline
 // strings may contain more than just the showable text (e.g. a
 // translation context)
-let fbLocales = [ "en" ];
-const parts = lsmbConfig.language.match(/([a-z]{2})-([a-z]{2})/i);
+let fbLocales = ["en"];
+const parts = window.lsmbConfig.language.match(/([a-z]{2})-([a-z]{2})/i);
 if (parts) {
     locale = parts[1].toLowerCase() + "_" + parts[2].toUpperCase();
     fbLocales.unshift(parts[1].toLowerCase());
-}
-else {
-    locale = lsmbConfig.language;
+} else {
+    locale = window.lsmbConfig.language;
 }
 let messages = {};
 for (let l of [locale, ...fbLocales]) {
@@ -27,8 +28,9 @@ for (let l of [locale, ...fbLocales]) {
         messages[l] = await import(
             /* webpackChunkName: "lang-[request]" */ `@/locales/${l}.json`
         );
+    } catch {
+        // do nothing, the file doesn't need to exist
     }
-    catch (e) {} // do nothing, the file doesn't need to exist
 }
 const i18n = createI18n({
     globalInjection: true,
@@ -54,9 +56,7 @@ let lsmbDirective = {
 };
 
 if (document.getElementById("main")) {
-    app = createApp(Main)
-        .use(router)
-        .use(createPinia());
+    app = createApp(Main).use(router).use(createPinia());
 
     useSessionUserStore().initialize();
     appName = "#main";
@@ -65,7 +65,6 @@ if (document.getElementById("main")) {
     appName = "#login";
 } else {
     /* In case we're running a "setup.pl" page */
-    const dojoParser = require("dojo/parser");
     dojoParser.parse(document.body).then(() => {
         const l = document.getElementById("loading");
         if (l) {
