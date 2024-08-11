c27af013716b6df8fb47f92cb8a4a41e0ac297bb
Erik Huelsmann <ehuels@gmail.com>
2024-08-12 00:26:54 +0200

    Run eslint on Main.vue
    Fix circular error about missing-but-disallowed closing '/' on img
    and hr.
    
    Reflow.
    
    Allow require, in this case.

--- UI/src/views/Main.vue
+++ UI/src/views/Main.vue
@@ -1,5 +1,8 @@
+<!-- @format -->
+
 <script>
-import { provide, ref } from "vue";
+/* global require */
+import { provide } from "vue";
 import { useI18n } from "vue-i18n";
 import Toaster from "@/components/Toaster";
 import { createToasterMachine } from "@/components/Toaster.machines";
@@ -11,27 +14,26 @@ export default {
     components: {
         Toaster
     },
-    data() {
-        const cfg = window.lsmbConfig;
-        return {
-            modelValue: 50,
-            company: cfg.company,
-            login: cfg.login,
-            version: cfg.version,
-        }
-    },
     setup() {
-        const i18n = useI18n({ useScope: 'global' });
-        const { t, locale } = i18n;
+        const { t } = useI18n({ useScope: "global" });
 
         const toasterMachine = createToasterMachine({ items: [] }, {});
-        provide( "toaster-machine", toasterMachine );
+        provide("toaster-machine", toasterMachine);
 
         const { send } = toasterMachine;
         provide("notify", (notification) => {
             send({ type: "add", item: notification });
         });
         return { t };
+    },
+    data() {
+        const cfg = window.lsmbConfig;
+        return {
+            modelValue: 50,
+            company: cfg.company,
+            login: cfg.login,
+            version: cfg.version
+        };
     },
     beforeUpdate() {
         document.body.removeAttribute("data-lsmb-done");
@@ -51,55 +53,71 @@ export default {
 </script>
 
 <template>
-    <div id="console-container"
-         data-dojo-type="dijit/layout/BorderContainer"
-         data-dojo-props="persist: true"
-         style="width: 100%; height: 100%">
-        <div data-dojo-type="dijit/layout/ContentPane"
-             data-dojo-props="region:'center'"
-             id="maindiv">
-            <router-view></router-view>
+    <div
+        id="console-container"
+        data-dojo-type="dijit/layout/BorderContainer"
+        data-dojo-props="persist: true"
+        style="width: 100%; height: 100%"
+    >
+        <div
+            id="maindiv"
+            data-dojo-type="dijit/layout/ContentPane"
+            data-dojo-props="region:'center'"
+        >
+            <router-view />
         </div>
-        <div id="menudiv" class="edgePanel"
-             style="width:30ex"
-             data-dojo-type="dijit/layout/ContentPane"
-             data-dojo-props="region: 'left', splitter: true, minSize: 200">
-            <div style="text-align: center"><a target="_blank"
-                                               rel="noopener noreferrer"
-                                               href="http://ledgersmb.org/">
-                <img class="cornerlogo"
-                     src="images/ledgersmb_small-trans.png"
-                     width="100"
-                     height="50"
-                     style="border:1px"
-                     alt="LedgerSMB" />
-            </a>
+        <div
+            id="menudiv"
+            class="edgePanel"
+            style="width: 30ex"
+            data-dojo-type="dijit/layout/ContentPane"
+            data-dojo-props="region: 'left', splitter: true, minSize: 200"
+        >
+            <div style="text-align: center">
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="http://ledgersmb.org/"
+                >
+                    <img
+                        class="cornerlogo"
+                        src="images/ledgersmb_small-trans.png"
+                        width="100"
+                        height="50"
+                        style="border: 1px"
+                        alt="LedgerSMB"
+                    />
+                </a>
             </div>
-            <div id="version_info">{{ t('LedgerSMB {version}',
-                                        { version: version }) }}</div>
+            <div id="version_info">
+                {{ t("LedgerSMB {version}", { version: version }) }}
+            </div>
             <table id="header_info" class="header_table">
                 <tr>
                     <td id="login_info_header" class="header_left">
                         {{ t("User") }}
                     </td>
-                    <td id="company_info_header"
-                        class="header_right">
+                    <td id="company_info_header" class="header_right">
                         {{ t("Company") }}
                     </td>
                 </tr>
                 <tr>
                     <td id="login_info" class="header_left">{{ login }}</td>
-                    <td id="company_info" class="header_right">{{ company }}</td>
+                    <td id="company_info" class="header_right">
+                        {{ company }}
+                    </td>
                 </tr>
             </table>
-            <hr>
+            <hr />
 
-            <div id="top_menu" data-dojo-type="lsmb/menus/Tree">
-            </div>
+            <div id="top_menu" data-dojo-type="lsmb/menus/Tree" />
         </div>
-        <div id="errorDialog" title="An error occurred!"
-             style="display:none;min-width:40ex"
-             data-dojo-type="dijit/Dialog"></div>
+        <div
+            id="errorDialog"
+            title="An error occurred!"
+            style="display: none; min-width: 40ex"
+            data-dojo-type="dijit/Dialog"
+        />
     </div>
     <Toaster />
 </template>
