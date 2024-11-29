41bb61cd1bf97d1b3565b5484fd7c49dcbcb1f87
Erik Huelsmann <ehuels@gmail.com>
2024-11-29 16:33:09 +0100

    Clean polyfills inserted by jest-fixed-jsdom

--- UI/tests/common/jest.polyfills.js
+++ UI/tests/common/jest.polyfills.js
@@ -1,14 +1,3 @@
 /* @format */
 /* global globalThis */
 
-const { Blob } = require("node:buffer");
-const { fetch, Headers, FormData, Request, Response } = require("undici");
-
-Object.defineProperties(globalThis, {
-    fetch: { value: fetch, writable: true },
-    Blob: { value: Blob },
-    Headers: { value: Headers },
-    FormData: { value: FormData },
-    Request: { value: Request, configurable: true },
-    Response: { value: Response, configurable: true }
-});
