dd165d7dd5cc78c09474d502e760b7e765090612
Nick Prater <nick@npbroadcast.com>
2025-03-09 08:40:55 +0000

    Remove newlines from lsmb-checkbox.js to please eslint
    Remove two newlines to resolve linter complaints.

--- UI/src/elements/lsmb-checkbox.js
+++ UI/src/elements/lsmb-checkbox.js
@@ -5,11 +5,9 @@ import { LsmbBaseInput } from "@/elements/lsmb-base-input";
 const dojoCheckBox = require("dijit/form/CheckBox");
 
 export class LsmbCheckBox extends LsmbBaseInput {
-
     _widgetClass() {
         return dojoCheckBox;
     }
-
 }
 
 customElements.define("lsmb-checkbox", LsmbCheckBox);
