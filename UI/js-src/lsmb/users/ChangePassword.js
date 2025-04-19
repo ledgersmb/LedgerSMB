13e88d9ad36513ad6b03a850346e274216c1b6a8
Nick Prater <nick@npbroadcast.com>
2025-04-19 21:47:26 +0100

    Consistent capitalization of field labels on Change Password screen
    Make capitalization of field labels consistent on change password
    screen - consistent both within the screen and with other interfaces
    within LedgerSMB.
    
    In lsmb, every work in a field label is capitalized, so
    `New Password` rather than `New password`.

--- UI/js-src/lsmb/users/ChangePassword.js
+++ UI/js-src/lsmb/users/ChangePassword.js
@@ -30,7 +30,7 @@ define([
             _lstrings: {
                 title: "Change Password",
                 "old password": "Old Password",
-                "new password": "New password",
+                "new password": "New Password",
                 verify: "Verify",
                 change: "Change Password",
                 "no-oldpw": "No Old Password",
