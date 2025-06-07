337d8abf1a8d1a389ed1a303fbd3570435d30ef2
Erik Huelsmann <ehuels@gmail.com>
2025-06-07 23:27:13 +0200

    Allow updating checkbox values from Pub/Sub channels

--- UI/src/elements/lsmb-checkbox.js
+++ UI/src/elements/lsmb-checkbox.js
@@ -8,7 +8,7 @@ const topic = require("dojo/topic");
 
 export class LsmbCheckBox extends LsmbBaseInput {
     _valueAttrs() {
-        return [...super._valueAttrs(), "checked", "topic"];
+        return [...super._valueAttrs(), "checked", "topic", "update-from"];
     }
 
     _rmAttrs() {
@@ -22,17 +22,27 @@ export class LsmbCheckBox extends LsmbBaseInput {
     _connectedCallback() {
         super._connectedCallback();
         if (this.dojoWidget) {
+            let widget = this.dojoWidget;
             let props = this._collectProps();
             if (props.topic) {
-                this.dojoWidget.own(
-                    on(this.dojoWidget.domNode, "change", () => {
-                        if (this.dojoWidget.checked) {
+                widget.own(
+                    on(widget.domNode, "change", () => {
+                        if (widget.checked) {
                             topic.publish(props.topic, props.value);
                         } else {
                             topic.publish(props.topic, null);
                         }
                     })
                 );
+            }
+            if (props["update-from"]) {
+                props["update-from"].split(",").forEach((channel) => {
+                    widget.own(
+                        topic.subscribe(channel, (value) => {
+                            widget.set("checked", value);
+                        })
+                    );
+                });
             }
         }
     }
