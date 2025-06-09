90ed2d80969014d88670c37b2f281873c489f93b
Erik Huelsmann <ehuels@gmail.com>
2025-06-09 21:49:09 +0200

    Remove unnecessary menu_insert() function

---
+++ sql/changes/1.13/drop-function-menu_insert.sql
@@ -0,0 +1,2 @@
+
+DROP FUNCTION IF EXISTS menu_insert(in_parent_id int, in_position int, in_label text);
--- sql/modules/Menu.sql
+++ sql/modules/Menu.sql
@@ -78,37 +78,6 @@ This function returns the complete menu tree.  It is used to generate nested
 menus for the web interface.
 $$;
 
-CREATE OR REPLACE FUNCTION
-menu_insert(in_parent_id int, in_position int, in_label text)
-returns int
-AS $$
-DECLARE
-        new_id int;
-BEGIN
-        UPDATE menu_node
-            -- prevent duplicates by setting negative as a first step
-           SET position = -1 * (position + 1)
-         WHERE parent = in_parent_id
-               AND position >= in_position;
-
-        UPDATE menu_node
-            -- negate again now that all numbers are final
-           SET position = -1 * position
-         WHERE parent = in_parent_id
-               AND position < 0;
-
-        INSERT INTO menu_node (parent, position, label)
-        VALUES (in_parent_id, in_position, in_label);
-
-        RETURN currval('menu_node_id_seq');
-END;
-$$ language plpgsql;
-
-comment on function menu_insert(int, int, text) is $$
-This function inserts menu items at arbitrary positions.  The arguments are, in
-order:  parent, position, label.  The return value is the id number of the menu
-item created. $$;
-
 
 DROP VIEW IF EXISTS menu_friendly;
 CREATE VIEW menu_friendly AS
