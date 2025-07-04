62d08a54dfda0ecb8fd1a006f634e2a8d50feb6b
Erik Huelsmann <ehuels@gmail.com>
2025-07-04 11:59:18 +0200

    Adjust based on advice from @shehwarahmad from fiverr.com

--- UI/jest.config.js
+++ UI/jest.config.js
@@ -262,7 +262,7 @@ module.exports = {
     },
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-    // transformIgnorePatterns: [],
+    transformIgnorePatterns: [ '/node_modules/(?!(lodash-es|@quasar)/)' ],
 
     // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
     // unmockedModulePathPatterns: undefined,
