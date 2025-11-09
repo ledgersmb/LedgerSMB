d2eff7fd39a94f2aff600e770d68d685a9c9622d
Erik Huelsmann <ehuels@gmail.com>
2025-11-09 19:39:16 +0100

    Update polyfills

--- UI/tests/common/mocks/server.js
+++ UI/tests/common/mocks/server.js
@@ -3,12 +3,6 @@
 
 import { setupServer } from "msw/node";
 import { handlers } from "./handlers";
-const { MessageChannel, MessagePort } = require("node:worker_threads");
 
 // This configures a Service Server with the given request handlers.
 export const server = setupServer(...handlers);
-
-Object.defineProperties(globalThis, {
-    MessageChannel: { value: MessageChannel },
-    MessagePort: { value: MessagePort }
-});
