f01f6570dbf6fcbdd41b688f21219266e5890e45
Erik Huelsmann <ehuels@gmail.com>
2026-02-23 08:48:13 +0100

    Fix SQL function lastcost()

--- sql/modules/old.sql
+++ sql/modules/old.sql
@@ -43,28 +43,30 @@ END;
 --
 
 
-CREATE OR REPLACE FUNCTION lastcost(int) RETURNS FLOAT AS '
+CREATE OR REPLACE FUNCTION lastcost(int) RETURNS FLOAT AS
+$sql$
+  DECLARE
 
-DECLARE
-
-v_cost float;
-v_parts_id alias for $1;
+  v_cost float;
+  v_parts_id alias for $1;
 
 BEGIN
 
-  SELECT INTO v_cost sellprice FROM invoice i
-  JOIN ap a ON (a.id = i.trans_id)
-  WHERE i.parts_id = v_parts_id
-  ORDER BY a.transdate desc, a.id desc
-  LIMIT 1;
+  SELECT INTO v_cost sellprice
+    FROM invoice i
+           JOIN ap a ON (a.id = i.trans_id)
+           JOIN transactions txn ON a.id = txn.id
+   WHERE i.parts_id = v_parts_id
+   ORDER BY txn.transdate desc, a.id desc
+   LIMIT 1;
 
   IF v_cost IS NULL THEN
     v_cost := 0;
   END IF;
 
-RETURN v_cost;
+  RETURN v_cost;
 END;
-' language plpgsql;
+$sql$ language plpgsql;
 -- end function
 --
 
