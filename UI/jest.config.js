05eeaa6c655198bcd7cf6db9fbc682e2a92cd42c
Erik Huelsmann <ehuels@gmail.com>
2026-04-18 16:49:30 +0200

    Fix jest configuration

--- UI/jest.config.js
+++ UI/jest.config.js
@@ -167,12 +167,13 @@ const common = {
     transform: {
         "^.+\\.yaml$": "yaml-jest-transform",
         "^.+\\.[mc]?js$": "babel-jest",
+        "^.+\\.jsx$": "babel-jest",
         "^@": "babel-jest",
         "^.+\\.vue$": "@vue/vue3-jest"
     },
 
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-    transformIgnorePatterns: [ '/node_modules/(?!(lodash-es|@quasar|until-async|msw|@mswjs|rettime)/)' ],
+    transformIgnorePatterns: [ '/node_modules/(?!(lodash-es|@quasar|until-async|msw|@mswjs|rettime|@open-draft)/)' ],
 
     // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
     // unmockedModulePathPatterns: undefined,
@@ -283,10 +284,11 @@ module.exports = {
                 customExportConditions: ["node", "node-addons"]
             },
             testPathIgnorePatterns: [ "<rootDir>/tests/specs/openapi/.*\\.spec\\.js" ],
-            transformIgnorePatterns: [ '/node_modules/(?!(lodash-es|quasar|until-async|msw|@mswjs|rettime)/)' ],
+            transformIgnorePatterns: [ '/node_modules/(?!(lodash-es|quasar|until-async|msw|@mswjs|rettime|@open-draft)/)' ],
             transform: {
                 "^.+\\.yaml$": "yaml-jest-transform",
                 "^.+\\.[mc]?js$": "babel-jest",
+                "^.+\\.jsx$": "babel-jest",
                 "^.+\\.vue$": "@vue/vue3-jest",
                 "^@": "babel-jest",
 
@@ -302,6 +304,7 @@ module.exports = {
             transform: {
                 "^.+\\.yaml$": "yaml-jest-transform",
                 "^.+\\.[mc]?js$": "babel-jest",
+                "^.+\\.jsx$": "babel-jest",
                 "^.+\\.vue$": "@vue/vue3-jest",
                 "^@": "babel-jest",
             },
