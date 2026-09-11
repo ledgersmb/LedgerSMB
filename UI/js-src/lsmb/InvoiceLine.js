57eeecfe1c6ab05438c2fe03401aa35859b84368
Erik Huelsmann <ehuels@gmail.com>
2026-09-11 21:59:01 +0200

    Reduce Dojo code for managing the invoice entry screen

--- UI/js-src/lsmb/InvoiceLine.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-define([
-    "dojo/_base/declare",
-    "dijit/_WidgetBase",
-    "dijit/_Container"
-], function (declare, _WidgetBase, _Container) {
-    return declare("lsmb/InvoiceLine", [_WidgetBase, _Container], {});
-});
